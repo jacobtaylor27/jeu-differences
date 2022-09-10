002b2e5e1c24df189d9d2df84574f80bff64275f
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-10 11:29:40 -0400

    feat: new game selection page init

---
+++ client/src/app/pages/game-selection-page/game-selection-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-selection-page',
+  templateUrl: './game-selection-page.component.html',
+  styleUrls: ['./game-selection-page.component.scss']
+})
+export class GameSelectionPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

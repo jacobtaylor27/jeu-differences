a8fb7004f9d227095f37f2b9b192e824c1fa0819
Xavier Jouslin <jouslin@nowray.net>
2022-09-13 10:39:17 -0400

    feat: create output event to propagate event for canvas

--- client/src/app/components/tool-box/tool-box.component.ts
+++ client/src/app/components/tool-box/tool-box.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-tool-box',
     templateUrl: './tool-box.component.html',
     styleUrls: ['./tool-box.component.scss'],
 })
-export class ToolBoxComponent {}
+export class ToolBoxComponent {
+    @Output() switchToEraseCanvasEvent: EventEmitter<void> = new EventEmitter<void>();
+    @Output() switchToPensilCanvasEvent: EventEmitter<void> = new EventEmitter<void>();
+    @Output() changeColorEvent: EventEmitter<void> = new EventEmitter<void>();
+}

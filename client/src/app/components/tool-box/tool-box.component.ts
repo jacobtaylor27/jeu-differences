f265584a77f3883f1562fe05f627a50608708967
Xavier Jouslin <jouslin@nowray.net>
2022-09-13 10:26:30 -0400

    feat: add toolbox component

---
+++ client/src/app/components/tool-box/tool-box.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-tool-box',
+    templateUrl: './tool-box.component.html',
+    styleUrls: ['./tool-box.component.scss'],
+})
+export class ToolBoxComponent {}

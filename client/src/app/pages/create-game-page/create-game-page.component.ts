92a72c88fea81171fb1db0f0c20d01077529ba29
Xavier Jouslin <jouslin@nowray.net>
2022-09-13 19:54:44 -0400

    feat: add tool box and do some manual test to see the event manager

--- client/src/app/pages/create-game-page/create-game-page.component.ts
+++ client/src/app/pages/create-game-page/create-game-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Tool } from '@app/constant/tool';
 
 @Component({
     selector: 'app-create-game-page',
@@ -8,12 +9,25 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateGamePageComponent {
     form: FormGroup;
+
     constructor() {
         this.form = new FormGroup({
             name: new FormControl('', Validators.required),
             expansionRadius: new FormControl(3, Validators.required),
-            img: new FormControl(null, [Validators.required]),
+            img: new FormControl(null, Validators.required),
             imgDiff: new FormControl(null, Validators.required),
         });
+    }
+    changeColor(color: string) {
+        console.log(color);
+        return;
+    }
+
+    changePensilState(tool: Tool) {
+        console.log(tool);
+        return;
+    }
+    onSubmit() {
+        return;
     }
 }

d932bc8cb8bcb6168992cfc164a642835916100f
Xavier Jouslin <jouslin@nowray.net>
2022-09-14 19:36:47 -0400

    feat: create start event handler

--- client/src/app/components/draw-canvas/draw-canvas.component.ts
+++ client/src/app/components/draw-canvas/draw-canvas.component.ts
@@ -5,8 +5,20 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './draw-canvas.component.html',
     styleUrls: ['./draw-canvas.component.scss'],
 })
-export class DrawCanvasComponent implements OnInit {
-    constructor() {}
+export class DrawCanvasComponent {
+    @Input() tool: Tool;
+    @Input() color: string = '#000000';
+    @ViewChild('canvas', { static: false }) canvas!: ElementRef<HTMLCanvasElement>;
+    coordDraw: Vec2 = { x: 0, y: 0 };
+    isClick: boolean = false;
 
-    ngOnInit(): void {}
+    // https://daily-dev-tips.com/posts/javascript-mouse-drawing-on-the-canvas/
+    start(event: MouseEvent) {
+        this.isClick = true;
+        this.reposition(event);
+    }
+    reposition(event: MouseEvent) {
+        this.coordDraw.x = event.clientX - this.canvas.nativeElement.offsetLeft;
+        this.coordDraw.y = event.clientY - this.canvas.nativeElement.offsetTop;
+    }
 }

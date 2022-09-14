d407d51f75759caa4493e9bbdc3b67ec3eae9952
Xavier Jouslin <jouslin@nowray.net>
2022-09-14 19:40:45 -0400

    feat: add draw pensil event handler

--- client/src/app/components/draw-canvas/draw-canvas.component.ts
+++ client/src/app/components/draw-canvas/draw-canvas.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Tool } from '@app/constant/tool';
+import { Vec2 } from '@app/interfaces/vec2';
 @Component({
     selector: 'app-draw-canvas',
     templateUrl: './draw-canvas.component.html',
@@ -21,6 +22,23 @@ export class DrawCanvasComponent {
     stop() {
         this.isClick = false;
     }
+
+    draw(event: MouseEvent) {
+        if (!this.isClick) {
+            return;
+        }
+        console.log('hello');
+        const ctx: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+        ctx.beginPath();
+        ctx.lineWidth = 5;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = this.color;
+        ctx.moveTo(this.coordDraw.x, this.coordDraw.y);
+        this.reposition(event);
+        ctx.lineTo(this.coordDraw.x, this.coordDraw.y);
+        ctx.stroke();
+    }
+
     reposition(event: MouseEvent) {
         this.coordDraw.x = event.clientX - this.canvas.nativeElement.offsetLeft;
         this.coordDraw.y = event.clientY - this.canvas.nativeElement.offsetTop;

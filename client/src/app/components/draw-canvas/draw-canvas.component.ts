afc023bc050357ababee3b7469e558f17b1093cd
Xavier Jouslin <jouslin@nowray.net>
2022-09-14 20:19:21 -0400

    feat: add draw Service dependency + refactor with reposition function

--- client/src/app/components/draw-canvas/draw-canvas.component.ts
+++ client/src/app/components/draw-canvas/draw-canvas.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Tool } from '@app/constant/tool';
 import { Vec2 } from '@app/interfaces/vec2';
+import { DrawService } from '@app/services/draw-service/draw-service.service';
 @Component({
     selector: 'app-draw-canvas',
     templateUrl: './draw-canvas.component.html',
@@ -13,10 +14,12 @@ export class DrawCanvasComponent {
     coordDraw: Vec2 = { x: 0, y: 0 };
     isClick: boolean = false;
 
+    constructor(private drawService: DrawService) {}
+
     // https://daily-dev-tips.com/posts/javascript-mouse-drawing-on-the-canvas/
     start(event: MouseEvent) {
         this.isClick = true;
-        this.reposition(event);
+        this.coordDraw = this.drawService.reposition(this.canvas.nativeElement, event);
     }
 
     stop() {
@@ -27,20 +30,14 @@ export class DrawCanvasComponent {
         if (!this.isClick) {
             return;
         }
-        console.log('hello');
         const ctx: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
         ctx.beginPath();
         ctx.lineWidth = 5;
         ctx.lineCap = 'round';
         ctx.strokeStyle = this.color;
         ctx.moveTo(this.coordDraw.x, this.coordDraw.y);
-        this.reposition(event);
+        this.coordDraw = this.drawService.reposition(this.canvas.nativeElement, event);
         ctx.lineTo(this.coordDraw.x, this.coordDraw.y);
         ctx.stroke();
-    }
-
-    reposition(event: MouseEvent) {
-        this.coordDraw.x = event.clientX - this.canvas.nativeElement.offsetLeft;
-        this.coordDraw.y = event.clientY - this.canvas.nativeElement.offsetTop;
     }
 }

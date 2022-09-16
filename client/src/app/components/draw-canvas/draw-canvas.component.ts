2831999bb24ecc9e24473a6abb00299be9065ddb
Xavier Jouslin <jouslin@nowray.net>
2022-09-15 22:52:32 -0400

    feat: change propreties with pencil interface

--- client/src/app/components/draw-canvas/draw-canvas.component.ts
+++ client/src/app/components/draw-canvas/draw-canvas.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Tool } from '@app/constant/tool';
+import { Pencil } from '@app/interfaces/pencil';
 import { Vec2 } from '@app/interfaces/vec2';
 import { DrawService } from '@app/services/draw-service/draw-service.service';
 @Component({
@@ -13,7 +14,7 @@ export class DrawCanvasComponent {
     @ViewChild('canvas', { static: false }) canvas!: ElementRef<HTMLCanvasElement>;
     coordDraw: Vec2 = { x: 0, y: 0 };
     isClick: boolean = false;
-
+    pencil: Pencil = { width: 5, cap: 'round', color: this.color };
     constructor(private drawService: DrawService) {}
 
     // https://daily-dev-tips.com/posts/javascript-mouse-drawing-on-the-canvas/
@@ -30,11 +31,15 @@ export class DrawCanvasComponent {
         if (!this.isClick) {
             return;
         }
+        this.drawPoint(event);
+    }
+
+    drawPoint(event: MouseEvent) {
         const ctx: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
         ctx.beginPath();
-        ctx.lineWidth = 5;
-        ctx.lineCap = 'round';
-        ctx.strokeStyle = this.color;
+        ctx.lineWidth = this.pencil.width;
+        ctx.lineCap = this.pencil.cap;
+        ctx.strokeStyle = this.pencil.color;
         ctx.moveTo(this.coordDraw.x, this.coordDraw.y);
         this.coordDraw = this.drawService.reposition(this.canvas.nativeElement, event);
         ctx.lineTo(this.coordDraw.x, this.coordDraw.y);

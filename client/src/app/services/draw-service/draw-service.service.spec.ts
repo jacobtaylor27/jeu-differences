f9e4472bcb773ee88adb6f66339902c67d619e68
Xavier Jouslin <jouslin@nowray.net>
2022-09-16 08:28:15 -0400

    test: test reposition

--- client/src/app/services/draw-service/draw-service.service.spec.ts
+++ client/src/app/services/draw-service/draw-service.service.spec.ts
@@ -13,4 +13,11 @@ describe('DrawServiceService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('reposition should return a position', () => {
+        const expectedReposition = { x: 10, y: 10 };
+        expect(service.reposition({ offsetLeft: 0, offsetTop: 0 } as HTMLCanvasElement, { clientX: 10, clientY: 10 } as MouseEvent)).toEqual(
+            expectedReposition,
+        );
+    });
 });

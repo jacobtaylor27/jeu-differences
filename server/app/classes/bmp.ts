526856154d9db3a4c9cd9b4ea21414a7d2b46532
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-16 12:38:37 -0400

    feat: reading the buffer of an image

--- server/app/classes/bmp.ts
+++ server/app/classes/bmp.ts
@@ -1,1 +1,13 @@
-export class Bmp {}
+import * as bmp from 'bmp-js';
+import * as fs from 'fs';
+
+export class Bmp {
+    width: number;
+    height: number;
+    bmpData: bmp.BmpDecoder;
+
+    constructor(filepath: string) {
+        const bmpBuffer = fs.readFileSync(filepath);
+        this.bmpData = bmp.decode(bmpBuffer);
+    }
+}

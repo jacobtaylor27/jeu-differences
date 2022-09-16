100e4a356524123ed9aeff4773ef5c6de664c45e
Xavier Jouslin <jouslin@nowray.net>
2022-09-16 08:43:50 -0400

    test: format tool box test file

--- client/src/app/components/tool-box/tool-box.component.spec.ts
+++ client/src/app/components/tool-box/tool-box.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ToolBoxComponent } from './tool-box.component';
 
 describe('ToolBoxComponent', () => {
-  let component: ToolBoxComponent;
-  let fixture: ComponentFixture<ToolBoxComponent>;
+    let component: ToolBoxComponent;
+    let fixture: ComponentFixture<ToolBoxComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ToolBoxComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ToolBoxComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(ToolBoxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ToolBoxComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

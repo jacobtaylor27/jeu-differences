013260ae328851488455814711f371bef4bd04e3
Xavier Jouslin <jouslin@nowray.net>
2022-09-15 22:59:08 -0400

    feat: add temp route for component view

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DrawCanvasComponent } from '@app/components/draw-canvas/draw-canvas.component';
+import { CreateGamePageComponent } from '@app/pages/create-game-page/create-game-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
@@ -8,7 +10,9 @@ const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
+    { path: 'create', component: CreateGamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'draw', component: DrawCanvasComponent },
     { path: '**', redirectTo: '/home' },
 ];
 

c1a49fbe9318271da4972897a145f86ef6fcfcb5
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-17 12:28:49 -0400

    feat: adding a basic class for the decoder

--- server/app/classes/bmp-encoder/bmp-encoder.ts
+++ server/app/classes/bmp-encoder/bmp-encoder.ts
@@ -1,1 +1,32 @@
-class BmpEncoder {}
+import { Bmp } from '@app/classes/bmp/bmp';
+import { Pixel } from '@app/interface/pixel';
+import * as bmp from 'bmp-js';
+
+export class BmpEncoder {
+    static encode(filename: string, bmpObj: Bmp) {
+        const width = bmpObj.width;
+        const height = bmpObj.height;
+        const data = this.getBuffer(bmpObj.pixels);
+        const bmpData = {
+            width,
+            height,
+            data,
+        };
+        const rawData = bmp.encode(bmpData);
+        // fs.WriteFileSync('./image.bmp', rawData.data);
+        return rawData;
+    }
+    static getBuffer(pixels: Pixel[][]): Buffer {
+        const rawPixels: number[] = [];
+
+        pixels.forEach((scanLine) => {
+            scanLine.forEach((pixel) => {
+                rawPixels.push(pixel.a);
+                rawPixels.push(pixel.r);
+                rawPixels.push(pixel.g);
+                rawPixels.push(pixel.b);
+            });
+        });
+        // return new Buffer(rawPixels, 'utf8');
+    }
+}

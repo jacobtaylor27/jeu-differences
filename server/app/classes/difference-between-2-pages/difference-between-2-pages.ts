ad5957d025964865b5e9cde23708d77e52b18620
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-09-17 15:59:19 -0400

    fix: changing variables names

--- server/app/classes/difference-between-2-pages/difference-between-2-pages.ts
+++ server/app/classes/difference-between-2-pages/difference-between-2-pages.ts
@@ -2,40 +2,45 @@ import { BmpDecoder } from '@app/classes/bmp-decoder/bmp-decoder';
 import { Bmp } from '@app/classes/bmp/bmp';
 import { Pixel } from '@app/interface/pixel';
 export class DifferenceBetween2Images {
-    firstImagePath: string;
-    secondImagePath: string;
+    originalImagePath: string;
+    modifiedImagePath: string;
 
-    constructor(firstPath: string, secondPath: string) {
-        this.firstImagePath = firstPath;
-        this.secondImagePath = secondPath;
+    constructor(originalImagePath: string, modifiedImagePath: string) {
+        this.originalImagePath = originalImagePath;
+        this.modifiedImagePath = modifiedImagePath;
     }
 
-    static async differenceBetween2Images(firstPath: string, secondPath: string): Promise<Bmp> {
-        const firstBmp = await this.produceImageBmp(firstPath);
-        const secondBmp = await this.produceImageBmp(secondPath);
-        if (this.haveSameHeight(firstBmp.height, secondBmp.height) && this.haveSameWidth(firstBmp.width, secondBmp.width)) {           
-            for (let i = 0; i < firstBmp.pixels.length; i++) {
-                for (let j = 0; j < firstBmp.pixels.length; j++) {
-                    if (this.equalPixels(firstBmp.pixels[i][j], secondBmp.pixels[i][j])) {
-                        this.whitePixel(secondBmp.pixels[i][j]);
+    static async differenceBetween2Images(originalImagePath: string, modifiedImagePath: string): Promise<Bmp> {
+        const originalImage = await this.produceImageBmp(originalImagePath);
+        const modifiedImage = await this.produceImageBmp(modifiedImagePath);
+        if (this.haveSameHeight(originalImage.height, modifiedImage.height) && this.haveSameWidth(originalImage.width, modifiedImage.width)) {
+            for (let i = 0; i < originalImage.pixels.length; i++) {
+                for (let j = 0; j < modifiedImage.pixels.length; j++) {
+                    if (this.equalPixels(originalImage.pixels[i][j], modifiedImage.pixels[i][j])) {
+                        this.whitePixel(modifiedImage.pixels[i][j]);
                     }
                 }
             }
         }
-        return secondBmp; 
+        return modifiedImage; 
     }
 
     static async produceImageBmp(imagePath: string): Promise<Bmp> {
         return await BmpDecoder.decode(imagePath);
     }
-    static haveSameHeight(firstImageHeight: number, secondImageHeight: number) {
-        return firstImageHeight === secondImageHeight;
+    static haveSameHeight(originalImageHeight: number, modifiedImageHeight: number) {
+        return originalImageHeight === modifiedImageHeight;
     }
-    static haveSameWidth(firstImageWidth: number, secondImageWidth: number): boolean {
-        return firstImageWidth === secondImageWidth; 
+    static haveSameWidth(originalImageWidth: number, modifiedImageWidth: number): boolean {
+        return originalImageWidth === modifiedImageWidth;
     }
-    static equalPixels(firstPixel: Pixel, secondPixel: Pixel): boolean {
-        return firstPixel.a === secondPixel.a && firstPixel.b === secondPixel.b && firstPixel.g === secondPixel.g && firstPixel.r === secondPixel.r ; 
+    static equalPixels(originalImage: Pixel, modifiedImage: Pixel): boolean {
+        return (
+            originalImage.a === modifiedImage.a &&
+            originalImage.b === modifiedImage.b &&
+            originalImage.g === modifiedImage.g &&
+            originalImage.r === modifiedImage.r
+        );
     }
     static whitePixel(pixel: Pixel): void {
         pixel.a = 0;

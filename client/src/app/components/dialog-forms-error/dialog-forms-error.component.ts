cad26a3b7131e25fd6017fedf7cf399306c11b78
Xavier Jouslin <jouslin@nowray.net>
2022-09-17 12:38:14 -0400

    feat: set template to display all errors in the form

--- client/src/app/components/dialog-forms-error/dialog-forms-error.component.ts
+++ client/src/app/components/dialog-forms-error/dialog-forms-error.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-dialog-forms-error',
-  templateUrl: './dialog-forms-error.component.html',
-  styleUrls: ['./dialog-forms-error.component.scss']
+    selector: 'app-dialog-forms-error',
+    templateUrl: './dialog-forms-error.component.html',
+    styleUrls: ['./dialog-forms-error.component.scss'],
 })
-export class DialogFormsErrorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DialogFormsErrorComponent {
+    @Input() formTitle: string;
+    @Input() errorMessages: string[];
 }

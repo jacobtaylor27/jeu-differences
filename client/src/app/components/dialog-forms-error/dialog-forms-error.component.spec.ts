5de49e99e9115581fd865c815c6979253c0a7aa8
Xavier Jouslin <jouslin@nowray.net>
2022-09-17 15:34:42 -0400

    test: add provider for httclient and material dependency

--- client/src/app/components/dialog-forms-error/dialog-forms-error.component.spec.ts
+++ client/src/app/components/dialog-forms-error/dialog-forms-error.component.spec.ts
@@ -1,23 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { DialogFormsErrorComponent } from './dialog-forms-error.component';
 
 describe('DialogFormsErrorComponent', () => {
-  let component: DialogFormsErrorComponent;
-  let fixture: ComponentFixture<DialogFormsErrorComponent>;
+    let component: DialogFormsErrorComponent;
+    let fixture: ComponentFixture<DialogFormsErrorComponent>;
+    const model = { formTitle: 'form', errorMessages: ['test error'] };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DialogFormsErrorComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DialogFormsErrorComponent],
+            imports: [MatDialogModule],
+            providers: [{ provide: MAT_DIALOG_DATA, useValue: model }],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DialogFormsErrorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DialogFormsErrorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

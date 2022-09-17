4aff744d001b6340166f26b7f936ff17c53bc22f
Xavier Jouslin <jouslin@nowray.net>
2022-09-17 15:53:57 -0400

    fix: lint on Tool Box Service

--- client/src/app/services/tool-box/tool-box.service.spec.ts
+++ client/src/app/services/tool-box/tool-box.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { ToolBoxService } from './tool-box.service';
 
 describe('ToolBoxService', () => {
-  let service: ToolBoxService;
+    let service: ToolBoxService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ToolBoxService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ToolBoxService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });

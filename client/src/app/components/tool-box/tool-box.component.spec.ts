73cea81dbb74b3dde4f601fc41f325459443f934
Xavier Jouslin <jouslin@nowray.net>
2022-09-17 10:12:24 -0400

    test: add provider for tool box component

--- client/src/app/components/tool-box/tool-box.component.spec.ts
+++ client/src/app/components/tool-box/tool-box.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSliderChange } from '@angular/material/slider';
+import { Tool } from '@app/constant/tool';
+import { Pencil } from '@app/interfaces/pencil';
+import { ToolBoxService } from '@app/services/tool-box/tool-box.service';
+import { Subject } from 'rxjs';
 
 import { ToolBoxComponent } from './tool-box.component';
 
 describe('ToolBoxComponent', () => {
     let component: ToolBoxComponent;
     let fixture: ComponentFixture<ToolBoxComponent>;
-
+    let toolBoxServiceSpyObj: jasmine.SpyObj<ToolBoxService>;
     beforeEach(async () => {
+        toolBoxServiceSpyObj = jasmine.createSpyObj('ToolBoxService', [], { $pencil: new Subject() });
         await TestBed.configureTestingModule({
             declarations: [ToolBoxComponent],
+            providers: [{ provide: ToolBoxService, useValue: toolBoxServiceSpyObj }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(ToolBoxComponent);

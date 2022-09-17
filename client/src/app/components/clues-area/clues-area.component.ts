43a39abcdd6f22d42b69c4fccb0dd3d203f22167
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-17 16:08:32 -0400

    feat : add button for clues and icons

--- client/src/app/components/clues-area/clues-area.component.ts
+++ client/src/app/components/clues-area/clues-area.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
     styleUrls: ['./clues-area.component.scss'],
 })
 export class CluesAreaComponent {
-    constructor() {}
+    NUMBER_OF_CLUES: number = 3;
+    clueAskedCounter: number = 0;
+    isDisabled: boolean = false;
+
+    getClue() {
+        this.clueAskedCounter++;
+        if (this.clueAskedCounter === this.NUMBER_OF_CLUES) {
+            this.isDisabled = true;
+        }
+        // TODO add logic
+    }
 }

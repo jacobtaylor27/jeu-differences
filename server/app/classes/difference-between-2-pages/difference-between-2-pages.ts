f65958a0fe32fd38f74bc9a8f80986667aa65d27
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-09-17 14:35:28 -0400

    feat:Adding functions to detect the difference between 2 images

--- server/app/classes/difference-between-2-pages/difference-between-2-pages.ts
+++ server/app/classes/difference-between-2-pages/difference-between-2-pages.ts
@@ -1,5 +1,6 @@
 import { BmpDecoder } from '@app/classes/bmp-decoder/bmp-decoder';
 import { Bmp } from '@app/classes/bmp/bmp';
+import { Pixel } from '@app/interface/pixel';
 export class DifferenceBetween2Images {
     firstImagePath: string;
     secondImagePath: string;
@@ -14,5 +15,31 @@ export class DifferenceBetween2Images {
     }
     private async secondImageBmp(secondImagePath: string): Promise<Bmp> {
         return await BmpDecoder.decode(secondImagePath);
+    }
+    private haveSameHeight(firstImageHeight: number, secondImageHeight: number) {
+        return firstImageHeight === secondImageHeight;
+    }
+    private haveSameWidth(firstImageWidth: number, secondImageWidth: number): boolean {
+        return firstImageWidth === secondImageWidth; 
+    }
+    private equalPixels(firstPixel: Pixel, secondPixel: Pixel): boolean {
+        return firstPixel.a === secondPixel.a && firstPixel.b === secondPixel.b && firstPixel.g === secondPixel.g && firstPixel.r === secondPixel.r ; 
+    }
+    private whitePixel(pixel: Pixel): void {
+        pixel.a = 0;
+        pixel.b = 255;
+        pixel.g = 255;
+        pixel.r = 255;
+    }
+    private async differenceBetween2Images(firstPath: string, secondPath: string) {
+        const firstBmp = await this.firstImageBmp(firstPath);
+        const secondBmp = await this.secondImageBmp(secondPath);
+        if (this.haveSameHeight(firstBmp.height, secondBmp.height) && this.haveSameWidth(firstBmp.width, secondBmp.width)) {
+            for (let i = 0; i < firstBmp.pixels.length; i++) {
+                if (this.equalPixels(firstBmp[i], secondBmp[i])) {
+                    this.whitePixel(secondBmp[i]);
+                }
+            }
+        }
     }
 }

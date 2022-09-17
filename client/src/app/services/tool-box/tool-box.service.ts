c2c03daa74597b49d5b8a637f09dd33e75cd6f9b
Xavier Jouslin <jouslin@nowray.net>
2022-09-16 21:10:02 -0400

    feat: add pencil subject in the tool box service

--- client/src/app/services/tool-box/tool-box.service.ts
+++ client/src/app/services/tool-box/tool-box.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Pencil } from '@app/interfaces/pencil';
+import { Subject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
-export class ToolBoxService {}
+export class ToolBoxService {
+    $pencil: Subject<Pencil>;
+    constructor() {
+        this.$pencil = new Subject();
+    }
+}

8a25f7b4c7e02a0ec651e807bed708302b18465c
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-17 16:04:42 -0400

    fix: deleting image before each test so that the results won't be influenced by the one that came before

--- server/app/classes/bmp-encoder/bmp-encoder.spec.ts
+++ server/app/classes/bmp-encoder/bmp-encoder.spec.ts
@@ -2,19 +2,20 @@ import { BmpDecoder } from '@app/classes/bmp-decoder/bmp-decoder';
 import { BmpEncoder } from '@app/classes/bmp-encoder/bmp-encoder';
 import { Bmp } from '@app/classes/bmp/bmp';
 import { expect } from 'chai';
+import * as fs from 'fs';
 import { describe } from 'mocha';
 
 describe('BmpEncoder', () => {
-    beforeEach(() => {});
-    afterEach(() => {});
+    const FILENAME_FOR_TESTING = './assets/test-bmp/result.bmp';
+    beforeEach(() => {
+        fs.unlink(FILENAME_FOR_TESTING, (_) => {});
+    });
     it('getBuffer() should convert a 2D array of pixels into a buffer', async () => {
         const originalFile = './assets/test-bmp/test_bmp_original.bmp';
         const bmpProduced: Bmp = await BmpDecoder.decode(originalFile);
+        BmpEncoder.encode(FILENAME_FOR_TESTING, bmpProduced);
 
-        const producedFile = './assets/test-bmp/result.bmp';
-        BmpEncoder.encode(producedFile, bmpProduced);
-
-        const bmpExpected: Bmp = await BmpDecoder.decode(producedFile);
+        const bmpExpected: Bmp = await BmpDecoder.decode(FILENAME_FOR_TESTING);
         expect(bmpProduced).to.eql(bmpExpected);
     });
 

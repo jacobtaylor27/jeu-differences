feb3cbb4b5ace5311aacc2fcdda6ba5aa77e5364
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-17 14:22:38 -0400

    feat : added mat-dialog to exit && redirecting to home-page

--- client/src/app/components/exit-game-button/exit-game-button.component.ts
+++ client/src/app/components/exit-game-button/exit-game-button.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-exit-game-button',
-  templateUrl: './exit-game-button.component.html',
-  styleUrls: ['./exit-game-button.component.scss']
+    selector: 'app-exit-game-button',
+    templateUrl: './exit-game-button.component.html',
+    styleUrls: ['./exit-game-button.component.scss'],
 })
-export class ExitGameButtonComponent implements OnInit {
+export class ExitGameButtonComponent {
+    @ViewChild('exitDialogContent')
+    private readonly exitDialogContentRef: TemplateRef<HTMLElement>;
+    constructor(private readonly matDialog: MatDialog) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    onExit(): void {
+        this.matDialog.open(this.exitDialogContentRef);
+    }
 }

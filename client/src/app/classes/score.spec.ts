1eed6633326254faf91f4e3d38d29696d80e822e
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-18 12:39:09 -0400

    feat: new test for score

--- client/src/app/classes/score.spec.ts
+++ client/src/app/classes/score.spec.ts
@@ -1,14 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 import { Score } from '@app/classes/score';
+import { GameCategory } from '@app/enums/game-category';
 
-describe('CanvasTestHelper', () => {
+describe('Score', () => {
     let score: Score;
 
     beforeEach(() => {
-        score = TestBed.inject(Score);
+        TestBed.configureTestingModule({
+            providers: [Score],
+        }).compileComponents();
+
+        score = new Score('test', 10, GameCategory.Solo);
     });
 
     it('should be created', () => {
         expect(score).toBeTruthy();
+    });
+
+    it('should convert time', () => {
+        score.time = 60;
+        expect(score.convertTime()).toEqual('1:00');
+        
+        score.time = 9;
+        expect(score.convertTime()).toEqual('0:09');
     });
 });

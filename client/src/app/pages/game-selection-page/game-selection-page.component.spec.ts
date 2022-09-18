4b7623e2d7f039d09855de915a634f0d916ac400
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-18 13:10:10 -0400

    feat: select game component tests

--- client/src/app/pages/game-selection-page/game-selection-page.component.spec.ts
+++ client/src/app/pages/game-selection-page/game-selection-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { GameSelectionPageComponent } from './game-selection-page.component';
 
@@ -8,9 +9,9 @@ describe('GameSelectionPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameSelectionPageComponent ]
-    })
-    .compileComponents();
+      declarations: [ GameSelectionPageComponent ],
+      imports: [MatDialogModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GameSelectionPageComponent);
     component = fixture.componentInstance;
@@ -19,5 +20,47 @@ describe('GameSelectionPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open dialog on click create game', () => {
+    const spy = spyOn(component["matDialog"], 'open');
+    component.onSelectCreateGame();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should open dialog on click play game', () => {
+    const spy = spyOn(component["matDialog"], 'open');
+    component.onSelectPlayGame();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('GameSelectionService showNextFour() should be called when clicking next button', () => {
+    const spy = spyOn(component["GameSelectionService"], 'showNextFour');
+    component.onClickNext();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('GameSelectionService showPreviousFour() should be called when clicking previous button', () => {
+    const spy = spyOn(component["GameSelectionService"], 'showPreviousFour');
+    component.onClickPrevious();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('GameSelectionService hasPreviousCards() should be called to disable previous button', () => {
+    const spy = spyOn(component["GameSelectionService"], 'hasPreviousCards');
+    component.hasCardsBefore();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('GameSelectionService hasNextCards() should be called to disable next button', () => {
+    const spy = spyOn(component["GameSelectionService"], 'hasNextCards');
+    component.hasCardsAfter();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('GameSelectionService getActiveCards() should be called to get the card to the component', () => {
+    const spy = spyOn(component["GameSelectionService"], 'getActiveCards');
+    component.getGameCards();
+    expect(spy).toHaveBeenCalled();
   });
 });

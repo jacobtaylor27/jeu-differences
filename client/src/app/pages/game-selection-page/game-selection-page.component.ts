e7f75a70432db1ce69320c34f9379a063907bc3d
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-18 19:19:09 -0400

    fix: changing the name of the methodes used so that all the tests passes

--- client/src/app/pages/game-selection-page/game-selection-page.component.ts
+++ client/src/app/pages/game-selection-page/game-selection-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { GameCard } from '@app/interfaces/game-card';
 import { GameSelectionService } from '@app/services/game-selection.service';

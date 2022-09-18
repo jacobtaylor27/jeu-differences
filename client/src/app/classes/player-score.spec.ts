e440e833c9cca51258f237b95c96faef2f4c1581
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-18 19:50:34 -0400

    feat: adding a test to that player-score can have a 100% coverage

--- client/src/app/classes/player-score.spec.ts
+++ client/src/app/classes/player-score.spec.ts
@@ -15,4 +15,13 @@ describe('Score', () => {
         const playerScore = new PlayerScore(playersName, time, GameCategory.Solo);
         expect(playerScore).toBeTruthy();
     });
+
+    it('The methodes should make the attributes accessible', () => {
+        const time = 3;
+        const name = 'Arthur';
+        const playerScore = new PlayerScore(name, time, GameCategory.Solo);
+        expect(playerScore.getName()).toEqual(name);
+        expect(playerScore.getCategory()).toBe(GameCategory.Solo);
+        expect(playerScore.getTime()).toEqual(time);
+    });
 });

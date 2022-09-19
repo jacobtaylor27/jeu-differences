f8dfc11ea9dc81668d783cdce1739ab6b045c93b
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-19 18:26:22 -0400

    feat: admin page layout

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameCard } from '@app/interfaces/game-card';
+import { GameSelectionService } from '@app/services/game-selection.service';
 
 @Component({
     selector: 'app-admin-page',
     templateUrl: './admin-page.component.html',
     styleUrls: ['./admin-page.component.scss'],
 })
-export class AdminPageComponent {}
+export class AdminPageComponent implements OnInit {
+    constructor(readonly gameSelectionService: GameSelectionService) {}
+
+    favoriteTheme: string = 'deeppurple-amber-theme';
+    gameCards: GameCard[] = [];
+
+    ngOnInit(): void {
+        this.getGameCards();
+    }
+
+    getGameCards(): void {
+        this.gameCards = this.gameSelectionService.getActiveCards();
+    }
+}

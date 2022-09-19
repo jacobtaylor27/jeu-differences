f867871d3c8ab75786721efefc66d6eba7c4413e
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-19 17:43:45 -0400

    feat: carousel and game-card implementation

--- client/src/app/components/game-card/game-card.component.ts
+++ client/src/app/components/game-card/game-card.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class GameCardComponent {
   constructor() { }
+
+  favoriteTheme: string = 'deeppurple-amber-theme';
 }
--- client/src/app/components/game-carousel/game-carousel.component.ts
+++ client/src/app/components/game-carousel/game-carousel.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class GameCarouselComponent {
   constructor() { }
+
+  favoriteTheme: string = 'deeppurple-amber-theme';
 }

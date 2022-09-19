ac7942a6bfccf746a081109de9a4f4fef2128331
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-19 00:48:34 -0400

    feat: adding a verification to see if the pixels are weither black or white

--- server/app/classes/difference-interpreter/difference-interpreter.ts
+++ server/app/classes/difference-interpreter/difference-interpreter.ts
@@ -1,8 +1,12 @@
 import { Bmp } from '@app/classes/bmp/bmp';
-import { MAX_VALUE_PIXEL } from '@app/constants/encoding';
+import { MAX_VALUE_PIXEL, MIN_VALUE_PIXEL } from '@app/constants/encoding';
 import { Pixel } from '@app/interface/pixel';
 export class DifferenceInterpreter {
     static getDifference(bmpDifferentiated: Bmp) {
+        if (!this.isBmpDifferentiated(bmpDifferentiated)) {
+            throw new Error('The pixels are not perfectly black or perfectly white');
+        }
+
         let maxRegion = 0;
 
         const pixels = bmpDifferentiated.getPixels();
@@ -21,7 +25,7 @@ export class DifferenceInterpreter {
         if (row < 0 || column < 0 || row >= pixels.length || column >= pixels[row].length) {
             return 0;
         }
-        if (!this.isPixelWhite(pixels[row][column])) {
+        if (this.isPixelBlack(pixels[row][column])) {
             return 0;
         }
         let size = 1;
@@ -35,5 +39,21 @@ export class DifferenceInterpreter {
 
     private static isPixelWhite(pixel: Pixel) {
         return pixel.r === MAX_VALUE_PIXEL && pixel.g === MAX_VALUE_PIXEL && pixel.b === MAX_VALUE_PIXEL;
+    }
+
+    private static isPixelBlack(pixel: Pixel) {
+        return pixel.r === MIN_VALUE_PIXEL && pixel.g === MIN_VALUE_PIXEL && pixel.b === MIN_VALUE_PIXEL;
+    }
+
+    private static isBmpDifferentiated(bmp: Bmp): boolean {
+        const pixels: Pixel[][] = bmp.getPixels();
+        for (const scanLine of pixels) {
+            for (const pixel of scanLine) {
+                if (!this.isPixelBlack(pixel) && !this.isPixelWhite(pixel)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }

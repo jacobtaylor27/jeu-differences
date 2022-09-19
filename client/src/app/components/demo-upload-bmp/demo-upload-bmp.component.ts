9fdc3f7d54194d10801632e166b35a82acc50419
Xavier Jouslin <jouslin@nowray.net>
2022-09-19 18:30:26 -0400

    feat: start to create method to upload image

--- client/src/app/components/demo-upload-bmp/demo-upload-bmp.component.ts
+++ client/src/app/components/demo-upload-bmp/demo-upload-bmp.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
-  selector: 'app-demo-upload-bmp',
-  templateUrl: './demo-upload-bmp.component.html',
-  styleUrls: ['./demo-upload-bmp.component.scss']
+    selector: 'app-demo-upload-bmp',
+    templateUrl: './demo-upload-bmp.component.html',
+    styleUrls: ['./demo-upload-bmp.component.scss'],
 })
-export class DemoUploadBmpComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DemoUploadBmpComponent {
+    img: string;
+    form: FormGroup;
+    constructor() {
+        this.form = new FormGroup({
+            img: new FormControl(null),
+        });
+    }
+    selectFile(event: Event) {
+        const files = (event.target as HTMLInputElement).files;
+        if (files === null) {
+            return;
+        }
+        const file = files[0];
+        if (file.type !== 'image/bmp') {
+            return;
+        }
+    }
 }

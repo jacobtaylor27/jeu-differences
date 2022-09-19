219f498fcb2e1cf29cd320eb515fb448d30a66b1
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-18 20:46:36 -0400

    feat : timer stopwatch works (without clue penalty / difference))

--- client/src/app/components/timer-stopwatch/timer-stopwatch.component.ts
+++ client/src/app/components/timer-stopwatch/timer-stopwatch.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
-  selector: 'app-timer-stopwatch',
-  templateUrl: './timer-stopwatch.component.html',
-  styleUrls: ['./timer-stopwatch.component.scss']
+    selector: 'app-timer-stopwatch',
+    templateUrl: './timer-stopwatch.component.html',
+    styleUrls: ['./timer-stopwatch.component.scss'],
 })
 export class TimerStopwatchComponent implements OnInit {
+    minutesInitial: number = 2;
+    secondsInitial: number = 0;
+    valueTimer: number = 0;
 
-  constructor() { }
+    minutesDisplay: number = 0;
+    secondsDisplay: number = 0;
 
-  ngOnInit(): void {
-  }
+    sub: Subscription;
 
+    ngOnInit(): void {
+        this.startTimer();
+    }
+
+    ngOnDestroy(): void {
+        this.stopTimer();
+    }
+
+    private startTimer() {
+        const time = timer(1, 1000);
+        this.sub = time.subscribe((seconds) => {
+            this.secondsDisplay = this.getSeconds(seconds);
+            this.minutesDisplay = this.getMinutes(seconds);
+        });
+    }
+
+    private stopTimer() {
+        this.sub.unsubscribe();
+    }
+
+    private getSeconds(ticks: number) {
+        return this.pad(ticks % 60);
+    }
+
+    private getMinutes(ticks: number) {
+        return this.pad(Math.floor(ticks / 60) % 60);
+    }
+
+    private pad(digit: any) {
+        return digit <= 9 ? '0' + digit : digit;
+    }
+
+    displayTime(): string {
+        return (
+            (this.minutesDisplay && this.minutesDisplay <= 59 ? this.minutesDisplay : '00') +
+            ' : ' +
+            (this.secondsDisplay && this.secondsDisplay <= 59 ? this.secondsDisplay : '00')
+        );
+    }
 }

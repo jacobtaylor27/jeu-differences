38fb61a593036190af3962b2ce4206bf0df84ad8
Xavier Jouslin <jouslin@nowray.net>
2022-09-19 16:57:37 -0400

    add demo upload bmp component

---
+++ client/src/app/components/demo-upload-bmp/demo-upload-bmp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-upload-bmp',
+  templateUrl: './demo-upload-bmp.component.html',
+  styleUrls: ['./demo-upload-bmp.component.scss']
+})
+export class DemoUploadBmpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

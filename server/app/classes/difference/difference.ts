b3bc1fbffe8fa8f320e2afdc5d2945d1e95f60cc
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-19 00:56:15 -0400

    feat: a difference won't have a pixel before or after. setters might be added latter

--- server/app/classes/difference/difference.ts
+++ server/app/classes/difference/difference.ts
@@ -1,24 +1,13 @@
 import { Coordinate } from '@app/interface/coordinate';
-import { Pixel } from '@app/interface/pixel';
 
 export class Difference {
     private coordinate: Coordinate;
-    private originalPixel: Pixel;
-    private modifiedPixel: Pixel;
 
-    constructor(coordinate: Coordinate, originalPixel: Pixel, modifiedPixel: Pixel) {
+    constructor(coordinate: Coordinate) {
         this.coordinate = coordinate;
-        this.originalPixel = originalPixel;
-        this.modifiedPixel = modifiedPixel;
     }
 
     getCoordinate(): Coordinate {
         return this.coordinate;
-    }
-    getOriginalPixel(): Pixel {
-        return this.originalPixel;
-    }
-    getModifiedPixel(): Pixel {
-        return this.modifiedPixel;
     }
 }

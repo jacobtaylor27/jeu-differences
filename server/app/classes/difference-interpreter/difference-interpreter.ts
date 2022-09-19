60400e4c79dd4cfd67287fbde8ed2777ea56b259
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-19 00:29:50 -0400

    feat: incorporating a basic implementation of depth for search algorithm

--- server/app/classes/difference-interpreter/difference-interpreter.ts
+++ server/app/classes/difference-interpreter/difference-interpreter.ts
@@ -1,7 +1,39 @@
 import { Bmp } from '@app/classes/bmp/bmp';
-
+import { MAX_VALUE_PIXEL } from '@app/constants/encoding';
+import { Pixel } from '@app/interface/pixel';
 export class DifferenceInterpreter {
-    static getDifference(differenceOfBmps: Bmp) {
-        // detects the number of black elements than aren't connected and regroup them into difference objects
+    static getDifference(bmpDifferentiated: Bmp) {
+        let maxRegion = 0;
+
+        const pixels = bmpDifferentiated.getPixels();
+        for (let row = 0; row < bmpDifferentiated.getWidth(); row++) {
+            for (let column = 0; column < bmpDifferentiated.getHeight(); column++) {
+                if (this.isPixelWhite(pixels[row][column])) {
+                    const size = this.getRegionSize(pixels, row, column);
+                    maxRegion = Math.max(size, maxRegion);
+                }
+            }
+        }
+        return maxRegion;
+    }
+
+    private static getRegionSize(pixels: Pixel[][], row: number, column: number): number {
+        if (row < 0 || column < 0 || row >= pixels.length || column >= pixels[row].length) {
+            return 0;
+        }
+        if (!this.isPixelWhite(pixels[row][column])) {
+            return 0;
+        }
+        let size = 1;
+        for (let r = row - 1; r <= row + 1; r++) {
+            for (let c = column - 1; c <= column + 1; c++) {
+                size += this.getRegionSize(pixels, r, c);
+            }
+        }
+        return size;
+    }
+
+    private static isPixelWhite(pixel: Pixel) {
+        return pixel.r === MAX_VALUE_PIXEL && pixel.g === MAX_VALUE_PIXEL && pixel.b === MAX_VALUE_PIXEL;
     }
 }
--- server/app/constants/encoding.ts
+++ server/app/constants/encoding.ts
@@ -1,1 +1,2 @@
 export const PIXEL_DEPT = 4;
+export const MAX_VALUE_PIXEL = 255;

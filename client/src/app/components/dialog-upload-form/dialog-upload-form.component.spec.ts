9525df6b77668c6bf6d23af9fdd9db853c4245b5
Xavier Jouslin <jouslin@nowray.net>
2022-09-20 19:04:17 -0400

    test: add test tp create an image

--- client/src/app/components/dialog-upload-form/dialog-upload-form.component.spec.ts
+++ client/src/app/components/dialog-upload-form/dialog-upload-form.component.spec.ts
@@ -5,10 +5,16 @@ import { DialogUploadFormComponent } from './dialog-upload-form.component';
 describe('DialogUploadFormComponent', () => {
     let component: DialogUploadFormComponent;
     let fixture: ComponentFixture<DialogUploadFormComponent>;
-
+    let toolBoxServiceSpyObj: jasmine.SpyObj<ToolBoxService>;
     beforeEach(async () => {
+        toolBoxServiceSpyObj = jasmine.createSpyObj(
+            'ToolBoxService',
+            [],
+            [{ $uploadImageInDiff: new Subject(), $uploadImageInSource: new Subject() }],
+        );
         await TestBed.configureTestingModule({
             declarations: [DialogUploadFormComponent],
+            providers: [{ provide: ToolBoxService, useValue: toolBoxServiceSpyObj }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(DialogUploadFormComponent);
@@ -19,4 +25,14 @@ describe('DialogUploadFormComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should create a image from a file stream', async () => {
+        const expectedImage = {} as ImageBitmap;
+        const createImageSpy = spyOn(window, 'createImageBitmap').and.resolveTo(expectedImage);
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const image = await component.createImage({ slice: () => {} } as File);
+        expect(createImageSpy).toHaveBeenCalled();
+        expect(image).toEqual(expectedImage);
+    });
+
 });

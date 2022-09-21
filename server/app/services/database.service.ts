3f409ac7bf60b8004d6254f6d4195759759f2cbe
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-20 20:31:49 -0400

    feat: new service to access db

---
+++ server/app/services/database.service.ts
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+import { Service } from 'typedi';
+
+const DB_USERNAME = 'admin';
+const DB_PASSWORD = 'mpwqKiEIeOimdmr5';
+
+const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.7njxiw7.mongodb.net/?retryWrites=true&w=majority`;
+const DB_NAME = 'seven-differences';
+
+@Service()
+export class DatabaseService {
+    private client: MongoClient;
+    private db: any;
+
+    constructor() {}
+
+    async populateDatabase(collectionName:string, data:any): Promise<void> { 
+        const collection = this.client.db(DB_NAME).collection(collectionName);
+        const nbDocuments = await collection.countDocuments();
+
+        if (nbDocuments === 0) {
+            await collection.insertMany(data);
+        }
+    }
+
+    async connectToDatabase(url: string = DB_URL): Promise<void> { 
+        try {
+            this.client = new MongoClient(url);
+            await this.client.connect();
+            this.db = this.client.db(DB_NAME);    
+        } catch (error) { 
+            console.error(error);
+        }
+    }
+}

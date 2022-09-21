9e81cdef9b1be8123b852b5ad6ea3b2a5fe71b49
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-21 16:42:15 -0400

    fix: using the array instead of the width and height of the array

--- server/app/classes/difference-interpreter/difference-interpreter.ts
+++ server/app/classes/difference-interpreter/difference-interpreter.ts
@@ -6,11 +6,13 @@ import { Pixel } from '@app/interface/pixel';
 export class DifferenceInterpreter {
     static getDifference(bmpDifferentiated: Bmp): BmpCoordinate[][] {
         if (!this.isBmpDifferentiated(bmpDifferentiated)) throw new Error('The pixels are not perfectly black or white');
+        
         const differences: BmpCoordinate[][] = [];
         const pixels = bmpDifferentiated.getPixels();
-        for (let row = 0; row < bmpDifferentiated.getWidth(); row++) {
-            for (let column = 0; column < bmpDifferentiated.getHeight(); column++) {
-                if (this.isPixelBlack(pixels[column][row])) {
+
+        for (let row = 0; row < pixels.length; row++) {
+            for (let column = 0; column < pixels[row].length; column++) {
+                if (this.isPixelBlack(pixels[row][column])) {
                     const difference = this.getRegion(pixels, row, column);
                     if (difference.length !== 0) {
                         differences.push(difference);
@@ -25,11 +27,11 @@ export class DifferenceInterpreter {
         if (row < 0 || column < 0 || row >= pixels.length || column >= pixels[row].length) {
             return [];
         }
-        if (this.isPixelWhite(pixels[column][row])) {
+        if (this.isPixelWhite(pixels[row][column])) {
             return [];
         }
         const differences: BmpCoordinate[] = [new BmpCoordinate(row, column)];
-        this.setPixelWhite(pixels[column][row]);
+        this.setPixelWhite(pixels[row][column]);
         for (let r = row - 1; r <= row + 1; r++) {
             for (let c = column - 1; c <= column + 1; c++) {
                 if (r !== row || c !== column) {
@@ -37,6 +39,7 @@ export class DifferenceInterpreter {
                 }
             }
         }
+        console.log(differences);
         return differences;
     }
 

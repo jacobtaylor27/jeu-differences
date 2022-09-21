24a4ff0cd24964214fe90fa3095e4bf7ae80e1a3
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-21 13:50:43 -0400

    feat : create service for timer gestion

---
+++ client/src/app/services/timer.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TimerService {
+    private timer: number;
+    private secondsDisplay: number;
+    private minutesDisplay: number;
+
+    constructor() {}
+
+    setTimer(timer: number) {
+        this.timer = timer;
+    }
+
+    private calculateSeconds(totalSeconds: number) {
+        this.secondsDisplay = this.pad((this.timer - totalSeconds) % 60);
+    }
+    private calculateMinutes(totalSeconds: number) {
+        this.minutesDisplay = this.pad(Math.floor((this.timer - totalSeconds) / 60) % 60);
+    }
+
+    calculateSecondsLeft(totalSeconds: number): number {
+        return this.timer - totalSeconds;
+    }
+
+    displayTime(totalSeconds: number): string {
+        this.calculateSeconds(totalSeconds);
+        this.calculateMinutes(totalSeconds);
+        return (
+            (this.minutesDisplay && this.minutesDisplay <= 59 ? this.minutesDisplay : '00') +
+            ' : ' +
+            (this.secondsDisplay && this.secondsDisplay <= 59 ? this.secondsDisplay : '00')
+        );
+    }
+
+    private pad(digit: any) {
+        return digit <= 9 ? '0' + digit : digit;
+    }
+}

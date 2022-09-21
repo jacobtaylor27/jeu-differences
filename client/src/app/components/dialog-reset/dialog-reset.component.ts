50fdab057d68bb460b54ceba56792a0137f75c4f
Xavier Jouslin <jouslin@nowray.net>
2022-09-20 23:39:43 -0400

    feat: create a form to reset image and canvas

--- client/src/app/components/dialog-reset/dialog-reset.component.ts
+++ client/src/app/components/dialog-reset/dialog-reset.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToolBoxService } from '@app/services/tool-box/tool-box.service';
 
 @Component({
-  selector: 'app-dialog-reset',
-  templateUrl: './dialog-reset.component.html',
-  styleUrls: ['./dialog-reset.component.scss']
+    selector: 'app-dialog-reset',
+    templateUrl: './dialog-reset.component.html',
+    styleUrls: ['./dialog-reset.component.scss'],
 })
-export class DialogResetComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class DialogResetComponent {
+    form: FormGroup;
+    constructor(private toolService: ToolBoxService) {
+        this.form = new FormGroup({
+            reset: new FormControl('', Validators.required),
+        });
+    }
 
 }

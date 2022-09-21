d5d07c600d161d872baf73cd42a833bc5f220069
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-20 22:17:20 -0400

    feat: name input on game cards

--- client/src/app/components/user-name-input/user-name-input.component.ts
+++ client/src/app/components/user-name-input/user-name-input.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-name-input',
   templateUrl: './user-name-input.component.html',
   styleUrls: ['./user-name-input.component.scss']
 })
-export class UserNameInputComponent implements OnInit {
-
+export class UserNameInputComponent {
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  favoriteTheme: string = 'deeppurple-amber-theme';
+  name:string;
 }
--- client/src/app/services/game-card.service.ts
+++ client/src/app/services/game-card.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UserNameInputComponent } from '@app/components/user-name-input/user-name-input.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameCardService {
-  constructor() { }
+  constructor(private readonly matDialog: MatDialog) { }
 
-  openUseNameInputDialog(): void {}
+  openUseNameInputDialog(): void {
+    this.matDialog.open(UserNameInputComponent);
+  }
 }

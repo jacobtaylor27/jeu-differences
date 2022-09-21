5295f4e4529aede6a301c2d656600141e83b394f
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-21 16:11:09 -0400

    feat: adding a testing image

--- server/app/classes/difference-interpreter/difference-interpreter.spec.ts
+++ server/app/classes/difference-interpreter/difference-interpreter.spec.ts
@@ -1,10 +1,11 @@
 import { BmpCoordinate } from '@app/classes/bmp-coordinate/bmp-coordinate';
+import { BmpDecoder } from '@app/classes/bmp-decoder/bmp-decoder';
 import { Bmp } from '@app/classes/bmp/bmp';
 import { DifferenceInterpreter } from '@app/classes/difference-interpreter/difference-interpreter';
 import { expect } from 'chai';
 import { describe } from 'mocha';
 
-describe('Difference interpreter', () => {
+describe('Difference interpreter', async () => {
     it('Should throw an exception if given a bmp with pixels other than black or white', () => {
         // prettier-ignore
         // eslint-disable-next-line
@@ -68,16 +69,12 @@ describe('Difference interpreter', () => {
         expect(coordinates.length).to.equal(nbOfDifference);
     });
 
-    it('The algorithm should also work on a bmp with a large width and height', () => {
-        // prettier-ignore
-        // eslint-disable-next-line
-        const rawData = [0, 255, 255, 255, 0, 0, 0, 0, 0, 255, 255, 255, 0, 0, 0, 0];
-        const width = 2;
-        const height = 2;
-        const bmpWithColors = new Bmp(width, height, rawData);
-        const nbOfDifference = 1;
-
-        const coordinates: BmpCoordinate[][] = DifferenceInterpreter.getCoordinates(bmpWithColors);
-        expect(coordinates.length).to.equal(nbOfDifference);
+    it('The algorithm should also work on a bmp with a large width and height', async () => {
+        const filepath = './assets/test-bmp/ten_difference.bmp';
+        const bmpDecoded = await BmpDecoder.decode(filepath);
+        const nbOfDifference = 10;
+        const differences: BmpCoordinate[][] = DifferenceInterpreter.getCoordinates(bmpDecoded);
+        console.log(differences);
+        expect(differences.length).to.equal(nbOfDifference);
     });
 });
--- server/app/index.ts
+++ server/app/index.ts
@@ -3,6 +3,15 @@ import 'reflect-metadata';
 import 'module-alias/register';
 import { Server } from '@app/server';
 import { Container } from 'typedi';
+import { BmpDecoder } from '@app/classes/bmp-decoder/bmp-decoder';
+import { BmpCoordinate } from '@app/classes/bmp-coordinate/bmp-coordinate';
+import { DifferenceInterpreter } from '@app/classes/difference-interpreter/difference-interpreter';
+
+const filepath = './assets/test-bmp/ten_difference.bmp';
+BmpDecoder.decode(filepath).then((bmpDecoded) => {
+    const differences: BmpCoordinate[][] = DifferenceInterpreter.getCoordinates(bmpDecoded);
+    console.log(differences);
+})
 
 const server: Server = Container.get(Server);
 server.init();

8f08016e7bb854176062b09ebf2f05771bc67489
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-22 19:08:09 -0400

    feat : added tests for game-page

--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -1,15 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
 import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+import { CommunicationService } from '@app/services/communication.service';
+import { of } from 'rxjs';
 import { GamePageComponent } from './game-page.component';
+import SpyObj = jasmine.SpyObj;
 
 describe('GamePageComponent', () => {
     let component: GamePageComponent;
     let fixture: ComponentFixture<GamePageComponent>;
+    let communicationServiceSpy: SpyObj<CommunicationService>;
 
     beforeEach(async () => {
+        communicationServiceSpy = jasmine.createSpyObj('ExampleService', ['getTimeValue']);
+        communicationServiceSpy.getTimeValue.and.returnValue(of({ title: '', body: '' }));
+
         await TestBed.configureTestingModule({
             declarations: [GamePageComponent, SidebarComponent, PlayAreaComponent],
+            imports: [RouterTestingModule, HttpClientModule],
+            providers: [{ provide: CommunicationService, useValue: communicationServiceSpy }],
         }).compileComponents();
     });
 
@@ -21,5 +32,10 @@ describe('GamePageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should call getTime when page is loaded', () => {
+        component.getTimerValue();
+        expect(communicationServiceSpy.getTimeValue).toHaveBeenCalled();
     });
 });

7f03c39747fabf7178e97f45e520f835677d3fac
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-23 00:47:02 -0400

    fix : delete constructor and on init

--- client/src/app/components/differences-area/differences-area.component.ts
+++ client/src/app/components/differences-area/differences-area.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-differences-area',
-  templateUrl: './differences-area.component.html',
-  styleUrls: ['./differences-area.component.scss']
+    selector: 'app-differences-area',
+    templateUrl: './differences-area.component.html',
+    styleUrls: ['./differences-area.component.scss'],
 })
-export class DifferencesAreaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class DifferencesAreaComponent {}

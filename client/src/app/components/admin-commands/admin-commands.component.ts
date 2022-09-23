250d2fd121d4775ca661eedcbd54afc4d76bd7a6
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-23 10:47:00 -0400

    feat: deleteGames and resetScores event handle

--- client/src/app/components/admin-commands/admin-commands.component.ts
+++ client/src/app/components/admin-commands/admin-commands.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { GameCard } from '@app/interfaces/game-card';
 import { AdminService } from '@app/services/admin.service';
+import { GameConstantsSettingsComponent } from '../game-constants-settings/game-constants-settings.component';
 
 @Component({
   selector: 'app-admin-commands',
@@ -7,17 +10,24 @@ import { AdminService } from '@app/services/admin.service';
   styleUrls: ['./admin-commands.component.scss']
 })
 export class AdminCommandsComponent {
-  constructor(private readonly adminService: AdminService) { }
-
   favoriteTheme: string = 'deeppurple-amber-theme';
 
+  constructor(private readonly matDialog: MatDialog, private readonly adminService: AdminService) { }
+
+  getGameCards(): GameCard[] {
+    return this.adminService.GameCards;
+  }
+
+
   onClickModifySettings(): void {
-    this.adminService.openGameSettingsDialog();
+    this.matDialog.open(GameConstantsSettingsComponent);
   }
 
   onClickDeleteGames(): void {
+    this.adminService.deleteAllGames();
   }
 
   onClickResetHighScores(): void {
+    this.adminService.resetAllHighScores();
   }
 }

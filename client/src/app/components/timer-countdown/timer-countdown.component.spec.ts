ed566d31c929f6010b86042cbf2f8fa43894e961
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-22 21:45:41 -0400

    feat : added MatDialog to the test

--- client/src/app/components/timer-countdown/timer-countdown.component.spec.ts
+++ client/src/app/components/timer-countdown/timer-countdown.component.spec.ts
@@ -1,23 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { TimerCountdownComponent } from './timer-countdown.component';
 
 describe('TimerCountdownComponent', () => {
-  let component: TimerCountdownComponent;
-  let fixture: ComponentFixture<TimerCountdownComponent>;
+    let component: TimerCountdownComponent;
+    let fixture: ComponentFixture<TimerCountdownComponent>;
+    const model = {};
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TimerCountdownComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TimerCountdownComponent],
+            imports: [MatDialogModule],
+            providers: [{ provide: MAT_DIALOG_DATA, useValue: model }],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(TimerCountdownComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TimerCountdownComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        component.ngOnInit();
+        expect(component).toBeTruthy();
+    });
 });

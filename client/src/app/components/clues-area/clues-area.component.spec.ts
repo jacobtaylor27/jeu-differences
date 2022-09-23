43ed516a92c740d8de218aead4d7607bd305e1ee
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-22 20:11:05 -0400

    feat : added test c;lue is asked when i is pressed

--- client/src/app/components/clues-area/clues-area.component.spec.ts
+++ client/src/app/components/clues-area/clues-area.component.spec.ts
@@ -3,21 +3,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CluesAreaComponent } from './clues-area.component';
 
 describe('CluesAreaComponent', () => {
-  let component: CluesAreaComponent;
-  let fixture: ComponentFixture<CluesAreaComponent>;
+    let component: CluesAreaComponent;
+    let fixture: ComponentFixture<CluesAreaComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CluesAreaComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CluesAreaComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(CluesAreaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(CluesAreaComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should ask for clue when i keyboard is pressed', () => {
+        const getClueSpy = spyOn(component, 'getClue');
+        const expectedKey = 'i';
+        const buttonEvent = {
+            key: expectedKey,
+        } as KeyboardEvent;
+        component.buttonDetect(buttonEvent);
+        expect(getClueSpy).toHaveBeenCalled();
+    });
+
 });

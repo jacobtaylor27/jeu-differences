7b8c8193043b821655181db517c595f582ff8f1a
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-23 10:56:44 -0400

    feat: methods to navigate through carousel

--- client/src/app/components/game-carousel/game-carousel.component.ts
+++ client/src/app/components/game-carousel/game-carousel.component.ts
@@ -1,14 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { GameCard } from '@app/interfaces/game-card';
+import { GameCarouselService } from '@app/services/game-carousel.service';
 
 @Component({
   selector: 'app-game-carousel',
   templateUrl: './game-carousel.component.html',
   styleUrls: ['./game-carousel.component.scss']
 })
-export class GameCarouselComponent {
-  constructor() { }
-
+export class GameCarouselComponent implements OnInit{
   favoriteTheme: string = 'deeppurple-amber-theme';
   @Input() gameCards: GameCard[] = [];
+
+  constructor(private readonly gameCarouselService: GameCarouselService) {}
+
+  ngOnInit(): void {
+    this.gameCards = this.gameCarouselService.getCards();
+    this.resetStartingRange();
+  }
+
+  resetStartingRange(): void {
+    this.gameCarouselService.resetRange();
+  }
+
+  onClickPrevious(): void {
+    this.gameCarouselService.showPreviousFour();
+  }
+
+  onClickNext(): void {
+    this.gameCarouselService.showNextFour();
+  }
+
+  hasCardsBefore(): boolean {
+    return this.gameCarouselService.hasPreviousCards();
+  }
+
+  hasCardsAfter(): boolean {
+    return this.gameCarouselService.hasNextCards();
+  }
 }

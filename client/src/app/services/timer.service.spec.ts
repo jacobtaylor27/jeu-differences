68f78e05b9d06c90e6b0d65e4ce60e09b6ebe579
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-22 22:26:51 -0400

    feat : timer service calculate and display tests

--- client/src/app/services/timer.service.spec.ts
+++ client/src/app/services/timer.service.spec.ts
@@ -3,14 +3,23 @@ import { TestBed } from '@angular/core/testing';
 import { TimerService } from './timer.service';
 
 describe('TimerService', () => {
-  let service: TimerService;
+    let service: TimerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TimerService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(TimerService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should display time with format mm : ss', () => {
+        let timeSeconds = 65;
+        let timeDisplayExpected = '01 : 05';
+        expect(service.displayTime(timeSeconds)).toEqual(timeDisplayExpected);
+        timeSeconds = 1;
+        timeDisplayExpected = '00 : 01';
+        expect(service.displayTime(timeSeconds)).toEqual(timeDisplayExpected);
+    });
 });

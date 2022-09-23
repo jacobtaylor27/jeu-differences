9d545acd999804e70746ebc37d3a5e3b88a58dbf
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-23 11:24:57 -0400

    feat: delete card, reset score

--- client/src/app/services/game-card.service.ts
+++ client/src/app/services/game-card.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { UserNameInputComponent } from '@app/components/user-name-input/user-name-input.component';
+import { GameCard } from '@app/interfaces/game-card';
+import { GameCardHandlerService } from './game-card-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameCardService {
-  constructor(private readonly matDialog: MatDialog) { }
+  constructor(private readonly gameCardHandlerService: GameCardHandlerService) { }
 
-  openUseNameInputDialog(): void {
-    this.matDialog.open(UserNameInputComponent);
+  deleteGame(game: GameCard) {
+    this.gameCardHandlerService.deleteGame(game);
+  }
+
+  resetHighScores(game: GameCard) {
+    this.gameCardHandlerService.resetHighScores(game);
   }
 }

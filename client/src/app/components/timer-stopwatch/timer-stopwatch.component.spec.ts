ccc251426d51b55628cd46da24561b719baf8838
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-22 21:37:24 -0400

    feat : test to be sure timer starts at 00:00

--- client/src/app/components/timer-stopwatch/timer-stopwatch.component.spec.ts
+++ client/src/app/components/timer-stopwatch/timer-stopwatch.component.spec.ts
@@ -1,23 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, discardPeriodicTasks, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { TimerStopwatchComponent } from './timer-stopwatch.component';
 
 describe('TimerStopwatchComponent', () => {
-  let component: TimerStopwatchComponent;
-  let fixture: ComponentFixture<TimerStopwatchComponent>;
+    let component: TimerStopwatchComponent;
+    let fixture: ComponentFixture<TimerStopwatchComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TimerStopwatchComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TimerStopwatchComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(TimerStopwatchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TimerStopwatchComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should start at 00 : 00', fakeAsync(() => {
+        component.ngOnInit();
+        tick(1);
+        expect(component.timerDisplay).toEqual('00 : 00');
+    }));
+
 });

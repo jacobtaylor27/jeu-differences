53e65356a2dfec00984eac41cd265823de509687
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-23 11:27:41 -0400

    feat: game carousel manipulation

--- client/src/app/services/game-carousel.service.ts
+++ client/src/app/services/game-carousel.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { GameCard } from '@app/interfaces/game-card';
+import { GameCardHandlerService } from './game-card-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameCarouselService {
+  constructor(private readonly gameCardHandlerService: GameCardHandlerService) {}
 
-  constructor() { }
+  getCards(): GameCard[] {
+    return this.gameCardHandlerService.GameCards;
+  }
+
+  resetRange(): void {
+    this.gameCardHandlerService.resetActiveRange();
+  }
+
+  setCardMode(makeAdmin: boolean = false): void {
+    this.gameCardHandlerService.GameCards.forEach((gameCard) => {
+      gameCard.isAdminCard = makeAdmin;
+    });
+  }
+
+  hasPreviousCards(): boolean {
+    return this.gameCardHandlerService.activeCardsRange.start > 0;
+  }
+
+  hasNextCards(): boolean {
+    return this.gameCardHandlerService.activeCardsRange.end < this.gameCardHandlerService.GameCards.length - 1;
+  }
+
+  showPreviousFour(): void {
+    this.gameCardHandlerService.decreaseActiveRange();
+    this.gameCardHandlerService.setActiveCards(this.gameCardHandlerService.activeCardsRange.start, this.gameCardHandlerService.activeCardsRange.end);
+  }
+
+  showNextFour(): void {
+    this.gameCardHandlerService.increaseActiveRange();
+    this.gameCardHandlerService.setActiveCards(this.gameCardHandlerService.activeCardsRange.start, this.gameCardHandlerService.activeCardsRange.end);
+  }
 }

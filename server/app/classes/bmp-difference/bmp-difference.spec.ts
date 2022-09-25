d0f8d53968ec84b21d96dc47007847cd03d4dded
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-09-25 11:45:05 -0400

    fix : making methods private and changing tests

--- server/app/classes/bmp-difference/bmp-difference.spec.ts
+++ server/app/classes/bmp-difference/bmp-difference.spec.ts
@@ -1,6 +1,6 @@
 import { BmpDecoder } from '@app/classes/bmp-decoder/bmp-decoder';
-import { Coordinates } from '@app/interface/coordinates';
-import { Pixel } from '@app/interface/pixel';
+// import { Coordinates } from '@app/interface/coordinates';
+// import { Pixel } from '@app/interface/pixel';
 import { expect } from 'chai';
 import { describe } from 'mocha';
 import { BmpDifference } from './bmp-difference';
@@ -20,30 +20,20 @@ describe(' DifferenceBetween2Images', () => {
         ];
         const filepath1 = './assets/test-bmp/bmp_test_2x2.bmp';
         const filepath2 = './assets/test-bmp/bmp_test_2x2.bmp';
+        const radius = 0;
 
-        const bmpProduced = await BmpDifference.getDifference(filepath1, filepath2);
+        const bmpProduced = await BmpDifference.getDifference(filepath1, filepath2, radius);
 
         expect(bmpProduced.getWidth()).to.equals(expectedWidth);
         expect(bmpProduced.getHeight()).to.equals(expectedHeight);
         expect(bmpProduced.getPixels()).to.eql(pixelsExpected);
     });
-    it('both images should have the same height', async () => {
-        const result = false;
-        const originalImageHeight = 3;
-        const modifiedImageHeight = 2;
-        expect(BmpDifference.isHeightEqual(originalImageHeight, modifiedImageHeight)).to.equal(result);
-    });
-    it('both images should have the same Width', async () => {
-        const result = false;
-        const originalImageWidth = 3;
-        const modifiedImageWidth = 2;
-        expect(BmpDifference.isWidthEqual(originalImageWidth, modifiedImageWidth)).to.equal(result);
-    });
     it('Should throw an error if the height of the two images is not the same', async () => {
         const filePathOriginalBmp = './assets/test-bmp/bmp-test_2x3.bmp';
         const filePathModifiedBmp = './assets/test-bmp/bmp-test_2x2.bmp';
+        const radius = 0;
         try {
-            const bmpProduced = await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp);
+            const bmpProduced = await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp, radius);
             expect(bmpProduced).to.equals(undefined);
         } catch (e) {
             expect(e).to.be.instanceof(Error);
@@ -52,8 +42,42 @@ describe(' DifferenceBetween2Images', () => {
     it('Should throw an error if the width of the two images is not the same', async () => {
         const filePathOriginalBmp = './assets/test-bmp/bmp-test_3x2.bmp';
         const filePathModifiedBmp = './assets/test-bmp/bmp-test_2x2.bmp';
+        const radius = 0;
         try {
-            const bmpProduced = await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp);
+            const bmpProduced = await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp, radius);
+            expect(bmpProduced).to.equals(undefined);
+        } catch (e) {
+            expect(e).to.be.instanceof(Error);
+        }
+    });
+    it('Should throw an error if the original path is not valid', async () => {
+        const invalidPath = '';
+        const filePathModifiedBmp = './assets/test-bmp/bmp-test_2x2.bmp';
+        const radius = 0;
+        try {
+            const bmpProduced = await BmpDifference.getDifference(invalidPath, filePathModifiedBmp, radius);
+            expect(bmpProduced).to.equals(undefined);
+        } catch (e) {
+            expect(e).to.be.instanceof(Error);
+        }
+    });
+    it('Should throw an error if the modified path is not valid', async () => {
+        const filePathOriginalBmp = './assets/test-bmp/bmp-test_2x2.bmp';
+        const invalidPath = '';
+        const radius = 0;
+        try {
+            const bmpProduced = await BmpDifference.getDifference(filePathOriginalBmp, invalidPath, radius);
+            expect(bmpProduced).to.equals(undefined);
+        } catch (e) {
+            expect(e).to.be.instanceof(Error);
+        }
+    });
+    it('Should throw an error if the file is not a bitmap', async () => {
+        const filePathOriginalBmp = './assets/test-bmp/bmp-test_2x2.bmp';
+        const filepath = './assets/test-bmp/jpg_test.jpg';
+        const radius = 0;
+        try {
+            const bmpProduced = await BmpDifference.getDifference(filePathOriginalBmp, filepath, radius);
             expect(bmpProduced).to.equals(undefined);
         } catch (e) {
             expect(e).to.be.instanceof(Error);
@@ -80,165 +104,48 @@ describe(' DifferenceBetween2Images', () => {
         expect(bmpProduced.getHeight()).to.equals(expectedHeight);
         expect(bmpProduced.getPixels()).to.eql(pixelsExpected);
     });
-    it('verifying that 2 pixels have the same properties', async () => {
-        const result = true;
-        const originalPixel: Pixel = { a: 0, r: 255, g: 255, b: 255 };
-        const modifiedPixel: Pixel = { a: 0, r: 255, g: 255, b: 255 };
-        expect(BmpDifference.arePixelsEqual(originalPixel, modifiedPixel)).to.equal(result);
-    });
-    it('transforming a random pixel into a white one', async () => {
-        const result: Pixel = { a: 0, r: 255, g: 255, b: 255 };
-        const originalPixel: Pixel = { a: 0, r: 128, g: 0, b: 128 };
-        expect(BmpDifference.setPixelWhite(originalPixel)).to.eql(result);
-    });
-    it('transforming a random pixel into a black one', async () => {
-        const result: Pixel = { a: 0, r: 0, g: 0, b: 0 };
-        const originalPixel: Pixel = { a: 0, r: 128, g: 0, b: 128 };
-        expect(BmpDifference.setPixelBlack(originalPixel)).to.eql(result);
+    it('verifying that the value of radius is greater or equal to zero', async () => {
+        const filePathOriginalBmp = './assets/test-bmp/bmp-test_2x2.bmp';
+        const filePathModifiedBmp = './assets/test-bmp/bmp-test_2x2.bmp';
+        const radius = -1;
+        try {
+            const bmpProduced = await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp, radius);
+            expect(bmpProduced).to.equals(undefined);
+        } catch (e) {
+            expect(e).to.be.instanceof(Error);
+        }
     });
     it('Should produce a difference between two normal size images ', async () => {
         const filePathOriginalBmp = './assets/test-bmp/test_bmp_original.bmp';
         const filePathModifiedBmp = './assets/test-bmp/test_bmp_modified.bmp';
+        const radius = 0;
         const filePathExpectedBmp = './assets/test-bmp/test-expected-difference-0px.bmp';
-        const difference = await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp);
+        const difference = await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp, radius);
         const expectedDifference = await BmpDecoder.decode(filePathExpectedBmp);
         expect(difference).to.be.eql(expectedDifference);
     });
-    it('Should return an array of coordinates with all the black pixels in a given image ', async () => {
-        const filePathBmp = './assets/test-bmp/test-white-image-with-one-black-pixel.bmp';
-        const expectedCoord: Coordinates[] = [{ x: 6, y: 6 }];
-        const bmpImage = await BmpDecoder.decode(filePathBmp);
-        const blackPixelsCoordinates: Coordinates[] = BmpDifference.getBlackPixelsFromOriginalImage(bmpImage);
-        expect(blackPixelsCoordinates).to.be.eql(expectedCoord);
-    });
-    it('Should apply  3 pixel enlargement radius  for a given point ', async () => {
-        const radius = 3;
-        const center: Coordinates = { x: 5, y: 5 };
-        const expectedCoord: Coordinates[] = [
-            { x: 2, y: 5 },
-            { x: 3, y: 3 },
-            { x: 3, y: 4 },
-            { x: 3, y: 5 },
-            { x: 3, y: 6 },
-            { x: 3, y: 7 },
-            { x: 4, y: 3 },
-            { x: 4, y: 4 },
-            { x: 4, y: 5 },
-            { x: 4, y: 6 },
-            { x: 4, y: 7 },
-            { x: 5, y: 2 },
-            { x: 5, y: 3 },
-            { x: 5, y: 4 },
-            { x: 5, y: 5 },
-            { x: 5, y: 6 },
-            { x: 5, y: 7 },
-            { x: 5, y: 8 },
-            { x: 6, y: 3 },
-            { x: 6, y: 4 },
-            { x: 6, y: 5 },
-            { x: 6, y: 6 },
-            { x: 6, y: 7 },
-            { x: 7, y: 3 },
-            { x: 7, y: 4 },
-            { x: 7, y: 5 },
-            { x: 7, y: 6 },
-            { x: 7, y: 7 },
-            { x: 8, y: 5 },
-        ];
-        const result: Coordinates[] = BmpDifference.applyEnlargement(center, radius);
-        expect(expectedCoord).to.be.eql(result);
-    });
-    it('Should apply  3 pixel enlargement radius  for a given array of Coordinates ', async () => {
-        const radius = 3;
-        const arrayCoord: Coordinates[] = [
-            { x: 5, y: 5 },
-            { x: 10, y: 10 },
-        ];
-        const expectedCoord = [
-            { x: 2, y: 5 },
-            { x: 3, y: 3 },
-            { x: 3, y: 4 },
-            { x: 3, y: 5 },
-            { x: 3, y: 6 },
-            { x: 3, y: 7 },
-            { x: 4, y: 3 },
-            { x: 4, y: 4 },
-            { x: 4, y: 5 },
-            { x: 4, y: 6 },
-            { x: 4, y: 7 },
-            { x: 5, y: 2 },
-            { x: 5, y: 3 },
-            { x: 5, y: 4 },
-            { x: 5, y: 5 },
-            { x: 5, y: 6 },
-            { x: 5, y: 7 },
-            { x: 5, y: 8 },
-            { x: 6, y: 3 },
-            { x: 6, y: 4 },
-            { x: 6, y: 5 },
-            { x: 6, y: 6 },
-            { x: 6, y: 7 },
-            { x: 7, y: 3 },
-            { x: 7, y: 4 },
-            { x: 7, y: 5 },
-            { x: 7, y: 6 },
-            { x: 7, y: 7 },
-            { x: 8, y: 5 },
-            { x: 7, y: 10 },
-            { x: 8, y: 8 },
-            { x: 8, y: 9 },
-            { x: 8, y: 10 },
-            { x: 8, y: 11 },
-            { x: 8, y: 12 },
-            { x: 9, y: 8 },
-            { x: 9, y: 9 },
-            { x: 9, y: 10 },
-            { x: 9, y: 11 },
-            { x: 9, y: 12 },
-            { x: 10, y: 7 },
-            { x: 10, y: 8 },
-            { x: 10, y: 9 },
-            { x: 10, y: 10 },
-            { x: 10, y: 11 },
-            { x: 10, y: 12 },
-            { x: 10, y: 13 },
-            { x: 11, y: 8 },
-            { x: 11, y: 9 },
-            { x: 11, y: 10 },
-            { x: 11, y: 11 },
-            { x: 11, y: 12 },
-            { x: 12, y: 8 },
-            { x: 12, y: 9 },
-            { x: 12, y: 10 },
-            { x: 12, y: 11 },
-            { x: 12, y: 12 },
-            { x: 13, y: 10 },
-        ];
-        const result: Coordinates[] = BmpDifference.getCoordinatesAfterEnlargement(arrayCoord, radius);
-        expect(expectedCoord).to.be.eql(result);
-    });
     it('Should apply  3 pixel enlargement radius  for a given image ', async () => {
+        const filePathOriginalBmp = './assets/test-bmp/test_bmp_original.bmp';
+        const filePathModifiedBmp = './assets/test-bmp/test_bmp_modified.bmp';
         const radius = 3;
-        const filePathBmp = './assets/test-bmp/test-expected-difference-0px.bmp';
         const filePathOfTheResultBmp = './assets/test-bmp/test-expected-difference-3px.bmp';
         const expectedBmpImage = await BmpDecoder.decode(filePathOfTheResultBmp);
-        const originalBmp = await BmpDecoder.decode(filePathBmp);
-        expect(expectedBmpImage).to.be.eql(BmpDifference.createBmpWithDifferences(originalBmp, radius));
+        expect(expectedBmpImage).to.be.eql(await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp, radius));
     });
     it('Should apply  9 pixel enlargement radius  for a given image ', async () => {
+        const filePathOriginalBmp = './assets/test-bmp/test_bmp_original.bmp';
+        const filePathModifiedBmp = './assets/test-bmp/test_bmp_modified.bmp';
         const radius = 9;
-        const filePathBmp = './assets/test-bmp/test-expected-difference-0px.bmp';
         const filePathOfTheResultBmp = './assets/test-bmp/test-expected-difference-9px.bmp';
         const expectedBmpImage = await BmpDecoder.decode(filePathOfTheResultBmp);
-        const originalBmp = await BmpDecoder.decode(filePathBmp);
-        expect(expectedBmpImage).to.be.eql(BmpDifference.createBmpWithDifferences(originalBmp, radius));
+        expect(expectedBmpImage).to.be.eql(await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp, radius));
     });
     it('Should apply  15 pixel enlargement radius  for a given image ', async () => {
+        const filePathOriginalBmp = './assets/test-bmp/test_bmp_original.bmp';
+        const filePathModifiedBmp = './assets/test-bmp/test_bmp_modified.bmp';
         const radius = 15;
-        const filePathBmp = './assets/test-bmp/test-expected-difference-0px.bmp';
         const filePathOfTheResultBmp = './assets/test-bmp/test-expected-difference-15px.bmp';
         const expectedBmpImage = await BmpDecoder.decode(filePathOfTheResultBmp);
-        const originalBmp = await BmpDecoder.decode(filePathBmp);
-        expect(expectedBmpImage).to.be.eql(BmpDifference.createBmpWithDifferences(originalBmp, radius));
+        expect(expectedBmpImage).to.be.eql(await BmpDifference.getDifference(filePathOriginalBmp, filePathModifiedBmp, radius));
     });
 });
--- server/app/classes/bmp-difference/bmp-difference.ts
+++ server/app/classes/bmp-difference/bmp-difference.ts
@@ -3,11 +3,10 @@ import { Bmp } from '@app/classes/bmp/bmp';
 import { Coordinates } from '@app/interface/coordinates';
 import { Pixel } from '@app/interface/pixel';
 export class BmpDifference {
-    static async getDifference(originalImagePath: string, modifiedImagePath: string): Promise<Bmp> {
+    static async getDifference(originalImagePath: string, modifiedImagePath: string, radius: number): Promise<Bmp> {
         const originalImage: Bmp = await BmpDecoder.decode(originalImagePath);
         const modifiedImage: Bmp = await BmpDecoder.decode(modifiedImagePath);
         if (!this.areBmpCompatible(originalImage, modifiedImage)) throw new Error('Both images do not have the same height or width');
-
         const resultImage: Bmp = new Bmp(modifiedImage.getWidth(), modifiedImage.getHeight(), Bmp.convertPixelsToRaw(modifiedImage.getPixels()));
         for (let i = 0; i < originalImage.getPixels().length; i++) {
             for (let j = 0; j < originalImage.getPixels()[i].length; j++) {
@@ -18,9 +17,11 @@ export class BmpDifference {
                 }
             }
         }
-        return resultImage;
+        return this.createBmpWithDifferences(resultImage, radius);
     }
-    static createBmpWithDifferences(originalImage: Bmp, radius: number): Bmp {
+    private static createBmpWithDifferences(originalImage: Bmp, radius: number): Bmp {
+        if (radius < 0) throw new Error('radius should be greater or equal to zero');
+        if (radius === 0) return originalImage;
         const resultCoordinates: Coordinates[] = this.getCoordinatesAfterEnlargement(this.getBlackPixelsFromOriginalImage(originalImage), radius);
         const pixelResult: Pixel[][] = originalImage.getPixels();
         resultCoordinates.forEach((coordinate) => {
@@ -29,7 +30,7 @@ export class BmpDifference {
         return new Bmp(originalImage.getWidth(), originalImage.getHeight(), Bmp.convertPixelsToRaw(pixelResult));
     }
 
-    static applyEnlargement(center: Coordinates, radius: number): Coordinates[] {
+    private static applyEnlargement(center: Coordinates, radius: number): Coordinates[] {
         const result: Coordinates[] = [];
         for (let i = -radius; i <= radius; i++) {
             for (let j = -radius; j <= radius; j++) {
@@ -38,7 +39,7 @@ export class BmpDifference {
         }
         return result;
     }
-    static getBlackPixelsFromOriginalImage(differenceBmp: Bmp): Coordinates[] {
+    private static getBlackPixelsFromOriginalImage(differenceBmp: Bmp): Coordinates[] {
         const coordinatesOfBlackPixels: Coordinates[] = [];
         for (let i = 0; i < differenceBmp.getPixels().length; i++) {
             for (let j = 0; j < differenceBmp.getPixels()[i].length; j++) {
@@ -49,7 +50,7 @@ export class BmpDifference {
         }
         return coordinatesOfBlackPixels;
     }
-    static isInsideBoundaries(coordinate: Coordinates, center: Coordinates, radius: number): boolean {
+    private static isInsideBoundaries(coordinate: Coordinates, center: Coordinates, radius: number): boolean {
         if (
             Math.abs(coordinate.x) <= Math.ceil(Math.sqrt(Math.pow(radius, 2) - Math.pow(coordinate.y, 2))) &&
             Math.abs(coordinate.y) <= Math.ceil(Math.sqrt(Math.pow(radius, 2) - Math.pow(coordinate.x, 2)))
@@ -59,7 +60,7 @@ export class BmpDifference {
         return false;
     }
 
-    static getCoordinatesAfterEnlargement(originalCoordinates: Coordinates[], radius: number): Coordinates[] {
+    private static getCoordinatesAfterEnlargement(originalCoordinates: Coordinates[], radius: number): Coordinates[] {
         const resultCoordinates: Coordinates[] = [];
         originalCoordinates.forEach((coordinate) => {
             const result = this.applyEnlargement(coordinate, radius);
@@ -69,14 +70,7 @@ export class BmpDifference {
         });
         return resultCoordinates;
     }
-
-    static isHeightEqual(originalImageHeight: number, modifiedImageHeight: number): boolean {
-        return originalImageHeight === modifiedImageHeight;
-    }
-    static isWidthEqual(originalImageWidth: number, modifiedImageWidth: number): boolean {
-        return originalImageWidth === modifiedImageWidth;
-    }
-    static arePixelsEqual(pixelOriginalImg: Pixel, pixelModifiedImg: Pixel): boolean {
+    private static arePixelsEqual(pixelOriginalImg: Pixel, pixelModifiedImg: Pixel): boolean {
         return (
             pixelOriginalImg.a === pixelModifiedImg.a &&
             pixelOriginalImg.b === pixelModifiedImg.b &&
@@ -84,21 +78,21 @@ export class BmpDifference {
             pixelOriginalImg.r === pixelModifiedImg.r
         );
     }
-    static setPixelWhite(pixel: Pixel): Pixel {
+    private static setPixelWhite(pixel: Pixel): Pixel {
         pixel.a = 0;
         pixel.b = 255;
         pixel.g = 255;
         pixel.r = 255;
         return pixel;
     }
-    static setPixelBlack(pixel: Pixel): Pixel {
+    private static setPixelBlack(pixel: Pixel): Pixel {
         pixel.a = 0;
         pixel.b = 0;
         pixel.g = 0;
         pixel.r = 0;
         return pixel;
     }
-    static isBlackPixel(pixel: Pixel): boolean {
+    private static isBlackPixel(pixel: Pixel): boolean {
         return pixel.a === 0 && pixel.b === 0 && pixel.g === 0 && pixel.r === 0;
     }
     private static areBmpCompatible(originalImage: Bmp, modifiedImage: Bmp): boolean {

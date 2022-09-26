43e066aed1df917fabb0b6db71e744ac096db20f
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-26 16:23:10 -0400

    fix: making vlaidateDifference work

---
+++ common/difference.ts
@@ -0,0 +1,5 @@
+export interface Difference {
+    id: number;
+    nbDifference: number;
+    file: string;
+}
--- server/app/services/game-logic-service/game-logic.service.ts
+++ server/app/services/game-logic-service/game-logic.service.ts
@@ -1,6 +1,9 @@
 import { GameService } from '@app/services/game-service/game.service';
+import { Bmp } from '@common/bmp';
 import { Coordinate } from '@common/coordinate';
+import { Difference } from '@common/difference';
 import { Service } from 'typedi';
+
 @Service()
 export class GameLogicService {
     constructor(private readonly gameService: GameService) {}
@@ -18,5 +21,15 @@ export class GameLogicService {
             }
         }
         return undefined;
+    }
+    async validateDifferenceBmp(originalBmp: Bmp, modifiedBmp: Bmp): Promise<Difference> {
+        console.log(originalBmp);
+        console.log(modifiedBmp);
+        const difference: Difference = {
+            id: 0,
+            nbDifference: 0,
+            file: '',
+        };
+        return difference;
     }
 }

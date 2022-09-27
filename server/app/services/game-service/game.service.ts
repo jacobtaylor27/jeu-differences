e4b7fe0b969739dceedb3c61391aead1b47c71c5
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-27 06:05:26 -0400

    fix: deleting gamecards, it's data duplicate

--- server/app/services/game-service/game.service.ts
+++ server/app/services/game-service/game.service.ts
@@ -1,6 +1,5 @@
 import { DatabaseService } from '@app/services/database-service/database.service';
 import { Game } from '@common/game';
-import { GameCard } from '@common/game-card';
 import { Service } from 'typedi';
 
 @Service()
@@ -23,24 +22,6 @@ export class GameService {
             }
         }
         return undefined;
-    }
-    async getGameCardById(gameId: number): Promise<GameCard | undefined> {
-        const game: Game | undefined = await this.getGameById(gameId);
-        if (game !== undefined) {
-            return this.convertGameIntoGameCard(game);
-        } else {
-            return undefined;
-        }
-    }
-    async getAllGameCards(): Promise<GameCard[]> {
-        const gameCards: GameCard[] = [];
-        const games: Game[] = await this.getAllGames();
-        games.forEach((game) => {
-            this.convertGameIntoGameCard(game).then((gameCard) => {
-                gameCards.push(gameCard);
-            });
-        });
-        return gameCards;
     }
     async addGame(game: Game): Promise<boolean> {
         if (!(await this.doesGameAlreadyExists(game.id))) {
@@ -66,14 +47,5 @@ export class GameService {
             }
         }
         return false;
-    }
-    private async convertGameIntoGameCard(game: Game): Promise<GameCard> {
-        const gameCard: GameCard = {
-            id: game.id,
-            idOriginalBmp: game.idOriginalBmp,
-            bestScores: game.bestScores,
-            name: game.name,
-        };
-        return gameCard;
     }
 }

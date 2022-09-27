02c9fa610bd02b45bc296fba4533a23fc17997b7
Xavier Jouslin <jouslin@nowray.net>
2022-09-27 17:02:35 -0400

    test: validate FindDifferenceState

--- server/app/classes/find-difference-state/find-difference-state.spec.ts
+++ server/app/classes/find-difference-state/find-difference-state.spec.ts
@@ -1,1 +1,26 @@
-describe('FindDifferenceState', () => {});
+import { SinonSpiedInstance, spy } from 'sinon';
+import { GameContext } from '@app/classes/game-context/game-context';
+import { expect } from 'chai';
+import { EndGameState } from '@app/classes/end-game-state/end-game-state';
+import { FindDifferenceState } from './find-difference-state';
+
+describe('PlayerOneTourState', () => {
+    let state: FindDifferenceState;
+    let gameContextSpyObj: SinonSpiedInstance<GameContext>;
+    let gameContext: GameContext;
+    beforeEach(() => {
+        state = new FindDifferenceState();
+        gameContext = new GameContext('', state);
+        gameContextSpyObj = spy(gameContext);
+        state.setContext(gameContext);
+    });
+    it('should get a status', () => {
+        expect(state.status()).to.equal('FindDifference');
+    });
+    it('should go to the next state', () => {
+        const expectedNewState = new EndGameState();
+        state.next();
+        expect(gameContextSpyObj.transitionTo.called).to.equal(true);
+        expect(gameContext.gameState()).to.equal(expectedNewState.status());
+    });
+});

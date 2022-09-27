de6decfcc2bf2bf0c0f8d27a7508fd7e776f8133
Xavier Jouslin <jouslin@nowray.net>
2022-09-27 17:02:00 -0400

    test: validate EndGameState

--- server/app/classes/end-game-state/end-game-state.spec.ts
+++ server/app/classes/end-game-state/end-game-state.spec.ts
@@ -1,1 +1,26 @@
-describe('EndGameState', () => {});
+import { SinonSpiedInstance, spy } from 'sinon';
+import { GameContext } from '@app/classes/game-context/game-context';
+import { expect } from 'chai';
+import { EndGameState } from '@app/classes/end-game-state/end-game-state';
+
+describe('EndGame', () => {
+    let state: EndGameState;
+    let gameContextSpyObj: SinonSpiedInstance<GameContext>;
+    let gameContext: GameContext;
+
+    beforeEach(() => {
+        state = new EndGameState();
+        gameContext = new GameContext('', state);
+        gameContextSpyObj = spy(gameContext);
+        state.setContext(gameContext);
+    });
+
+    it('should get a status', () => {
+        expect(state.status()).to.equal('EndGame');
+    });
+
+    it('should go to the next state', () => {
+        state.next();
+        expect(gameContextSpyObj.transitionTo.called).to.equal(false);
+    });
+});

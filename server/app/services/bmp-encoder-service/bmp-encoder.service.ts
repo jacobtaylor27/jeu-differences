65c12b3fd525e81fa441c76f05387822e8b49590
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-27 19:19:13 -0400

    fix: finding the issue with the awaiting condition

--- server/app/services/bmp-encoder-service/bmp-encoder.service.ts
+++ server/app/services/bmp-encoder-service/bmp-encoder.service.ts
@@ -7,7 +7,7 @@ import { Service } from 'typedi';
 @Service()
 export class BmpEncoderService {
     async encode(filepath: string, bmpObj: Bmp) {
-        if (!this.isFileExtensionValid(filepath)) throw new Error('File extension must be a .bmp');
+        if (!(await this.isFileExtensionValid(filepath))) throw new Error('File extension must be a .bmp');
         const width: number = bmpObj.getWidth();
         const height: number = bmpObj.getHeight();
         const data: Buffer = await this.getBuffer(bmpObj.getPixels());

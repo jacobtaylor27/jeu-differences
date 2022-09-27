717dc811990ff8819cea189f4517ed204d085d28
Xavier Jouslin <jouslin@nowray.net>
2022-09-27 17:03:54 -0400

    test: validate GameState

--- server/app/classes/game-state/game-state.spec.ts
+++ server/app/classes/game-state/game-state.spec.ts
@@ -1,1 +1,27 @@
-describe('GameState', () => {})
+import { GameState } from './game-state';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+import { GameContext } from '@app/classes/game-context/game-context';
+
+class MockGameState extends GameState {
+    next(): void {
+        return;
+    }
+    status(): string {
+        return 'test';
+    }
+}
+
+describe('GameState', () => {
+    let state: GameState;
+
+    beforeEach(() => {
+        state = new MockGameState();
+    });
+
+    it('should set the context', () => {
+        const expectedGameContext = new GameContext('', state);
+        state.setContext(expectedGameContext);
+        expect(state.context).to.equal(expectedGameContext);
+    });
+});

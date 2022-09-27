a970a59b2b954842285bac893fcd78985ab7c8f2
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-27 11:58:09 -0400

    feat: basic implementation of the methodes of gameservice

---
+++ server/app/services/game-logic-service/game-logic.service.ts
@@ -0,0 +1,35 @@
+import { GameService } from '@app/services/game-service/game.service';
+import { Bmp } from '@common/bmp';
+import { Coordinate } from '@common/coordinate';
+import { Difference } from '@common/difference';
+import { Service } from 'typedi';
+
+@Service()
+export class GameLogicService {
+    constructor(private readonly gameService: GameService) {}
+
+    // à la place de renvoyer undefined, renvoyer une erreur
+    async validateCoordinates(gameId: number, coordinate: Coordinate): Promise<Coordinate[] | undefined> {
+        const game = await this.gameService.getGameById(gameId);
+        if (game === undefined) return undefined;
+
+        for (const areaOfDifference of game.differences) {
+            for (const coordDiff of areaOfDifference) {
+                if (coordinate.row === coordDiff.row && coordinate.column === coordDiff.column) {
+                    return areaOfDifference;
+                }
+            }
+        }
+        return undefined;
+    }
+    async validateDifferenceBmp(originalBmp: Bmp, modifiedBmp: Bmp): Promise<Difference> {
+        console.log(originalBmp);
+        console.log(modifiedBmp);
+        const difference: Difference = {
+            id: 0,
+            nbDifference: 0,
+            file: '',
+        };
+        return difference;
+    }
+}

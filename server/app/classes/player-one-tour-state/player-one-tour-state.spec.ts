0b6a409dff433ac35f9be095586ea6c4c5f9e5f3
Xavier Jouslin <jouslin@nowray.net>
2022-09-27 17:06:19 -0400

    test: Validate PlayerOneTourState

--- server/app/classes/player-one-tour-state/player-one-tour-state.spec.ts
+++ server/app/classes/player-one-tour-state/player-one-tour-state.spec.ts
@@ -1,1 +1,29 @@
-describe('PlayerOneTourState', () => {});
+import { SinonSpiedInstance, spy } from 'sinon';
+import { GameContext } from '@app/classes/game-context/game-context';
+import { expect } from 'chai';
+import { PlayerOneTourState } from '@app/classes/player-one-tour-state/player-one-tour-state';
+import { PlayerTwoTourState } from '@app/classes/player-two-tour-state/player-two-tour-state';
+
+describe('PlayerOneTourState', () => {
+    let state: PlayerOneTourState;
+    let gameContextSpyObj: SinonSpiedInstance<GameContext>;
+    let gameContext: GameContext;
+
+    beforeEach(() => {
+        state = new PlayerOneTourState();
+        gameContext = new GameContext('', state);
+        gameContextSpyObj = spy(gameContext);
+        state.setContext(gameContext);
+    });
+
+    it('should get a status', () => {
+        expect(state.status()).to.equal('PlayerOneTour');
+    });
+
+    it('should go to the next state', () => {
+        const expectedNewState = new PlayerTwoTourState();
+        state.next();
+        expect(gameContextSpyObj.transitionTo.called).to.equal(true);
+        expect(gameContext.gameState()).to.equal(expectedNewState.status());
+    });
+});
--- server/app/classes/player-one-tour-state/player-one-tour-state.ts
+++ server/app/classes/player-one-tour-state/player-one-tour-state.ts
@@ -1,8 +1,9 @@
 import { GameState } from '@app/classes/game-state/game-state';
+import { PlayerTwoTourState } from '@app/classes/player-two-tour-state/player-two-tour-state';
 
 export class PlayerOneTourState extends GameState {
     next(): void {
-        this.context.transitionTo(new GameState());
+        this.context.transitionTo(new PlayerTwoTourState());
     }
     status(): string {
         return 'PlayerOneTour';

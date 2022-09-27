f2af3017cff6b42dc7bee094e0ce3aa77837ae0f
Xavier Jouslin <jouslin@nowray.net>
2022-09-27 11:33:12 -0400

    feat: add manager for game state

--- server/app/classes/game-context/game-context.spec.ts
+++ server/app/classes/game-context/game-context.spec.ts
@@ -1,4 +1,4 @@
-describe('', () => {
+describe('GameContext', () => {
     it('should change to the next state', () => {});
 
     it('should get the game state', () => {});
--- server/app/classes/game-context/game-context.ts
+++ server/app/classes/game-context/game-context.ts
@@ -1,11 +1,27 @@
+import { GameState } from '@app/classes/game-state/game-state';
+
 export class GameContext {
-    private state: string;
+    private state: GameState;
     private mode: string;
+
+    constructor(mode: string, state: GameState) {
+        this.state = state;
+        this.mode = mode;
+    }
+
+    get gameMode() {
+        return this.mode;
+    }
+
     next() {
-        return;
+        this.state.next();
+    }
+
+    transitionTo(newState: GameState) {
+        this.state = newState;
     }
 
     gameState(): string {
-        return '';
+        return this.state.status();
     }
 }

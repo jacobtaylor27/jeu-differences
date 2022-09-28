11c300514524acd1ef7c5aafc61aa39cab5b7182
Xavier Jouslin <jouslin@nowray.net>
2022-09-27 20:03:37 -0400

    refactor: add enum to simplify game state

---
+++ server/app/enum/game-status.ts
@@ -0,0 +1,8 @@
+export enum GameStatus {
+    EndGame = 'EndGame',
+    FindDifference = 'FindDifference',
+    InitGame = 'InitGame',
+    InitTimer = 'InitTimer',
+    PlayerOneTour = 'PlayerOneTour',
+    PlayerTwoTour = 'PlayerTwoTour',
+}

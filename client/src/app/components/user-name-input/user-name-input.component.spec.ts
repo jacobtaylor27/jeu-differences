e3caddd84e11b4303a7b9c6b2fa81dfaaf607e4d
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-28 17:14:08 -0400

    fix : added imports to remove warning

--- client/src/app/components/user-name-input/user-name-input.component.spec.ts
+++ client/src/app/components/user-name-input/user-name-input.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from '@app/modules/material.module';
+import { AdminCommandsComponent } from '../admin-commands/admin-commands.component';
 
 import { UserNameInputComponent } from './user-name-input.component';
 
@@ -10,8 +12,8 @@ describe('UserNameInputComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [UserNameInputComponent],
-            imports: [AppMaterialModule, NoopAnimationsModule],
+            declarations: [UserNameInputComponent, AdminCommandsComponent],
+            imports: [AppMaterialModule, NoopAnimationsModule, FormsModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(UserNameInputComponent);

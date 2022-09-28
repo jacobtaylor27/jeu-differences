9c5145e9dec179270203bb68e7bcfa3a2cffd988
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-28 17:08:49 -0400

    fix : imports for warnings in tests

--- client/src/app/components/game-constants-settings/game-constants-settings.component.spec.ts
+++ client/src/app/components/game-constants-settings/game-constants-settings.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppMaterialModule } from '@app/modules/material.module';
+import { GameConstantFieldComponent } from '../game-constant-field/game-constant-field.component';
 
 import { GameConstantsSettingsComponent } from './game-constants-settings.component';
 
-describe('GameConstantsSettingsComponent', () => {
+fdescribe('GameConstantsSettingsComponent', () => {
     let component: GameConstantsSettingsComponent;
     let fixture: ComponentFixture<GameConstantsSettingsComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [GameConstantsSettingsComponent],
+            declarations: [GameConstantsSettingsComponent, GameConstantFieldComponent],
+            imports: [AppMaterialModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(GameConstantsSettingsComponent);

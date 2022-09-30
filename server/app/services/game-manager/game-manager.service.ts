9c7bf520cc6e3cce67512ffa43c806543bc20bb6
Xavier Jouslin <jouslin@nowray.net>
2022-09-29 21:05:39 -0400

    feat: add difference method to check if a new difference is found

--- server/app/services/game-manager/game-manager.service.ts
+++ server/app/services/game-manager/game-manager.service.ts
@@ -3,11 +3,13 @@ import { GameService } from '@app/services/game-info-service/game-info.service';
 // import { Coordinate } from '@common/coordinate';
 import { GameInfo } from '@common/game-info';
 import { Game } from '@app/classes/game/game';
+import { BmpDifferenceInterpreter } from '@app/services/bmp-difference-interpreter-service/bmp-difference-interpreter.service';
+import { Coordinate } from '@common/coordinate';
 
 @Service()
 export class GameManagerService {
     games: Game[] = [];
-    constructor(private gameInfo: GameService) {}
+    constructor(private gameInfo: GameService, public differenceService: BmpDifferenceInterpreter) {}
 
     async createGame(players: string[], mode: string, gameCardId: number) {
         const gameCard: GameInfo = await this.gameInfo.getGameById(gameCardId);
@@ -15,14 +17,22 @@ export class GameManagerService {
         this.games.push(game);
         return game.identifier;
     }
-    isDifference(idGame: number, coord: Coordinate) {
-        // Todo: get the game by id
-        // Todo: get the region of difference
+
+    isDifference(gameId: string, coord?: Coordinate) {
+        if (!this.isGameFound(gameId) || !coord) {
+            return [];
+        }
+        // const game = this.findGame(gameId) as Game;
+        // const difference = this.differenceService.difference(game.information.idDifferenceBmp, coord);
+        // if ( difference.length !== 0) { game.differenceFounded(difference); }
+        // return difference;
+        return [];
     }
 
     isGameFound(gameId: string) {
         return this.findGame(gameId) !== undefined;
     }
+
     isGameOver(gameId: string) {
         return this.isGameFound(gameId) ? this.findGame(gameId)?.isGameOver() : null;
     }

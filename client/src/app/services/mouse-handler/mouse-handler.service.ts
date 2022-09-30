41c7f0866cf5bb9ee08e6ad85fdc4a06a11636b0
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-09-30 13:55:18 -0400

    feat : method for mouseHitdetect

--- client/src/app/services/mouse-handler/mouse-handler.service.ts
+++ client/src/app/services/mouse-handler/mouse-handler.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Vec2 } from '@app/interfaces/vec2';
 
 @Injectable({
     providedIn: 'root',
 })
-export class MouseHandlerService {}
+export class MouseHandlerService {
+    mouseHitDetect(event: MouseEvent): Vec2 {
+        return { x: event.clientX, y: event.clientY };
+    }
+}

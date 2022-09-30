dff65da4db22ded042f59754e8634ca70611ff76
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-09-30 10:13:24 -0400

    fix: making test pass for file manager

--- server/app/services/file-manager-service/file-manager.service.spec.ts
+++ server/app/services/file-manager-service/file-manager.service.spec.ts
@@ -24,4 +24,29 @@ describe('File Manager Service', async () => {
         const dirName = './assets/test-src/bad-directory-name';
         await expect(fileManagerService.getFileNames(dirName)).to.eventually.be.rejectedWith(Error).and.have.property('code', 'ENOENT');
     });
+
+    it("writeFile(path) should throw an exception if the dirName doesn't exist", async () => {
+        const buffer: Buffer = Buffer.from([0, 0, 0]);
+        const dirName = './assets/test-doesnt-exist/test-file.txt';
+        await expect(fileManagerService.writeFile(dirName, buffer)).to.eventually.be.rejectedWith(Error).and.have.property('code', 'ENOENT');
+    });
+
+    it('writeFile(path) should return undefined after having created a file', async () => {
+        const buffer: Buffer = Buffer.from([0, 0, 0]);
+        const dirName = './assets/test-bmp/test-file.txt';
+        await expect(fileManagerService.writeFile(dirName, buffer)).to.eventually.equal(undefined);
+        await fileManagerService.deleteFile(dirName);
+    });
+
+    it('deleteFile(path) should return undefined when deleting a file', async () => {
+        const dirName = './assets/test-bmp/test-file.txt';
+        const buffer: Buffer = Buffer.from([0, 0, 0]);
+        await fileManagerService.writeFile(dirName, buffer);
+        await expect(fileManagerService.deleteFile(dirName)).to.eventually.equal(undefined);
+    });
+
+    it('deleteFile(path) should return undefined', async () => {
+        const dirName = './assets/test-bad/path/test-file.txt';
+        await expect(fileManagerService.deleteFile(dirName)).to.eventually.equal(undefined);
+    });
 });

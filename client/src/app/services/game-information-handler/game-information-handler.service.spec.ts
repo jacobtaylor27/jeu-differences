d28a328780d957fd026154d61d07d02970dbc4a6
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-09-30 11:13:36 -0400

    feat: new game info handler service

---
+++ client/src/app/services/game-information-handler/game-information-handler.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { GameInformationHandlerService } from './game-information-handler.service';
+
+describe('GameInformationHandlerService', () => {
+    let service: GameInformationHandlerService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(GameInformationHandlerService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+});

9831f35db72113669a0f315d21ae77e92b6f1108
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-01 14:43:56 -0400

    fix: adding a class pixel

---
+++ server/app/classes/pixel/pixel.ts
@@ -0,0 +1,41 @@
+import { PIXEL_COLOR } from '@app/constants/pixel-color';
+
+export class Pixel {
+    r: number;
+    g: number;
+    b: number;
+    a: number;
+
+    constructor(r: number, g: number, b: number) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = 0;
+    }
+
+    isWhite() {
+        return this.isColor(PIXEL_COLOR.white);
+    }
+
+    isBlack() {
+        return this.isColor(PIXEL_COLOR.black);
+    }
+
+    setWhite() {
+        this.setColor(PIXEL_COLOR.white);
+    }
+
+    setBlack() {
+        this.setColor(PIXEL_COLOR.white);
+    }
+
+    private isColor(color: number) {
+        return this.r === color && this.g === color && this.b === color;
+    }
+
+    private setColor(color: number) {
+        this.b = color;
+        this.g = color;
+        this.r = color;
+    }
+}

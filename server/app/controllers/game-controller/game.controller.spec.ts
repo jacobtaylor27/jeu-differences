940ba2b81abef389c05fc40550626b3d4df1452c
Xavier Jouslin <jouslin@nowray.net>
2022-10-01 19:44:20 -0400

    test: request validate a game

--- server/app/controllers/game-controller/game.controller.spec.ts
+++ server/app/controllers/game-controller/game.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Application } from '@app/app';
 import { GameService } from '@app/services/game-info-service/game-info.service';
 import { GameManagerService } from '@app/services/game-manager-service/game-manager.service';
+import { GameValidation } from '@app/services/game-validation-service/game-validation.service';
 import { Coordinate } from '@common/coordinate';
 import { GameInfo } from '@common/game-info';
 import { expect } from 'chai';
@@ -9,19 +10,22 @@ import { createStubInstance, SinonStubbedInstance } from 'sinon';
 import * as supertest from 'supertest';
 import { Container } from 'typedi';
 
-describe('GameController', () => {
+describe.only('GameController', () => {
     const expectedGameId = 'test';
     let gameManager: SinonStubbedInstance<GameManagerService>;
     let gameInfo: SinonStubbedInstance<GameService>;
+    let gameValidation: SinonStubbedInstance<GameValidation>;
     let expressApp: Express.Application;
 
     beforeEach(async () => {
         gameManager = createStubInstance(GameManagerService);
         gameInfo = createStubInstance(GameService);
+        gameValidation = createStubInstance(GameValidation);
         const app = Container.get(Application);
         // eslint-disable-next-line dot-notation
         Object.defineProperty(app['gameController'], 'gameManager', { value: gameManager });
         Object.defineProperty(app['gameController'], 'gameInfo', { value: gameInfo });
+        Object.defineProperty(app['gameController'], 'gameValidation', { value: gameValidation });
         expressApp = app.app;
     });
 
@@ -101,5 +105,33 @@ describe('GameController', () => {
     it('should return Not Found if the game does not exist', async () => {
         gameInfo.getGameById.rejects();
         return supertest(expressApp).get('/api/game/cards/0').expect(StatusCodes.NOT_FOUND);
+    });
+
+    it('should return a bad request if one of the attribute needed is not here to validate a new game card', async () => {
+        return supertest(expressApp).post('/api/game/card/validation').send({}).expect(StatusCodes.BAD_REQUEST);
+    });
+
+    it('should return a bad request if one of the attribute needed is not here to create a new game card', async () => {
+        return supertest(expressApp).post('/api/game/card').send({}).expect(StatusCodes.BAD_REQUEST);
+    });
+
+    it('should return Not Found if a problem in the attribute is detected', async () => {
+        gameValidation.isGameValid.rejects();
+        const expectedBody = { original: new ArrayBuffer(0), modify: new ArrayBuffer(0), differenceRadius: 0 };
+        return supertest(expressApp).post('/api/game/card/validation').send(expectedBody).expect(StatusCodes.NOT_FOUND);
+    });
+
+    it('should return Accepted if the game is valid', async () => {
+        const expectedIsValid = true;
+        gameValidation.isGameValid.resolves(expectedIsValid);
+        const expectedBody = { original: new ArrayBuffer(0), modify: new ArrayBuffer(0), differenceRadius: 0 };
+        return supertest(expressApp).post('/api/game/card/validation').send(expectedBody).expect(StatusCodes.ACCEPTED);
+    });
+
+    it('should return Not Accepted if the game is invalid', async () => {
+        const expectedIsValid = false;
+        gameValidation.isGameValid.resolves(expectedIsValid);
+        const expectedBody = { original: new ArrayBuffer(0), modify: new ArrayBuffer(0), differenceRadius: 0 };
+        return supertest(expressApp).post('/api/game/card/validation').send(expectedBody).expect(StatusCodes.NOT_ACCEPTABLE);
     });
 });

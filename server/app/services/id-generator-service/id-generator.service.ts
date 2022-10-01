f2bcb578dde72994f1428c79dc281d62ec3e1d66
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-01 19:05:16 -0400

    fix: merge with dev

--- server/app/services/id-generator-service/id-generator.service.ts
+++
@@ -1,9 +0,0 @@
-import { Service } from 'typedi';
-import { v4 } from 'uuid';
-
-@Service()
-export class IdGeneratorService {
-    async getNewId(): Promise<string> {
-        return v4();
-    }
-}

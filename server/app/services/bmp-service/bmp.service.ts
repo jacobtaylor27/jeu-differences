8bc3ad2e8d627e298f9ac5a14a18dc3c61611441
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-01 01:07:45 -0400

    fix: adding some coverage

--- server/app/services/bmp-service/bmp.service.ts
+++ server/app/services/bmp-service/bmp.service.ts
@@ -31,10 +31,7 @@ export class BmpService {
     }
     async addBFromArrayBuffer(bpmToConvert: ArrayBuffer, filepath: string): Promise<string> {
         const decodedBmp: Bmp = await this.bmpDecoderService.decodeArrayBufferToBmp(bpmToConvert);
-        const bmpId: string = v4();
-        const fullpath = filepath + bmpId + '.bmp';
-        await this.bmpEncoderService.encodeBmpIntoB(fullpath, decodedBmp);
-        return bmpId;
+        return this.addBFromBmp(decodedBmp, filepath);
     }
 
     async addBFromBmp(bmpObj: Bmp, filepath: string): Promise<string> {

c564c4fb3ed8c3d03656e1dd3d81c50fd60c6bcc
Xavier Jouslin <jouslin@nowray.net>
2022-10-01 19:08:13 -0400

    feat: set all dep of validation game service

--- server/app/services/game-validation-service/game-validation.service.ts
+++ server/app/services/game-validation-service/game-validation.service.ts
@@ -1,4 +1,17 @@
 import { Service } from 'typedi';
+import { BmpDifferenceInterpreter } from '@app/services/bmp-difference-interpreter-service/bmp-difference-interpreter.service';
+import { BmpSubtractorService } from '@app/services/bmp-subtractor-service/bmp-subtractor.service';
+import { Bmp } from '@app/classes/bmp/bmp';
+import { BmpCoordinate } from '@app/classes/bmp-coordinate/bmp-coordinate';
+import { Difference } from '@app/constants/game';
+import { BmpDecoderService } from '@app/services/bmp-decoder-service/bmp-decoder-service';
 
 @Service()
-export class GameValidation {}
+export class GameValidation {
+    constructor(
+        private bmpSubtractor: BmpSubtractorService,
+        private bmpDifferenceInterpreter: BmpDifferenceInterpreter,
+        private bmpDecoder: BmpDecoderService,
+    ) {}
+
+}

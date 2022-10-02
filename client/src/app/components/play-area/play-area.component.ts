7d643429f1a71b93898a8e4310377c268335c59f
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-01 21:43:57 -0400

    fix : get width and height for size canvas

--- client/src/app/components/play-area/play-area.component.ts
+++ client/src/app/components/play-area/play-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { SIZE } from '@app/constants/canvas';
 import { Vec2 } from '@app/interfaces/vec2';
 import { DifferencesDetectionHandlerService } from '@app/services/differences-detection-handler/differences-detection-handler.service';
 
@@ -13,6 +14,14 @@ export class PlayAreaComponent {
     mousePosition: Vec2 = { x: 0, y: 0 };
     buttonPressed = '';
 
+    get width(): number {
+        return SIZE.x;
+    }
+
+    get height(): number {
+        return SIZE.y;
+    }
+
     constructor(private readonly differencesDetectionHandlerService: DifferencesDetectionHandlerService) {}
 
     @HostListener('keydown', ['$event'])
@@ -22,6 +31,7 @@ export class PlayAreaComponent {
 
     mouseHitDetect($event: MouseEvent, canvas: String) {
         this.mousePosition = { x: $event.offsetX, y: $event.offsetY };
+
         const ctx: CanvasRenderingContext2D =
             canvas === 'original'
                 ? (this.canvasOriginal.nativeElement.getContext('2d') as CanvasRenderingContext2D)

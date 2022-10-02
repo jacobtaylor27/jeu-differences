3f2209484bc5aba7994960ae30fd149a5e391236
Xavier Jouslin <jouslin@nowray.net>
2022-10-02 09:49:19 -0400

    fix: remove ArrayBuffer to just Bmp object and refactor the code

--- server/app/services/game-validation-service/game-validation.service.spec.ts
+++ server/app/services/game-validation-service/game-validation.service.spec.ts
@@ -3,7 +3,6 @@ import { BmpDifferenceInterpreter } from '@app/services/bmp-difference-interpret
 import { expect } from 'chai';
 import { GameValidation } from './game-validation.service';
 import { BmpSubtractorService } from '@app/services/bmp-subtractor-service/bmp-subtractor.service';
-import { BmpDecoderService } from '@app/services/bmp-decoder-service/bmp-decoder-service';
 import { BmpCoordinate } from '@app/classes/bmp-coordinate/bmp-coordinate';
 import { Bmp } from '@app/classes/bmp/bmp';
 
@@ -11,71 +10,49 @@ describe('GameValidation', () => {
     let gameValidation: GameValidation;
     let bmpSubtractor: SinonStubbedInstance<BmpSubtractorService>;
     let bmpDifferenceInterpreterSpyObj: SinonStubbedInstance<BmpDifferenceInterpreter>;
-    let bmpDecoder: SinonStubbedInstance<BmpDecoderService>;
     // let differenceSpyObj: SinonSpiedInstance<BmpDifferenceInterpreter>;
 
     beforeEach(() => {
         bmpSubtractor = createStubInstance(BmpSubtractorService);
         bmpDifferenceInterpreterSpyObj = createStubInstance(BmpDifferenceInterpreter);
-        bmpDecoder = createStubInstance(BmpDecoderService);
-        gameValidation = new GameValidation(bmpSubtractor, bmpDifferenceInterpreterSpyObj, bmpDecoder);
+        gameValidation = new GameValidation(bmpSubtractor, bmpDifferenceInterpreterSpyObj);
     });
 
     it('should get the number of difference', async () => {
         const expectedDifferences = [[{} as BmpCoordinate]];
+        const expectedReject = 'test failed error';
+        const differenceBmpSpy = stub(gameValidation, 'differenceBmp').resolves();
         bmpDifferenceInterpreterSpyObj.getCoordinates.resolves(expectedDifferences);
-        expect(await gameValidation.numberDifference({} as Bmp)).to.equal(expectedDifferences.length);
-        bmpDifferenceInterpreterSpyObj.getCoordinates.rejects();
-        expect(await gameValidation.numberDifference({} as Bmp)).to.equal(null);
+        expect(await gameValidation.numberDifference({} as Bmp, {} as Bmp, 0)).to.equal(expectedDifferences.length);
+        differenceBmpSpy.rejects(new Error(expectedReject));
+        gameValidation.numberDifference({} as Bmp, {} as Bmp, 0).catch((reason: Error) => expect(reason.message).to.equal(expectedReject));
+        bmpDifferenceInterpreterSpyObj.getCoordinates.rejects(new Error(expectedReject));
+        gameValidation.numberDifference({} as Bmp, {} as Bmp, 0).catch((reason: Error) => expect(reason.message).to.equal(expectedReject));
     });
 
     it('should get difference from Bmp object', async () => {
         const expectedDifferences = {} as Bmp;
+        const expectedReject = 'test failed error';
         bmpSubtractor.getDifferenceBMP.resolves(expectedDifferences);
         expect(await gameValidation.differenceBmp({} as Bmp, {} as Bmp, 0)).to.equal(expectedDifferences);
-        bmpSubtractor.getDifferenceBMP.rejects();
-        expect(await gameValidation.differenceBmp({} as Bmp, {} as Bmp, 0)).to.equal(null);
+        bmpSubtractor.getDifferenceBMP.rejects(new Error(expectedReject));
+        await gameValidation.differenceBmp({} as Bmp, {} as Bmp, 0).catch((reason: Error) => expect(reason.message).to.equal(expectedReject));
     });
 
     it('should return check if the number of differences is valid', async () => {
-        const differenceBmpSpy = stub(gameValidation, 'differenceBmp').rejects();
-        expect(await gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0)).to.equal(null);
-        differenceBmpSpy.resolves();
+        const numberDifferenceSpy = stub(gameValidation, 'numberDifference').rejects();
+        const expectedReject = 'test failed error';
+        numberDifferenceSpy.rejects(new Error(expectedReject));
+        gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0).catch((reason: Error) => expect(reason.message).to.equal(expectedReject));
 
-        bmpDifferenceInterpreterSpyObj.getCoordinates.rejects();
-        expect(await gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0)).to.equal(null);
-
-        bmpDifferenceInterpreterSpyObj.getCoordinates.resolves([]);
+        numberDifferenceSpy.resolves(0);
         expect(await gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0)).to.equal(false);
 
-        bmpDifferenceInterpreterSpyObj.getCoordinates.resolves([[{} as BmpCoordinate], [{} as BmpCoordinate], [{} as BmpCoordinate]]);
+        numberDifferenceSpy.resolves(3);
         expect(await gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0)).to.equal(true);
-
-        bmpDifferenceInterpreterSpyObj.getCoordinates.resolves([
-            [{} as BmpCoordinate],
-            [{} as BmpCoordinate],
-            [{} as BmpCoordinate],
-            [{} as BmpCoordinate],
-            [{} as BmpCoordinate],
-            [{} as BmpCoordinate],
-            [{} as BmpCoordinate],
-            [{} as BmpCoordinate],
-            [{} as BmpCoordinate],
-            [{} as BmpCoordinate],
-        ]);
+        const exceedDifference = 20;
+        numberDifferenceSpy.resolves(exceedDifference);
         expect(await gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0)).to.equal(false);
-    });
-
-    it('should return if the game is valid', async () => {
-        const expectedReject = 'bmp decode failed';
-        bmpDecoder.decodeArrayBufferToBmp.rejects(expectedReject);
-        gameValidation.isGameValid({} as ArrayBuffer, {} as ArrayBuffer, 0).catch((reason) => expect(reason).to.equal(expectedReject));
-        bmpDecoder.decodeArrayBufferToBmp.resolves();
-        const isNbDifferenceValid = stub(gameValidation, 'isNbDifferenceValid').rejects(expectedReject);
-        gameValidation.isGameValid({} as ArrayBuffer, {} as ArrayBuffer, 0).catch((reason) => expect(reason).to.equal(expectedReject));
-        const expectedDifferenceValid = true;
-        isNbDifferenceValid.resolves(expectedDifferenceValid);
-        expect(await gameValidation.isGameValid({} as ArrayBuffer, {} as ArrayBuffer, 0)).to.equal(expectedDifferenceValid);
     });
 
     afterEach(() => {
--- server/app/services/game-validation-service/game-validation.service.ts
+++ server/app/services/game-validation-service/game-validation.service.ts
@@ -4,50 +4,24 @@ import { BmpSubtractorService } from '@app/services/bmp-subtractor-service/bmp-s
 import { Bmp } from '@app/classes/bmp/bmp';
 import { BmpCoordinate } from '@app/classes/bmp-coordinate/bmp-coordinate';
 import { Difference } from '@app/constants/game';
-import { BmpDecoderService } from '@app/services/bmp-decoder-service/bmp-decoder-service';
 
 @Service()
 export class GameValidation {
-    constructor(
-        private bmpSubtractor: BmpSubtractorService,
-        private bmpDifferenceInterpreter: BmpDifferenceInterpreter,
-        private bmpDecoder: BmpDecoderService,
-    ) {}
+    constructor(private bmpSubtractor: BmpSubtractorService, private bmpDifferenceInterpreter: BmpDifferenceInterpreter) {}
 
-    async numberDifference(bmpDifferentiated: Bmp) {
-        return this.bmpDifferenceInterpreter
-            .getCoordinates(bmpDifferentiated)
-            .then((coordinates: BmpCoordinate[][]) => coordinates.length)
-            .catch(() => null);
+    async numberDifference(original: Bmp, modify: Bmp, radius: number) {
+        return this.differenceBmp(original, modify, radius).then(async (bmpDifferentiated: Bmp) =>
+            this.bmpDifferenceInterpreter.getCoordinates(bmpDifferentiated).then((coordinates: BmpCoordinate[][]) => coordinates.length),
+        );
     }
 
     async differenceBmp(original: Bmp, modify: Bmp, radius: number) {
-        return this.bmpSubtractor
-            .getDifferenceBMP(original, modify, radius)
-            .then((differenceBMP: Bmp) => differenceBMP)
-            .catch(() => null);
+        return this.bmpSubtractor.getDifferenceBMP(original, modify, radius).then((differenceBMP: Bmp) => differenceBMP);
     }
 
     async isNbDifferenceValid(original: Bmp, modify: Bmp, radius: number) {
-        return this.differenceBmp(original, modify, radius)
-            .then(async (bmpDifferentiated: Bmp) =>
-                this.bmpDifferenceInterpreter
-                    .getCoordinates(bmpDifferentiated)
-                    .then((coordinates: BmpCoordinate[][]) => coordinates.length >= Difference.MIN && coordinates.length <= Difference.MAX)
-                    .catch(() => null),
-            )
-            .catch(() => null);
-    }
-
-    async isGameValid(original: ArrayBuffer, modify: ArrayBuffer, radius: number) {
-        let modifyBmp: Bmp;
-        let originalBmp: Bmp;
-        try {
-            modifyBmp = await this.bmpDecoder.decodeArrayBufferToBmp(modify);
-            originalBmp = await this.bmpDecoder.decodeArrayBufferToBmp(original);
-        } catch (e) {
-            return Promise.reject();
-        }
-        return await this.isNbDifferenceValid(originalBmp, modifyBmp, radius);
+        return this.numberDifference(original, modify, radius).then(
+            (nbDifference: number) => nbDifference >= Difference.MIN && nbDifference <= Difference.MAX,
+        );
     }
 }

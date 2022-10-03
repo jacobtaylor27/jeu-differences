15a3daa20b3354563a1b94b7948fce3a9b21655a
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-03 11:23:12 -0400

    fix : get image to page works

--- client/src/app/pages/game-selection-page/game-selection-page.component.ts
+++ client/src/app/pages/game-selection-page/game-selection-page.component.ts
@@ -1,30 +1,30 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TimeFormatter } from '@app/classes/time-formatter';
 import { GameCard } from '@app/interfaces/game-card';
 import { GameCarouselService } from '@app/services/carousel/game-carousel.service';
 import { CommunicationService } from '@app/services/communication.service';
-import { ImgData } from '@common/img-data';
-import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-game-selection-page',
     templateUrl: './game-selection-page.component.html',
     styleUrls: ['./game-selection-page.component.scss'],
 })
-export class GameSelectionPageComponent implements OnInit, AfterViewInit {
+export class GameSelectionPageComponent implements OnInit {
     gameCards: GameCard[] = [];
     favoriteTheme: string = 'deeppurple-amber-theme';
 
-    constructor(readonly gameCarouselService: GameCarouselService, readonly communicationService: CommunicationService) {}
+    constructor(readonly gameCarouselService: GameCarouselService, readonly communicationService: CommunicationService) {
+        this.fetchImgData();
+    }
 
     ngOnInit(): void {
         this.gameCards = this.gameCarouselService.getCards();
         this.makeCardsSelectMode();
     }
 
-    ngAfterViewInit(): void {
-        console.log(this.fetchImgData());
-    }
+    // ngAfterViewInit(): void {
+    //     console.log(this.fetchImgData());
+    // }
 
     getNumberOfGames(): number {
         return this.gameCarouselService.getCarouselLength();
@@ -44,11 +44,8 @@ export class GameSelectionPageComponent implements OnInit, AfterViewInit {
 
     fetchImgData() {
         console.log('this is called');
-        this.communicationService.getImgData('hello').pipe(
-            map((imgDataRequested: ImgData) => {
-                console.log('humongous');
-                return imgDataRequested.imgData;
-            }),
-        );
+        this.communicationService.getImgData('hello').subscribe((imgData) => {
+            console.log(imgData);
+        });
     }
 }

63dcc68482945263bb90adccd3e7b55ba71e74c9
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-03 17:16:25 -0400

    fix : remove ?

--- common/game-info.ts
+++ common/game-info.ts
@@ -1,13 +1,13 @@
-import { Coordinate } from '@common/coordinate';
-import { Score } from '@common/score';
+import { Coordinate } from './coordinate';
+import { Score } from './score';
 export interface GameInfo {
-    id?: string;
+    id: string;
     name: string;
     idOriginalBmp: string;
     idEditedBmp: string;
-    idDifferenceBmp?: string;
-    soloScore?: Score[];
-    multiplayerScore?: Score[];
+    idDifferenceBmp: string;
+    soloScore: Score[];
+    multiplayerScore: Score[];
     differenceRadius: number;
     differences: Coordinate[][];
 }

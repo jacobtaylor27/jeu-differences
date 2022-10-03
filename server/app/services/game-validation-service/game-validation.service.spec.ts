a410c3e4a6f8c445844b8edb28763833a325ae4d
Xavier Jouslin <jouslin@nowray.net>
2022-10-03 13:39:17 -0400

    remove merge conflict

--- server/app/services/game-validation-service/game-validation.service.spec.ts
+++ server/app/services/game-validation-service/game-validation.service.spec.ts
@@ -1,17 +1,10 @@
 import { Bmp } from '@app/classes/bmp/bmp';
-import { BmpDecoderService } from '@app/services/bmp-decoder-service/bmp-decoder-service';
 import { BmpDifferenceInterpreter } from '@app/services/bmp-difference-interpreter-service/bmp-difference-interpreter.service';
 import { BmpSubtractorService } from '@app/services/bmp-subtractor-service/bmp-subtractor.service';
 import { Coordinate } from '@common/coordinate';
 import { expect } from 'chai';
 import { createStubInstance, restore, SinonStubbedInstance, stub } from 'sinon';
 import { GameValidation } from './game-validation.service';
-<<<<<<< HEAD
-import { BmpSubtractorService } from '@app/services/bmp-subtractor-service/bmp-subtractor.service';
-import { BmpCoordinate } from '@app/classes/bmp-coordinate/bmp-coordinate';
-import { Bmp } from '@app/classes/bmp/bmp';
-=======
->>>>>>> 837f2b81ddbe1360bf209a0baf7725ded8699913
 
 describe('GameValidation', () => {
     let gameValidation: GameValidation;
@@ -26,13 +19,9 @@ describe('GameValidation', () => {
     });
 
     it('should get the number of difference', async () => {
-<<<<<<< HEAD
-        const expectedDifferences = [[{} as BmpCoordinate]];
+        const expectedDifferences = [[{} as Coordinate]];
         const expectedReject = 'test failed error';
         const differenceBmpSpy = stub(gameValidation, 'differenceBmp').resolves();
-=======
-        const expectedDifferences = [[{} as Coordinate]];
->>>>>>> 837f2b81ddbe1360bf209a0baf7725ded8699913
         bmpDifferenceInterpreterSpyObj.getCoordinates.resolves(expectedDifferences);
         expect(await gameValidation.numberDifference({} as Bmp, {} as Bmp, 0)).to.equal(expectedDifferences.length);
         differenceBmpSpy.rejects(new Error(expectedReject));
@@ -59,28 +48,10 @@ describe('GameValidation', () => {
         numberDifferenceSpy.resolves(0);
         expect(await gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0)).to.equal(false);
 
-<<<<<<< HEAD
         numberDifferenceSpy.resolves(3);
         expect(await gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0)).to.equal(true);
         const exceedDifference = 20;
         numberDifferenceSpy.resolves(exceedDifference);
-=======
-        bmpDifferenceInterpreterSpyObj.getCoordinates.resolves([[{} as Coordinate], [{} as Coordinate], [{} as Coordinate]]);
-        expect(await gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0)).to.equal(true);
-
-        bmpDifferenceInterpreterSpyObj.getCoordinates.resolves([
-            [{} as Coordinate],
-            [{} as Coordinate],
-            [{} as Coordinate],
-            [{} as Coordinate],
-            [{} as Coordinate],
-            [{} as Coordinate],
-            [{} as Coordinate],
-            [{} as Coordinate],
-            [{} as Coordinate],
-            [{} as Coordinate],
-        ]);
->>>>>>> 837f2b81ddbe1360bf209a0baf7725ded8699913
         expect(await gameValidation.isNbDifferenceValid({} as Bmp, {} as Bmp, 0)).to.equal(false);
     });
 

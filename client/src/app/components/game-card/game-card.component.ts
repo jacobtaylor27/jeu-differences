f92126b1d6eb35dabf1dc8ac9bcbffd9580a3db9
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-03 16:18:14 -0400

    feat : component get the request and put image

--- client/src/app/components/game-card/game-card.component.ts
+++ client/src/app/components/game-card/game-card.component.ts
@@ -1,16 +1,48 @@
-import { Component, Input } from '@angular/core';
-import { Score } from '@common/score';
-import { GameCard } from '@app/interfaces/game-card';
+import { HttpResponse } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
 import { TimeFormatter } from '@app/classes/time-formatter';
+import { GameCard } from '@app/interfaces/game-card';
+import { CommunicationService } from '@app/services/communication.service';
+import { Score } from '@common/score';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-game-card',
     templateUrl: './game-card.component.html',
     styleUrls: ['./game-card.component.scss'],
 })
-export class GameCardComponent {
+export class GameCardComponent implements OnInit {
     @Input() gameCard: GameCard;
     favoriteTheme: string = 'deeppurple-amber-theme';
+    imageData$: Observable<any>;
+    imageSrc: string;
+
+    constructor(private readonly communicationService: CommunicationService) {}
+
+    ngOnInit() {
+        this.getImageName();
+    }
+
+    getImageName() {
+        this.imageData$ = this.communicationService.getImgData(this.gameCard.gameInformation.imgName);
+        this.imageData$.subscribe((response: HttpResponse<{ width: number; height: number; data: number[] }> | null) => {
+            if (!response || !response.body) {
+                return;
+            }
+
+            const canvas = document.createElement('canvas');
+            canvas.width = response.body.width;
+            canvas.height = response.body.height;
+            const ctx = canvas.getContext('2d');
+            const image: ImageData = new ImageData(new Uint8ClampedArray(response.body.data), response.body.width, response.body.height, {
+                colorSpace: 'srgb',
+            });
+
+            ctx?.putImageData(image, 0, 0);
+            const dataUrl = canvas.toDataURL('image/jpeg');
+            this.imageSrc = dataUrl.replace(/^data:image\/(png|jpg);base64,/, '');
+        });
+    }
 
     formatScoreTime(scoreTime: number): string {
         return TimeFormatter.getMMSSFormat(scoreTime);
@@ -18,10 +50,6 @@ export class GameCardComponent {
 
     getGameName(): string {
         return this.gameCard.gameInformation.name;
-    }
-
-    getImageName(): string {
-        return this.gameCard.gameInformation.imgName;
     }
 
     isAdminCard(): boolean {

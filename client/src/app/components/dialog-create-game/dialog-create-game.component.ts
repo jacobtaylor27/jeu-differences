eb68c502d686281e08c644202b57aaeebb60f55e
Xavier Jouslin <jouslin@nowray.net>
2022-10-03 19:17:10 -0400

    fix: create a game

--- client/src/app/components/dialog-create-game/dialog-create-game.component.ts
+++ client/src/app/components/dialog-create-game/dialog-create-game.component.ts
@@ -1,8 +1,12 @@
 // import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpStatusCode } from '@angular/common/http';
 import { AfterViewInit, Component, ElementRef, Inject, ViewChild } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CREATE_GAME } from '@app/constants/server';
+import { Canvas } from '@app/enums/canvas';
 import { Theme } from '@app/enums/theme';
+import { catchError, of } from 'rxjs';
 
 @Component({
     selector: 'app-dialog-create-game',
@@ -12,7 +16,7 @@ import { Theme } from '@app/enums/theme';
 export class DialogCreateGameComponent implements AfterViewInit {
     @ViewChild('imageDifference', { static: false }) differentImage!: ElementRef<HTMLCanvasElement>;
     theme: typeof Theme = Theme;
-    name: FormControl = new FormControl('', Validators.pattern(''));
+    form: FormGroup = new FormGroup({ name: new FormControl('', [Validators.pattern('[a-zA-Z ]*'), Validators.required]) });
     constructor(
         @Inject(MAT_DIALOG_DATA)
         public data: {
@@ -20,15 +24,45 @@ export class DialogCreateGameComponent implements AfterViewInit {
             src: ImageData;
             difference: ImageData;
             nbDifference: number;
-            differenceImage: ImageData;
-        } /* private http: HttpClient */,
+            differenceImage: number[];
+        },
+        private http: HttpClient,
     ) {}
 
     ngAfterViewInit() {
         const ctx = this.differentImage.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-        ctx.putImageData(this.data.differenceImage, 0, 0);
+        ctx.putImageData(new ImageData(new Uint8ClampedArray(this.data.differenceImage), Canvas.WIDTH, Canvas.HEIGHT, { colorSpace: 'srgb' }), 0, 0);
     }
     createGame() {
-        return;
+        this.http
+            .post<Record<string, never>>(
+                CREATE_GAME,
+                {
+                    original: { width: this.data.src.width, height: this.data.src.height, data: Array.from(this.data.src.data) },
+                    modify: { width: this.data.difference.width, height: this.data.difference.height, data: Array.from(this.data.difference.data) },
+                    differenceRadius: this.data.expansionRadius,
+                    name: (this.form.get('name') as FormControl).value,
+                },
+                { observe: 'response' },
+            )
+            .pipe(
+                catchError(() => {
+                    console.log('Le serveur a eu un probleme pour sauvegarder le jeu');
+                    return of(null);
+                }),
+            )
+            .subscribe((response: HttpResponse<Record<string, never>> | null) => {
+                if (!response) {
+                    return;
+                }
+                switch (response.status) {
+                    case HttpStatusCode.Created:
+                        console.log('Le jeu est cree');
+                        break;
+                    default:
+                        console.log('Le jeu n a pas ete accepter');
+                        break;
+                }
+            });
     }
 }

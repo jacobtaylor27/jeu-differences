c6d3ce932df9db01bd1bd94d37021b68e314f213
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-10-03 01:32:36 -0400

    fix: finding issue

--- client/src/app/pages/game-selection-page/game-selection-page.component.ts
+++ client/src/app/pages/game-selection-page/game-selection-page.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { TimeFormatter } from '@app/classes/time-formatter';
 import { GameCard } from '@app/interfaces/game-card';
 import { GameCarouselService } from '@app/services/carousel/game-carousel.service';
+import { CommunicationService } from '@app/services/communication.service';
+import { ImgData } from '@common/img-data';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-game-selection-page',
     templateUrl: './game-selection-page.component.html',
     styleUrls: ['./game-selection-page.component.scss'],
 })
-export class GameSelectionPageComponent implements OnInit {
+export class GameSelectionPageComponent implements OnInit, AfterViewInit {
     gameCards: GameCard[] = [];
     favoriteTheme: string = 'deeppurple-amber-theme';
 
-    constructor(readonly gameCarouselService: GameCarouselService) {}
+    constructor(readonly gameCarouselService: GameCarouselService, readonly communicationService: CommunicationService) {}
 
     ngOnInit(): void {
         this.gameCards = this.gameCarouselService.getCards();
         this.makeCardsSelectMode();
+    }
+
+    ngAfterViewInit(): void {
+        console.log(this.fetchImgData());
     }
 
     getNumberOfGames(): number {
@@ -33,5 +40,15 @@ export class GameSelectionPageComponent implements OnInit {
 
     formatScoreTime(scoreTime: number): string {
         return TimeFormatter.getMMSSFormat(scoreTime);
+    }
+
+    fetchImgData() {
+        console.log('this is called');
+        this.communicationService.getImgData('hello').pipe(
+            map((imgDataRequested: ImgData) => {
+                console.log('humongous');
+                return imgDataRequested.imgData;
+            }),
+        );
     }
 }

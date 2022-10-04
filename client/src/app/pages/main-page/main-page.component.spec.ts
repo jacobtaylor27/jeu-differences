69cf5aa802c07c9de66f054660d4ea0bfaee0fcb
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-04 13:29:01 -0400

    feat : added home page test

--- client/src/app/pages/main-page/main-page.component.spec.ts
+++ client/src/app/pages/main-page/main-page.component.spec.ts
@@ -3,6 +3,7 @@ import { AppMaterialModule } from '@app/modules/material.module';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
 import { MainPageService } from '@app/services/main-page/main-page.service';
+import { GameMode } from '@common/game-mode';
 
 describe('MainPageComponent', () => {
     let component: MainPageComponent;
@@ -12,7 +13,7 @@ describe('MainPageComponent', () => {
 
     beforeEach(async () => {
         spyMainPageService = jasmine.createSpyObj('GamePageService', ['setGameMode']);
-        spyGameInfosHandlerService = jasmine.createSpyObj('GameInformationHandlerService', ['setGameMode', 'getGameName']);
+        spyGameInfosHandlerService = jasmine.createSpyObj('GameInformationHandlerService', ['setGameMode', 'getGameName', 'getGameMode']);
 
         await TestBed.configureTestingModule({
             declarations: [MainPageComponent],
@@ -39,5 +40,15 @@ describe('MainPageComponent', () => {
 
     it("should have as title 'Jeu de différences'", () => {
         expect(component.title).toEqual('Jeu de différences');
+    });
+
+    it('should set GameMode to Classic when Classic button is clicked', () => {
+        component.onClickPlayClassic();
+        expect(spyMainPageService.setGameMode).toHaveBeenCalledWith(GameMode.Classic);
+    });
+
+    it('should set GameMode to Limited when Limited button is clicked', () => {
+        component.onClickPlayLimited();
+        expect(spyMainPageService.setGameMode).toHaveBeenCalledWith(GameMode.LimitedTime);
     });
 });

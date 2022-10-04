b87987983c56a39e83ee67c867b2f0f5b16d3eb5
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-10-04 14:19:19 -0400

    fix: lint

--- client/src/app/services/differences-detection-handler/differences-detection-handler.service.spec.ts
+++ client/src/app/services/differences-detection-handler/differences-detection-handler.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { DifferencesDetectionHandlerService } from './differences-detection-handler.service';
 
 describe('DifferencesDetectionHandlerService', () => {
-  let service: DifferencesDetectionHandlerService;
+    let service: DifferencesDetectionHandlerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DifferencesDetectionHandlerService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(DifferencesDetectionHandlerService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });

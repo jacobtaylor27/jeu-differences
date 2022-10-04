2184cd332ebaaef2cc6812a6e0d3bcbf7420f687
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-04 13:23:47 -0400

    feat : added test

--- client/src/app/services/mouse-handler/mouse-handler.service.spec.ts
+++ client/src/app/services/mouse-handler/mouse-handler.service.spec.ts
@@ -3,14 +3,24 @@ import { TestBed } from '@angular/core/testing';
 import { MouseHandlerService } from './mouse-handler.service';
 
 describe('MouseHandlerService', () => {
-  let service: MouseHandlerService;
+    let service: MouseHandlerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MouseHandlerService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(MouseHandlerService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should return MousePosition', () => {
+        const mouseEvent = {
+            clientX: 10,
+            clientY: 10,
+        } as MouseEvent;
+        console.log(service.mouseHitDetect(mouseEvent).x);
+        expect(service.mouseHitDetect(mouseEvent).x).toEqual(10);
+        expect(service.mouseHitDetect(mouseEvent).y).toEqual(10);
+    });
 });

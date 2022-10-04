de18b2a7b0fb4bf34d67917efe8dfc2c7ee28616
Xavier Jouslin <jouslin@nowray.net>
2022-10-04 11:54:21 -0400

    feat: add communication request to add and validate a game

--- client/src/app/services/communication/communication.service.spec.ts
+++ client/src/app/services/communication/communication.service.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { VALID_GAME } from '@app/constants/server';
-import { CommunicationService } from '@app/services/communication.service';
+import { CommunicationService } from '@app/services/communication/communication.service';
 import { Message } from '@common/message';
 
 describe('CommunicationService', () => {

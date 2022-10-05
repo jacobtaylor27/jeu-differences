799692d77dc0322b797e73435a7f5c5c9fadb70f
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-04 22:44:03 -0400

    fix : remove declaration

--- client/src/app/components/sidebar/sidebar.component.spec.ts
+++ client/src/app/components/sidebar/sidebar.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CluesAreaComponent } from '@app/components/clues-area/clues-area.component';
-import { DifferencesAreaComponent } from '@app/components/differences-area/differences-area.component';
 import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { TimerCountdownComponent } from '@app/components/timer-countdown/timer-countdown.component';
 import { TimerStopwatchComponent } from '@app/components/timer-stopwatch/timer-stopwatch.component';
@@ -16,7 +15,7 @@ describe('SidebarComponent', () => {
         spyGameInfosService = jasmine.createSpyObj('GameInformationHandlerService', ['getGameName', 'setPlayerName', 'getGameMode', 'getPlayerName']);
 
         await TestBed.configureTestingModule({
-            declarations: [SidebarComponent, CluesAreaComponent, TimerStopwatchComponent, TimerCountdownComponent, DifferencesAreaComponent],
+            declarations: [SidebarComponent, CluesAreaComponent, TimerStopwatchComponent, TimerCountdownComponent],
             imports: [AppMaterialModule],
             providers: [
                 {
@@ -29,6 +28,35 @@ describe('SidebarComponent', () => {
         fixture = TestBed.createComponent(SidebarComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        spyGameInfosService.gameInformation = {
+            id: '1',
+            name: 'test',
+            idOriginalBmp: 'imageName',
+            idEditedBmp: '1',
+            idDifferenceBmp: '1',
+            soloScore: [
+                {
+                    playerName: 'test2',
+                    time: 10,
+                },
+                {
+                    playerName: 'test',
+                    time: 10,
+                },
+            ],
+            multiplayerScore: [
+                {
+                    playerName: 'test2',
+                    time: 10,
+                },
+                {
+                    playerName: 'test',
+                    time: 10,
+                },
+            ],
+            differenceRadius: 3,
+            differences: [],
+        };
     });
 
     it('should create', () => {

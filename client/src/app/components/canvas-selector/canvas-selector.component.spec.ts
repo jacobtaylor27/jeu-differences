843116baa491fffec0db76132cdf647a9873831a
Xavier Jouslin <jouslin@nowray.net>
2022-10-11 11:12:55 -0400

    test: add dep

--- client/src/app/components/canvas-selector/canvas-selector.component.spec.ts
+++ client/src/app/components/canvas-selector/canvas-selector.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CanvasSelectorComponent } from './canvas-selector.component';
 
 describe('CanvasSelectorComponent', () => {
-  let component: CanvasSelectorComponent;
-  let fixture: ComponentFixture<CanvasSelectorComponent>;
+    let component: CanvasSelectorComponent;
+    let fixture: ComponentFixture<CanvasSelectorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CanvasSelectorComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CanvasSelectorComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(CanvasSelectorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(CanvasSelectorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

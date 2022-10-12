8ce3775b59bfcf77c7fb09c3ef7c5b477cb8990a
Xavier Jouslin <jouslin@nowray.net>
2022-10-12 17:47:05 -0400

    test: validate connection manager to socket

--- client/src/app/services/communication-socket/communication-socket.service.spec.ts
+++ client/src/app/services/communication-socket/communication-socket.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
-
+import * as io from 'socket.io-client';
 import { CommunicationSocketService } from './communication-socket.service';
 
-describe('CommunicationSocketService', () => {
-  let service: CommunicationSocketService;
+fdescribe('CommunicationSocketService', () => {
+    let service: CommunicationSocketService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CommunicationSocketService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(CommunicationSocketService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should connect to server by socket', () => {
+        const spySocketAlive = spyOnProperty(Object.getPrototypeOf(service), 'isSocketAlive').and.callFake(() => false);
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        service['connect']();
+        expect(spySocketAlive).toHaveBeenCalled();
+    });
+
+    it('should disconnect to server by socket', () => {
+        const spySocketAlive = spyOnProperty(Object.getPrototypeOf(service), 'isSocketAlive').and.callFake(() => true);
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        service['socket'] = { disconnect: () => {} } as io.Socket<never, never>;
+        const spyDisconnect = spyOn(service['socket'], 'disconnect');
+        service['disconnect']();
+        expect(spyDisconnect).toHaveBeenCalled();
+        expect(spySocketAlive).toHaveBeenCalled();
+    });
 });

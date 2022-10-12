f72cd52fa8ac64e9da0ef874173e60326ddfcd7f
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-12 16:20:03 -0400

    feat : added test for game page

--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -1,83 +1,85 @@
-// import { HttpClientModule } from '@angular/common/http';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { CluesAreaComponent } from '@app/components/clues-area/clues-area.component';
-// import { DifferencesAreaComponent } from '@app/components/differences-area/differences-area.component';
-// import { ExitGameButtonComponent } from '@app/components/exit-game-button/exit-game-button.component';
-// import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
-// import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
-// import { TimerCountdownComponent } from '@app/components/timer-countdown/timer-countdown.component';
-// import { TimerStopwatchComponent } from '@app/components/timer-stopwatch/timer-stopwatch.component';
-// import { AppMaterialModule } from '@app/modules/material.module';
-// import { CommunicationService } from '@app/services/communication/communication.service';
-// import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
-// import { GameMode } from '@common/game-mode';
-// import { of } from 'rxjs';
-// import { GamePageComponent } from './game-page.component';
-// import SpyObj = jasmine.SpyObj;
+import { HttpClientModule, HttpResponse } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CluesAreaComponent } from '@app/components/clues-area/clues-area.component';
+import { DifferencesAreaComponent } from '@app/components/differences-area/differences-area.component';
+import { ExitGameButtonComponent } from '@app/components/exit-game-button/exit-game-button.component';
+import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+import { AppMaterialModule } from '@app/modules/material.module';
+import { CommunicationService } from '@app/services/communication/communication.service';
+import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
+import { of } from 'rxjs';
+import { GamePageComponent } from './game-page.component';
 
-// describe('GamePageComponent', () => {
-//     let component: GamePageComponent;
-//     let fixture: ComponentFixture<GamePageComponent>;
-//     let communicationServiceSpy: SpyObj<CommunicationService>;
-//     let gameInformationHandlerServiceSpy: SpyObj<GameInformationHandlerService>;
+describe('GamePageComponent', () => {
+    let component: GamePageComponent;
+    let fixture: ComponentFixture<GamePageComponent>;
+    let dialogSpyObj: jasmine.SpyObj<MatDialog>;
+    let communicationServiceSpy: jasmine.SpyObj<CommunicationService>;
+    let gameInformationHandlerServiceSpy: jasmine.SpyObj<GameInformationHandlerService>;
 
-//     beforeEach(async () => {
-//         communicationServiceSpy = jasmine.createSpyObj('ExampleService', ['getTimeValue', 'getImgData']);
-//         gameInformationHandlerServiceSpy = jasmine.createSpyObj('GameInformationHandlerService', [
-//             'getGameMode',
-//             'getGameName',
-//             'getPlayerName',
-//             'getOriginalBmp',
-//             'getOriginalBmpId',
-//             'getModifiedBmpId',
-//             'getGameInformation',
-//         ]);
-//         communicationServiceSpy.getTimeValue.and.returnValue(of({ title: '', body: '' }));
-//         communicationServiceSpy.getImgData.and.returnValue(of());
+    beforeEach(async () => {
+        dialogSpyObj = jasmine.createSpyObj('MatDialog', ['open']);
+        communicationServiceSpy = jasmine.createSpyObj('CommunicationService', ['createGameRoom']);
 
-//         await TestBed.configureTestingModule({
-//             declarations: [
-//                 GamePageComponent,
-//                 SidebarComponent,
-//                 PlayAreaComponent,
-//                 CluesAreaComponent,
-//                 DifferencesAreaComponent,
-//                 ExitGameButtonComponent,
-//                 TimerCountdownComponent,
-//                 TimerStopwatchComponent,
-//             ],
-//             imports: [RouterTestingModule, HttpClientModule, AppMaterialModule],
-//             providers: [
-//                 { provide: CommunicationService, useValue: communicationServiceSpy },
-//                 {
-//                     provide: GameInformationHandlerService,
-//                     useValue: gameInformationHandlerServiceSpy,
-//                 },
-//             ],
-//         }).compileComponents();
-//     });
+        gameInformationHandlerServiceSpy = jasmine.createSpyObj('GameInformationHandlerService', [
+            'getGameMode',
+            'getGameName',
+            'getPlayerName',
+            'getOriginalBmp',
+            'getOriginalBmpId',
+            'getModifiedBmpId',
+            'getGameInformation',
+            'getId',
+        ]);
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(GamePageComponent);
-//         component = fixture.componentInstance;
-//         gameInformationHandlerServiceSpy.gameInformation = {
-//             id: '1',
-//             name: 'test',
-//             idOriginalBmp: 'original',
-//             idEditedBmp: 'edited',
-//             idDifferenceBmp: 'difference',
-//             soloScore: [],
-//             multiplayerScore: [],
-//             differenceRadius: 2,
-//             differences: [],
-//         };
-//         gameInformationHandlerServiceSpy.gameMode = GameMode.Classic;
-//         gameInformationHandlerServiceSpy.playerName = 'test';
-//         fixture.detectChanges();
-//     });
+        await TestBed.configureTestingModule({
+            declarations: [
+                GamePageComponent,
+                SidebarComponent,
+                PlayAreaComponent,
+                CluesAreaComponent,
+                DifferencesAreaComponent,
+                ExitGameButtonComponent,
+                // TimerStopwatchComponent,
+            ],
+            imports: [RouterTestingModule, HttpClientModule, AppMaterialModule],
+            providers: [
+                { provide: MatDialog, useValue: dialogSpyObj },
+                { provide: CommunicationService, useValue: communicationServiceSpy },
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+                {
+                    provide: GameInformationHandlerService,
+                    useValue: gameInformationHandlerServiceSpy,
+                },
+            ],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(GamePageComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should create game room on init', () => {
+        const spyCreateGame = spyOn(component, 'createGameRoom');
+        spyCreateGame.and.callFake(() => {});
+        component.ngOnInit();
+        expect(spyCreateGame).toHaveBeenCalled();
+    });
+
+    it('should createGameRoom properly', () => {
+        communicationServiceSpy.createGameRoom.and.callFake(() => {
+            return of({ body: { id: '1' } } as HttpResponse<{ id: string }>);
+        });
+        component.createGameRoom();
+        expect(communicationServiceSpy.createGameRoom).toHaveBeenCalled();
+
+        communicationServiceSpy.createGameRoom.and.rejectWith(undefined);
+        expect(communicationServiceSpy.createGameRoom).toHaveBeenCalled();
+    });
+});

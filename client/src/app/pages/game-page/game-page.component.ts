89e004f0a1f784c14265a02475759776781b815e
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-12 16:18:34 -0400

    fix : onInit instead of constructor

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,5 +1,5 @@
 import { HttpResponse } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommunicationService } from '@app/services/communication/communication.service';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
 
@@ -8,11 +8,16 @@ import { GameInformationHandlerService } from '@app/services/game-information-ha
     templateUrl: './game-page.component.html',
     styleUrls: ['./game-page.component.scss'],
 })
-export class GamePageComponent {
+export class GamePageComponent implements OnInit {
     favoriteTheme: string = 'deeppurple-amber-theme';
     gameId: string;
 
-    constructor(private readonly communicationService: CommunicationService, private readonly gameInfoHandlerService: GameInformationHandlerService) {
+    constructor(
+        private readonly communicationService: CommunicationService,
+        private readonly gameInfoHandlerService: GameInformationHandlerService,
+    ) {}
+
+    ngOnInit() {
         this.createGameRoom();
     }
 

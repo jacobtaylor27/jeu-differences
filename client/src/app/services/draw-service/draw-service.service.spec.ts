f036fefaa2f51ea746f819a0bf17324595dc67bf
Xavier Jouslin <jouslin@nowray.net>
2022-10-12 15:19:36 -0400

    test: validate reset canvas

--- client/src/app/services/draw-service/draw-service.service.spec.ts
+++ client/src/app/services/draw-service/draw-service.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { PropagateCanvasEvent } from '@app/enums/propagate-canvas-event';
+import { ToolBoxService } from '@app/services/tool-box/tool-box.service';
+import { Subject } from 'rxjs';
 
 import { DrawService } from './draw-service.service';
 
 describe('DrawServiceService', () => {
     let service: DrawService;
-
+    let toolBoxServiceSpyObj: jasmine.SpyObj<ToolBoxService>;
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        toolBoxServiceSpyObj = jasmine.createSpyObj('ToolBoxService', [], { $resetDiff: new Subject(), $resetSource: new Subject() });
+        TestBed.configureTestingModule({
+            providers: [{ provide: ToolBoxService, useValue: toolBoxServiceSpyObj }],
+        });
         service = TestBed.inject(DrawService);
     });
 
@@ -19,5 +25,29 @@ describe('DrawServiceService', () => {
         expect(service.reposition({ offsetLeft: 0, offsetTop: 0 } as HTMLCanvasElement, { clientX: 10, clientY: 10 } as MouseEvent)).toEqual(
             expectedReposition,
         );
+    });
+
+    it('should submit a form because the type is both', async () => {
+        const spyDiff = spyOn(toolBoxServiceSpyObj.$resetDiff, 'next');
+        const spySource = spyOn(toolBoxServiceSpyObj.$resetSource, 'next');
+        service.reset(PropagateCanvasEvent.Both);
+        expect(spyDiff).toHaveBeenCalled();
+        expect(spySource).toHaveBeenCalled();
+    });
+
+    it('should submit a form because the type is difference', async () => {
+        const spyDiff = spyOn(toolBoxServiceSpyObj.$resetDiff, 'next');
+        const spySource = spyOn(toolBoxServiceSpyObj.$resetSource, 'next');
+        service.reset(PropagateCanvasEvent.Difference);
+        expect(spyDiff).toHaveBeenCalled();
+        expect(spySource).not.toHaveBeenCalled();
+    });
+
+    it('should submit a form because the type is source', async () => {
+        const spyDiff = spyOn(toolBoxServiceSpyObj.$resetDiff, 'next');
+        const spySource = spyOn(toolBoxServiceSpyObj.$resetSource, 'next');
+        service.reset(PropagateCanvasEvent.Source);
+        expect(spyDiff).not.toHaveBeenCalled();
+        expect(spySource).toHaveBeenCalled();
     });
 });

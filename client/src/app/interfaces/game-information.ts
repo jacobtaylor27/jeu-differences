a809730572b184a399f018ac50d7fab54f439ffe
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-10-12 18:07:07 -0400

    feat: new interface client side

---
+++ client/src/app/interfaces/game-information.ts
@@ -0,0 +1,12 @@
+import { Score } from '@common/score';
+
+export interface GameInformation {
+    id: string;
+    name: string;
+    idOriginalBmp: string;
+    idEditedBmp: string;
+    idDifferenceBmp: string;
+    soloScore: Score[];
+    multiplayerScore: Score[];
+    differenceRadius: number;
+}

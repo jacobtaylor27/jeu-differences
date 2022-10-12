f61e2cf6fd744837c3fc6acbaa16a8d77a73f507
Xavier Jouslin <jouslin@nowray.net>
2022-10-12 16:16:09 -0400

    feat: socket client service

--- client/src/app/services/communication-socket/communication-socket.service.ts
+++ client/src/app/services/communication-socket/communication-socket.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class CommunicationSocketService {
+    socket: Socket;
+    constructor() {
+        this.connect();
+    }
+    private get isSocketAlive() {
+        return this.socket && this.socket.connected;
+    }
 
-  constructor() { }
+    disconnect() {
+        if (!this.isSocketAlive) {
+            return;
+        }
+        this.socket.disconnect();
+    }
+
+    private connect() {
+        if (this.isSocketAlive) {
+            return;
+        }
+        this.socket = io(environment.socketUrl, { transports: ['websocket'], upgrade: false });
+        this.socket.connect();
+    }
 }

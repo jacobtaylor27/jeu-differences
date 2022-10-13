7325482c402c5acc6d421e7183f919c58f499e8e
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-13 14:03:12 -0400

    fix : test that created a image

--- client/src/app/components/dialog-create-game/dialog-create-game.component.spec.ts
+++ client/src/app/components/dialog-create-game/dialog-create-game.component.spec.ts
@@ -4,12 +4,15 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Canvas } from '@app/enums/canvas';
 import { AppMaterialModule } from '@app/modules/material.module';
+import { CommunicationService } from '@app/services/communication/communication.service';
+import { of } from 'rxjs';
 
 import { DialogCreateGameComponent } from './dialog-create-game.component';
 
 describe('DialogCreateGameComponent', () => {
     let component: DialogCreateGameComponent;
     let fixture: ComponentFixture<DialogCreateGameComponent>;
+    let spyCommunicationService : jasmine.SpyObj<CommunicationService>
     const image = new ImageData(Canvas.WIDTH, Canvas.HEIGHT);
     const pixelLength = 4;
     const model = {
@@ -20,9 +23,11 @@ describe('DialogCreateGameComponent', () => {
         difference: image,
     };
     beforeEach(async () => {
+        spyCommunicationService = jasmine.createSpyObj('CommunicationService', ['createGame'])
         await TestBed.configureTestingModule({
             declarations: [DialogCreateGameComponent],
-            providers: [{ provide: MAT_DIALOG_DATA, useValue: model }],
+            providers: [{ provide: MAT_DIALOG_DATA, useValue: model },
+                { provide: CommunicationService, useValue: spyCommunicationService }],
             imports: [AppMaterialModule, BrowserAnimationsModule, HttpClientModule],
         }).compileComponents();
         fixture = TestBed.createComponent(DialogCreateGameComponent);
@@ -35,6 +40,10 @@ describe('DialogCreateGameComponent', () => {
     });
 
     it('should post the game', () => {
+        spyCommunicationService.createGame.and.callFake(() => {
+            return of()
+        });
         component.createGame();
+        expect(spyCommunicationService.createGame).toHaveBeenCalled();
     });
 });

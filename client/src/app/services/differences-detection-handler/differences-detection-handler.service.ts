f04e15affe2ecbdbc02a7b76001a6bfde1843353
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-13 22:20:17 -0400

    fix : de-comment method call

--- client/src/app/services/differences-detection-handler/differences-detection-handler.service.ts
+++ client/src/app/services/differences-detection-handler/differences-detection-handler.service.ts
@@ -5,9 +5,9 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DialogGameOverComponent } from '@app/components/dialog-gameover/dialog-gameover.component';
 import { Vec2 } from '@app/interfaces/vec2';
 import { CommunicationService } from '@app/services/communication/communication.service';
-// import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
 import { TimerService } from '@app/services/timer.service';
 import { Coordinate } from '@common/coordinate';
+import { GameInformationHandlerService } from '../game-information-handler/game-information-handler.service';
 @Injectable({
     providedIn: 'root',
 })
@@ -18,11 +18,11 @@ export class DifferencesDetectionHandlerService {
     isGameOver: boolean = false;
     contextImgModified: CanvasRenderingContext2D;
 
-    // eslint-disable-next-line max-params
+    
     constructor(
         private timer: TimerService,
         public matDialog: MatDialog,
-        private readonly communicationService: CommunicationService, // private readonly gameInfoHandlerService: GameInformationHandlerService,
+        private readonly communicationService: CommunicationService, private readonly gameInfoHandlerService: GameInformationHandlerService,
     ) {}
 
     setGameOver() {
@@ -60,7 +60,7 @@ export class DifferencesDetectionHandlerService {
                     return;
                 }
 
-                // this.setNumberDifferencesFound(response.body.differencesLeft, this.gameInfoHandlerService.gameInformation.differences.length);
+                this.setNumberDifferencesFound(response.body.differencesLeft, this.gameInfoHandlerService.getNbDifferences());
                 this.timer.setNbOfDifferencesFound();
                 this.differenceDetected(ctx, this.contextImgModified, response.body.difference);
                 if (response.body.isGameOver) {

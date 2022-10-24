0c5730b42a42ad837a10836d4ee2a3915499f696
Xavier Jouslin <jouslin@nowray.net>
2022-10-24 10:41:52 -0400

    fix: name method

--- common/difference.ts
+++ common/difference.ts
@@ -3,5 +3,5 @@ import { Coordinate } from './coordinate';
 export interface DifferenceFound {
     difference: { coords: Coordinate[]; isPlayerFoundDifference: boolean };
     isGameOver: boolean;
-    differenceLeft: number;
+    nbDifferencesLeft: number;
 }

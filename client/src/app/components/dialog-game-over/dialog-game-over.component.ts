bad09644cf2155cfcfda5b9aa523ccb156654316
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-24 13:48:27 -0400

    fix : run lint

--- client/src/app/components/dialog-game-over/dialog-game-over.component.ts
+++ client/src/app/components/dialog-game-over/dialog-game-over.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dialog-game-over',
-  templateUrl: './dialog-game-over.component.html',
-  styleUrls: ['./dialog-game-over.component.scss']
+    selector: 'app-dialog-game-over',
+    templateUrl: './dialog-game-over.component.html',
+    styleUrls: ['./dialog-game-over.component.scss'],
 })
-export class DialogGameOverComponent {
-
-
-}
+export class DialogGameOverComponent {}

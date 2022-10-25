7dc587794c60e7d8c3d0437a578e3891db89d437
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-10-25 18:01:19 -0400

    feat: socketurl for prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'ec2-15-223-70-81.ca-central-1.compute.amazonaws.com',
+    serverUrl: 'http://ec2-15-223-70-81.ca-central-1.compute.amazonaws.com:3000/api',
+    socketUrl: 'http://ec2-15-223-70-81.ca-central-1.compute.amazonaws.com:3000',
 };

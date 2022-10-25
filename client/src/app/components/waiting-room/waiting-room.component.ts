2843ca95e117cb8fba4192929d10b431b2ab0b8b
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-10-25 12:14:20 -0400

    feat: default theme

--- client/src/app/components/waiting-room/waiting-room.component.ts
+++ client/src/app/components/waiting-room/waiting-room.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./waiting-room.component.scss'],
 })
 export class WaitingRoomComponent {
-    constructor() {}
+    favoriteTheme: string = 'deeppurple-amber-theme';
 }

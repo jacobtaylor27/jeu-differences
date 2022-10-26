7da8fff8011d7cf759dc0dc576d8597705b5b228
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-10-26 08:38:10 -0400

    more tests for deployement

--- server/app/controllers/game-controller/game.controller.ts
+++ server/app/controllers/game-controller/game.controller.ts
@@ -191,11 +191,10 @@ export class GameController {
                 const modify = new Bmp({ width: req.body.modify.width, height: req.body.modify.height }, req.body.modify.data as number[]);
                 const numberDifference = await this.gameValidation.numberDifference(original, modify, req.body.differenceRadius as number);
                 const differenceImage = await this.bmpSubtractor.getDifferenceBMP(original, modify, req.body.differenceRadius as number);
-                res.status(
-                    (await this.gameValidation.isNbDifferenceValid(original, modify, req.body.differenceRadius as number))
-                        ? StatusCodes.ACCEPTED
-                        : StatusCodes.NOT_ACCEPTABLE,
-                ).send({
+                const nbDifference = await this.gameValidation.isNbDifferenceValid(original, modify, req.body.differenceRadius as number);
+                // eslint-disable-next-line no-console
+                console.log(nbDifference);
+                res.status(nbDifference ? StatusCodes.ACCEPTED : StatusCodes.NOT_ACCEPTABLE).send({
                     numberDifference,
                     width: differenceImage.getWidth(),
                     height: differenceImage.getHeight(),

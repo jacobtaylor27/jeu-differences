4692f8ec0653bfec0aa13a589bc7cd300625d19c
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-26 15:35:57 -0400

    fix : clean up!

--- client/src/app/components/timer-stopwatch/timer-stopwatch.component.ts
+++ client/src/app/components/timer-stopwatch/timer-stopwatch.component.ts
@@ -13,53 +13,11 @@ export class TimerStopwatchComponent implements OnInit {
 
     timerDisplay: string;
 
-    // private secondsTotal: number;
-    // private sub: Subscription;
-    // private differenceFind: number = 0;
-
     constructor(private readonly socketService: CommunicationSocketService) {}
        
-        // timerService.differenceFind.subscribe(() => {
-        //     this.differenceFind++;
-        //);
-
-        // timerService.gameOver.subscribe(() => {
-        //     this.stopTimer();
-        // });
     ngOnInit(): void {
         this.socketService.on(SocketEvent.Clock, (time : number) =>{
             this.timerDisplay = TimeFormatter.getMMSSFormat(time);
         })
     }
-
-    // ngAfterViewInit(): void {
-    //     this.startTimer();
-    // }
-
-    // ngOnDestroy(): void {
-    //     this.timerService.resetNbDifferencesFound();
-    //     this.stopTimer();
-    // }
-
-    // private startTimer() {
-    //     /* eslint-disable @typescript-eslint/no-magic-numbers -- 1000 for 1second */
-    //     const time = timer(1, 1000);
-    //     this.sub = time.subscribe((seconds) => {
-    //         this.calculateTime(seconds);
-    //         this.timerDisplay = this.timerService.displayTime(this.secondsTotal);
-    //     });
-    // }
-
-    // private stopTimer() {
-    //     this.sub.unsubscribe();
-    // }
-
-    // // private setPreviousNbDifference(nb: number) {
-    // //     this.previousNbFound = nb;
-    // // }
-
-    // private calculateTime(seconds: number) {
-    //     /* eslint-disable @typescript-eslint/no-magic-numbers -- fixed value for now but will change later on */
-    //     this.secondsTotal = seconds - 5 * this.differenceFind;
-    // }
 }

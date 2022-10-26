2baac67c164c0197eba9642fa9273a49b1ab9eca
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-26 16:30:35 -0400

    feat : test timer client side

--- client/src/app/components/timer-stopwatch/timer-stopwatch.component.spec.ts
+++ client/src/app/components/timer-stopwatch/timer-stopwatch.component.spec.ts
@@ -1,16 +1,28 @@
 import { ComponentFixture, TestBed,  } from '@angular/core/testing';
+import { SocketTestHelper } from '@app/classes/socket-test-helper';
 import { CluesAreaComponent } from '@app/components/clues-area/clues-area.component';
 import { AppMaterialModule } from '@app/modules/material.module';
+import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
+import { SocketEvent } from '@common/socket-event';
+import { Socket } from 'socket.io-client';
 import { TimerStopwatchComponent } from './timer-stopwatch.component';
 
 describe('TimerStopwatchComponent', () => {
     let component: TimerStopwatchComponent;
     let fixture: ComponentFixture<TimerStopwatchComponent>;
 
+   let socketServiceMock : CommunicationSocketService;
+   let socketHelper: SocketTestHelper;
+
     beforeEach(async () => {
+        socketHelper = new SocketTestHelper();
+        socketServiceMock = new CommunicationSocketService();
+        socketServiceMock['socket'] = socketHelper as unknown as Socket;
+
         await TestBed.configureTestingModule({
             declarations: [TimerStopwatchComponent, CluesAreaComponent],
             imports: [AppMaterialModule],
+            providers: [{ provide: CommunicationSocketService, useValue: socketServiceMock }]
         }).compileComponents();
 
         fixture = TestBed.createComponent(TimerStopwatchComponent);
@@ -22,5 +34,11 @@ describe('TimerStopwatchComponent', () => {
         component.ngOnInit();
         expect(component).toBeTruthy();
     });
+
+    it('should set the display time', ()=>{
+        socketHelper.peerSideEmit(SocketEvent.Clock, '2');
+        component.ngOnInit();
+        expect(component.timerDisplay).toBe('00 : 02')
+    })
 
 });

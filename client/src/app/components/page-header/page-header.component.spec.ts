55348ffe9f24433b08d4e6d3c7fded5db32466af
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-10-26 21:27:43 -0400

    fix: removing error in header component

--- client/src/app/components/page-header/page-header.component.spec.ts
+++ client/src/app/components/page-header/page-header.component.spec.ts
@@ -1,15 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ExitGameButtonComponent } from '@app/components/exit-game-button/exit-game-button.component';
 import { PageHeaderComponent } from './page-header.component';
-
 describe('PageHeaderComponent', () => {
     let component: PageHeaderComponent;
     let fixture: ComponentFixture<PageHeaderComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [PageHeaderComponent],
+            declarations: [PageHeaderComponent, ExitGameButtonComponent],
             imports: [MatToolbarModule, MatIconModule],
         }).compileComponents();
 

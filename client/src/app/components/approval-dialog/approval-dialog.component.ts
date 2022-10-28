0b3f76224dbd28ce97ed83459e76e10bd0bd3ef3
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-10-28 12:42:52 -0400

    feat: new approval dialog

---
+++ client/src/app/components/approval-dialog/approval-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-approval-dialog',
+    templateUrl: './approval-dialog.component.html',
+    styleUrls: ['./approval-dialog.component.scss'],
+})
+export class ApprovalDialogComponent {
+    constructor() {}
+}

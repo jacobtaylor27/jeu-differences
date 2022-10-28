d6c9dd75abcb1463bc7681ef8d6e4fbd8f34b372
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-10-27 22:42:47 -0400

    fix: revert to old db

--- server/app/constants/database.ts
+++ server/app/constants/database.ts
@@ -1,7 +1,7 @@
-export const DB_USERNAME = 'root';
-export const DB_PASSWORD = 'hsqKWIOkW1vn3e05';
+export const DB_USERNAME = 'admin';
+export const DB_PASSWORD = 'mpwqKiEIeOimdmr5';
 
-export const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.vznwnci.mongodb.net/?retryWrites=true&w=majority`;
+export const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.7njxiw7.mongodb.net/?retryWrites=true&w=majority`;
 export const DB_NAME = 'seven-differences';
 
 export const DB_GAME_COLLECTION = 'game-service';

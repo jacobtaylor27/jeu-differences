22603504187c3fc03572e08ae1b5d40a7f4ab85b
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-10-27 20:13:20 -0400

    stuff moved around

--- client/src/app/services/differences-detection-handler/differences-detection-handler.service.ts
+++ client/src/app/services/differences-detection-handler/differences-detection-handler.service.ts
@@ -41,11 +41,11 @@ export class DifferencesDetectionHandlerService {
     }
 
     playWrongSound() {
-        this.playSound(new Audio('/assets/sounds/wronganswer.wav'));
+        this.playSound(new Audio('assets/wronganswer.wav'));
     }
 
     playCorrectSound() {
-        this.playSound(new Audio('/assets/sounds/correctanswer.wav'));
+        this.playSound(new Audio('assets/correctanswer.wav'));
     }
 
     playSound(sound: HTMLAudioElement) {

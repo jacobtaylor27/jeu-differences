373cf4de2ad3a37b922c131a3fc58b3da1ade0c7
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-10-29 15:38:39 -0400

    feat : on approve, accept player client side

--- client/src/app/components/approval-dialog/approval-dialog.component.ts
+++ client/src/app/components/approval-dialog/approval-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, Input } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
+import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
+import { SocketEvent } from '@common/socket-event';
 
 @Component({
     selector: 'app-approval-dialog',
@@ -14,13 +17,15 @@ export class ApprovalDialogComponent {
         @Inject(MAT_DIALOG_DATA)
         public data: {
             opponentsName : string
-        },
+        }, private socketService : CommunicationSocketService,  private readonly gameInformationHandlerService: GameInformationHandlerService,
     ){
+
         this.opponentsName = data.opponentsName}
 
     onClickApprove() {
         // eslint-disable-next-line no-console
         console.log('Approve');
+        this.socketService.send(SocketEvent.AcceptPlayer, {gameId : this.gameInformationHandlerService.gameId})
     }
 
     onClickReject() {

21e4711ff7686d7ae3c50d4e4c9630e866be2b75
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-11-01 09:22:37 -0400

    feat: adding test for enter key

--- client/src/app/components/chat-box/chat-box.component.spec.ts
+++ client/src/app/components/chat-box/chat-box.component.spec.ts
@@ -1,11 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { SocketTestHelper } from '@app/classes/socket-test-helper';
 import { ChatBoxComponent } from '@app/components/chat-box/chat-box.component';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
 import { Socket } from 'socket.io-client';
-
 class SocketClientServiceMock extends CommunicationSocketService {
     override connect() {}
 }
@@ -15,16 +15,21 @@ describe('ChatBoxComponent', () => {
     let fixture: ComponentFixture<ChatBoxComponent>;
     let socketServiceMock: SocketClientServiceMock;
     let socketHelper: SocketTestHelper;
+    let spyRouter: jasmine.SpyObj<Router>;
 
     beforeEach(async () => {
         socketHelper = new SocketTestHelper();
         socketServiceMock = new SocketClientServiceMock();
         socketServiceMock.socket = socketHelper as unknown as Socket;
+        spyRouter = jasmine.createSpyObj('Router', ['navigate']);
 
         await TestBed.configureTestingModule({
             declarations: [ChatBoxComponent],
             imports: [AppMaterialModule, BrowserAnimationsModule],
-            providers: [{ provide: CommunicationSocketService, useValue: socketServiceMock }],
+            providers: [
+                { provide: CommunicationSocketService, useValue: socketServiceMock },
+                { provide: Router, useValue: spyRouter },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(ChatBoxComponent);
@@ -34,5 +39,12 @@ describe('ChatBoxComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should send the message when enter key is pressed', () => {
+        const spyClick = spyOn(component, 'onClickSend');
+        const key = { key: 'Enter' } as KeyboardEvent;
+        component.onDialogClick(key);
+        expect(spyClick).toHaveBeenCalled();
     });
 });

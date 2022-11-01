f7e281942e2e9b9aaf37b36f814ba459341f2fa2
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-11-01 09:03:41 -0400

    feat: importing sockets services for tests

--- client/src/app/components/chat-box/chat-box.component.spec.ts
+++ client/src/app/components/chat-box/chat-box.component.spec.ts
@@ -2,11 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChatBoxComponent } from '@app/components/chat-box/chat-box.component';
 import { AppMaterialModule } from '@app/modules/material.module';
+import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
+import { SocketTestHelper } from '@app/classes/socket-test-helper';
+import { Socket } from 'socket.io-client';
+
+class SocketClientServiceMock extends CommunicationSocketService {
+    override connect() {}
+}
+
 describe('ChatBoxComponent', () => {
     let component: ChatBoxComponent;
     let fixture: ComponentFixture<ChatBoxComponent>;
+    let socketServiceMock: SocketClientServiceMock;
+    let socketHelper: SocketTestHelper;
 
     beforeEach(async () => {
+        socketHelper = new SocketTestHelper();
+        socketServiceMock = new SocketClientServiceMock();
+        socketServiceMock.socket = socketHelper as unknown as Socket;
         await TestBed.configureTestingModule({
             declarations: [ChatBoxComponent],
             imports: [AppMaterialModule, BrowserAnimationsModule],

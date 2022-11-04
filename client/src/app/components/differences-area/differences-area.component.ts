ec3451a005bd4b717790ed0f566576d041cda715
Xavier Jouslin <jouslin@nowray.net>
2022-11-04 17:58:57 -0400

    fix: intialize the players score

--- client/src/app/components/differences-area/differences-area.component.ts
+++ client/src/app/components/differences-area/differences-area.component.ts
@@ -10,17 +10,29 @@ import { GameInformationHandlerService } from '@app/services/game-information-ha
 export class DifferencesAreaComponent implements OnInit {
     name: string;
     nbDifferences: string;
-
+    players: { name: string; nbDifference: string }[];
     constructor(
         private readonly gameInformationHandlerService: GameInformationHandlerService,
         private readonly differenceDetectionHandler: DifferencesDetectionHandlerService,
     ) {
-        this.name = this.gameInformationHandlerService.getPlayerName();
+        const mainPlayer = this.gameInformationHandlerService.getPlayer();
+        const opponentPlayer = this.gameInformationHandlerService.getOpponent();
+        this.players = !opponentPlayer
+            ? [{ name: mainPlayer.name, nbDifference: this.setNbDifferencesFound(mainPlayer.name) as string }]
+            : [
+                  { name: mainPlayer.name, nbDifference: this.setNbDifferencesFound(mainPlayer.name) as string },
+                  { name: opponentPlayer.name, nbDifference: this.setNbDifferencesFound(opponentPlayer.name) as string },
+              ];
+        this.gameInformationHandlerService.$differenceFound.subscribe((playerName: string) => {
+            if (!this.getPlayerIndex(playerName)) {
+                return;
+            }
+            this.players[this.getPlayerIndex(playerName)].nbDifference = this.setNbDifferencesFound(playerName);
+        });
     }
 
     ngOnInit(): void {
         this.differenceDetectionHandler.resetNumberDifferencesFound();
-        this.setNbDifferencesFound();
     }
 
     getPlayersName() : string[] {

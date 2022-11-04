92bc504c17c42a399612435900b159d880442f72
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-11-04 12:40:32 -0400

    fix: remove is shown att

--- client/src/app/interfaces/game-card.ts
+++ client/src/app/interfaces/game-card.ts
@@ -2,7 +2,6 @@ import { PublicGameInformation } from '@common/game-information';
 
 export interface GameCard {
     gameInformation: PublicGameInformation;
-    isShown: boolean;
     isAdminCard: boolean;
     isMulti: boolean;
 }

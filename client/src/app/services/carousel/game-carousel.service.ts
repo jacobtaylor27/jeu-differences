cf1704edbc547091a7bb3bc221dc752c7b883588
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-11-04 12:40:41 -0400

    fix: refactor

--- client/src/app/services/carousel/game-carousel.service.ts
+++ client/src/app/services/carousel/game-carousel.service.ts
@@ -1,56 +1,55 @@
 import { Injectable } from '@angular/core';
 import { GameCard } from '@app/interfaces/game-card';
 import { GameCardHandlerService } from '@app/services/game-card-handler/game-card-handler.service';
+import { CarouselInformation } from '@common/carousel-information';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GameCarouselService {
+    games: GameCard[] = [];
+    carouselInformation: CarouselInformation = {
+        currentPage: 0,
+        gamesOnPage: 0,
+        nbOfGames: 0,
+        nbOfPages: 0,
+        hasNext: false,
+        hasPrevious: false,
+    };
+
     constructor(private readonly gameCardHandlerService: GameCardHandlerService) {}
 
     getCards(): GameCard[] {
         return this.gameCardHandlerService.getGameCards();
     }
 
+    setCarouselInformation(carouselInfo: CarouselInformation): void {
+        this.carouselInformation = carouselInfo;
+    }
+
     getNumberOfCards(): number {
-        return this.gameCardHandlerService.getNumberOfCards();
+        return this.carouselInformation.nbOfGames;
     }
 
     hasMoreThanOneCard(): boolean {
-        return this.gameCardHandlerService.hasMoreThanOneCard();
-    }
-
-    setCards(cards: GameCard[]) {
-        this.gameCardHandlerService.setCards(cards);
-    }
-
-    getCarouselLength(): number {
-        return this.gameCardHandlerService.getNumberOfCards();
+        return this.carouselInformation.nbOfGames > 1;
     }
 
     setCardMode(makeAdmin: boolean = false): void {
-        this.gameCardHandlerService.setCardMode(makeAdmin);
+        for (const gameCard of this.games) {
+            gameCard.isAdminCard = makeAdmin;
+        }
     }
 
     hasCards(): boolean {
-        return this.gameCardHandlerService.hasCards();
+        return this.carouselInformation.nbOfGames > 0;
     }
 
     hasPreviousCards(): boolean {
-        //
-        return false;
+        return this.carouselInformation.hasPrevious;
     }
 
     hasNextCards(): boolean {
-        //
-        return true;
-    }
-
-    showPreviousFour(): void {
-        // request previous four
-    }
-
-    showNextFour(): void {
-        // request next four
+        return this.carouselInformation.hasNext;
     }
 }

0eb6e29d516837331c9416ee5cbc34e250b23376
Xavier Jouslin <jouslin@nowray.net>
2022-11-04 13:34:36 -0400

    feat: handle win and lose socket event

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { DialogGameOverComponent } from '@app/components/dialog-game-over/dialog-game-over.component';
+import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
 import { ExitButtonHandlerService } from '@app/services/exit-button-handler/exit-button-handler.service';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
+import { SocketEvent } from '@common/socket-event';
 
 @Component({
     selector: 'app-game-page',
@@ -11,7 +15,17 @@ export class GamePageComponent {
     favoriteTheme: string = 'deeppurple-amber-theme';
     clock: string;
 
-    constructor(public gameInfoHandlerService: GameInformationHandlerService, exitButtonService: ExitButtonHandlerService) {
+    // eslint-disable-next-line max-params
+    constructor(
+        private dialog: MatDialog,
+        public gameInfoHandlerService: GameInformationHandlerService,
+        exitButtonService: ExitButtonHandlerService,
+        private socket: CommunicationSocketService,
+    ) {
         exitButtonService.setGamePage();
+        this.handleSocket();
     }
-}
+    handleSocket() {
+        this.socket.on(SocketEvent.Win, () => this.openGameOverDialog(true));
+        this.socket.on(SocketEvent.Lose, () => this.openGameOverDialog(false));
+    }

db230fbd31c8518a27a0c8448b96f09b8a32a1e8
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-11-07 15:35:36 -0500

    fix: removing console.log

--- client/src/app/components/dialog-upload-form/dialog-upload-form.component.ts
+++ client/src/app/components/dialog-upload-form/dialog-upload-form.component.ts
@@ -56,7 +56,6 @@ export class DialogUploadFormComponent {
     }
 
     onSubmit(): void {
-        console.log(this.toolService.$uploadImage);
         if (!this.isFormSubmitted || !this.data.canvas) {
             return;
         }

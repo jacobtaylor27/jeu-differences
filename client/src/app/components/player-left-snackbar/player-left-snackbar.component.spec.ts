d7e68342a5fb788dbffc5d485e20097d5faaa5b7
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-07 16:13:37 -0500

    feat : added missing import and providers

--- client/src/app/components/player-left-snackbar/player-left-snackbar.component.spec.ts
+++ client/src/app/components/player-left-snackbar/player-left-snackbar.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarRef, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { AppMaterialModule } from '@app/modules/material.module';
 
 import { PlayerLeftSnackbarComponent } from './player-left-snackbar.component';
 
@@ -9,6 +12,8 @@ describe('PlayerLeftSnackbarComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [PlayerLeftSnackbarComponent],
+            imports: [MatDialogModule, AppMaterialModule,],
+            providers: [{provide: MatSnackBarRef, useValue: {}},{provide: MAT_SNACK_BAR_DATA, useValue: {}}]
         }).compileComponents();
 
         fixture = TestBed.createComponent(PlayerLeftSnackbarComponent);

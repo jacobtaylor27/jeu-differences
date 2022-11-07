a95cd21295c34a4368170c3cfbad4effcbbb5984
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-07 11:28:37 -0500

    feat : new dialog component

---
+++ client/src/app/components/rejected-dialog/rejected-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RejectedDialogComponent } from './rejected-dialog.component';
+
+describe('RejectedDialogComponent', () => {
+  let component: RejectedDialogComponent;
+  let fixture: ComponentFixture<RejectedDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RejectedDialogComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RejectedDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/components/rejected-dialog/rejected-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rejected-dialog',
+  templateUrl: './rejected-dialog.component.html',
+  styleUrls: ['./rejected-dialog.component.scss']
+})
+export class RejectedDialogComponent {
+  favoriteTheme: string = 'deeppurple-amber-theme';
+
+}

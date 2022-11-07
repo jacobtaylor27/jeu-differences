864484b72144ca9eb1c0ee10a90c1422ab3fdf34
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-06 22:54:56 -0500

    feat : adding import

--- client/src/app/components/loading-screen/loading-screen.component.spec.ts
+++ client/src/app/components/loading-screen/loading-screen.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppMaterialModule } from '@app/modules/material.module';
+import { GameCarouselComponent } from '../game-carousel/game-carousel.component';
 
 import { LoadingScreenComponent } from './loading-screen.component';
 
@@ -8,7 +12,8 @@ describe('LoadingScreenComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [LoadingScreenComponent],
+            declarations: [LoadingScreenComponent, GameCarouselComponent],
+            imports: [AppMaterialModule, BrowserModule, ReactiveFormsModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(LoadingScreenComponent);

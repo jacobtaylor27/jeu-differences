4dd6ad2f65d47e618c04b9989a2751f1d0f335e6
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-11-08 13:20:58 -0500

    fix: removed unused routes

--- server/app/controllers/game-controller/game.controller.ts
+++ server/app/controllers/game-controller/game.controller.ts
@@ -2,25 +2,19 @@ import { Bmp } from '@app/classes/bmp/bmp';
 import { PrivateGameInformation } from '@app/interface/game-info';
 import { BmpSubtractorService } from '@app/services/bmp-subtractor-service/bmp-subtractor.service';
 import { GameInfoService } from '@app/services/game-info-service/game-info.service';
-import { GameManagerService } from '@app/services/game-manager-service/game-manager.service';
 import { GameValidation } from '@app/services/game-validation-service/game-validation.service';
 import { Request, Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { Service } from 'typedi';
+// this import can't be imported without require
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 import LZString = require('lz-string');
 
 @Service()
 export class GameController {
     router: Router;
-    // justification: nous avons encapsule la logique dans different service. Le controller s occupe d appeler ces service
-    // eslint-disable-next-line max-params
-    constructor(
-        private gameManager: GameManagerService,
-        private gameInfo: GameInfoService,
-        private gameValidation: GameValidation,
-        private bmpSubtractor: BmpSubtractorService,
-    ) {
+
+    constructor(private gameInfo: GameInfoService, private gameValidation: GameValidation, private bmpSubtractor: BmpSubtractorService) {
         this.configureRouter();
     }
 
@@ -93,28 +87,6 @@ export class GameController {
             }
         });
 
-        this.router.get('/cards/:id', (req: Request, res: Response) => {
-            this.gameInfo
-                .getGameInfoById(req.params.id)
-                .then((game: PrivateGameInformation) => {
-                    res.status(StatusCodes.OK).send({
-                        game: {
-                            id: game.id,
-                            name: game.name,
-                            thumbnail: game.thumbnail,
-                            nbDifferences: game.differences.length,
-                            idOriginalBmp: game.idOriginalBmp,
-                            idEditedBmp: game.idEditedBmp,
-                            multiplayerScore: game.multiplayerScore,
-                            soloScore: game.soloScore,
-                        },
-                    });
-                })
-                .catch(() => {
-                    res.status(StatusCodes.NOT_FOUND).send();
-                });
-        });
-
         this.router.post('/card/validation', async (req: Request, res: Response) => {
             if (!req.body.original || !req.body.modify || req.body.differenceRadius === undefined) {
                 res.status(StatusCodes.BAD_REQUEST).send();
@@ -162,53 +134,5 @@ export class GameController {
                     res.status(StatusCodes.NOT_ACCEPTABLE).send();
                 });
         });
-
-        this.router.post('/create/:id', (req: Request, res: Response) => {
-            if (!req.body.players || req.body.players.length === 0 || !req.body.mode || !req.params.id) {
-                res.status(StatusCodes.BAD_REQUEST).send();
-                return;
-            }
-            this.gameManager
-                .createGame({ player: req.body.players, isMulti: false }, req.body.mode as string, req.params.id)
-                .then((gameId: string) => {
-                    res.status(StatusCodes.CREATED).send({ id: gameId });
-                })
-                .catch(() => {
-                    res.status(StatusCodes.NOT_FOUND).send();
-                });
-        });
-
-        this.router.post('/difference', (req: Request, res: Response) => {
-            if (req.body.x === undefined || req.body.y === undefined || !this.gameManager.isGameFound(req.body.id)) {
-                res.status(StatusCodes.BAD_REQUEST).send();
-                return;
-            }
-            const difference = this.gameManager.isDifference(req.body.id as string, '', {
-                x: req.body.x,
-                y: req.body.y,
-            });
-            if (difference === null) {
-                res.status(StatusCodes.NOT_FOUND).send();
-                return;
-            }
-            res.status(StatusCodes.OK).send({
-                difference,
-                isGameOver: this.gameManager.isGameOver(req.body.id),
-                differencesLeft: this.gameManager.nbDifferencesLeft(req.body.id),
-            });
-        });
-
-        /*
-        this.router.get('/validate/bmp', (req: Request, res: Response) => {
-            // TODO: retourne le nommbre de différences et un image de différence
-        });
-        
-        this.router.get('/validate/:id/coord?', (req: Request, res: Response) => {
-            // TODO: 
-            // HTTP_STATUS.ACCEPTED (202)
-            //      return differenceArea = [{pixel, coordonnées},...]
-            // HTTP_STATUS.NOT_ACCEPTED (406)
-        });
-        */
     }
 }

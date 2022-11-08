a6f52453494cc5be391425cc917f6328d6fcb9e8
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-11-07 22:48:47 -0500

    feat: adding a message event service

---
+++ server/app/services/message-event-service/message-event.service.ts
@@ -0,0 +1,18 @@
+import { Service } from 'typedi';
+
+@Service()
+export class EventMessageService {
+    differenceFoundMessage(userName?: string | undefined, isMulti?: boolean | undefined) {
+        return isMulti && userName
+            ? `Difference trouvée par ${userName} a ${new Date().toLocaleTimeString()}`
+            : `Difference trouvée a ${new Date().toLocaleTimeString()}`;
+    }
+
+    differenceNotFoundMessage(userName?: string | undefined, isMulti?: boolean | undefined) {
+        return isMulti && userName ? `Erreur par ${userName} a ${new Date().toLocaleTimeString()}` : `Erreur a ${new Date().toLocaleTimeString()}`;
+    }
+
+    leavingGameMessage(userName: string | undefined) {
+        return `${userName} a abandonné la partie a ${new Date().toLocaleTimeString()}`;
+    }
+}

c60ad21d80475d5705bc13ff472796e95f64c0ab
Xavier Jouslin <jouslin@nowray.net>
2022-11-08 01:40:20 -0500

    test: toolbox service

--- client/src/app/services/tool-box/tool-box.service.spec.ts
+++ client/src/app/services/tool-box/tool-box.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { CanvasType } from '@app/enums/canvas-type';
 
 import { ToolBoxService } from './tool-box.service';
 
@@ -12,5 +13,19 @@ describe('ToolBoxService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should add CanvasType', () => {
+        const spyPencil = spyOn(service.$pencil, 'set');
+        const spyUploadImage = spyOn(service.$uploadImage, 'set');
+        const spyResetBackground = spyOn(service.$resetBackground, 'set');
+        const spyResetForeground = spyOn(service.$resetForeground, 'set');
+        const spySwitchForeground = spyOn(service.$switchForeground, 'set');
+        service.addCanvasType(CanvasType.Left);
+        expect(spyPencil).toHaveBeenCalled();
+        expect(spyResetBackground).toHaveBeenCalled();
+        expect(spyResetForeground).toHaveBeenCalled();
+        expect(spyUploadImage).toHaveBeenCalled();
+        expect(spySwitchForeground).toHaveBeenCalled();
     });
 });

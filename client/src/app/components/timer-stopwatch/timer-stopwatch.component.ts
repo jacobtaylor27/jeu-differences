836bb4a6785d3d5e4c7e4b6f817fa6f23a72fec1
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-11-08 16:45:58 -0500

    fix: remove import

--- client/src/app/components/timer-stopwatch/timer-stopwatch.component.ts
+++ client/src/app/components/timer-stopwatch/timer-stopwatch.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
 import { TimeFormatterService } from '@app/services/time-formatter/time-formatter.service';
 import { SocketEvent } from '@common/socket-event';

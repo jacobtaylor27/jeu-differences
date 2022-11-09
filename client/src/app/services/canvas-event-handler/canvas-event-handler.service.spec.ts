040e7d758146c8c13a9a43655d53ef846814d88e
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-11-08 21:35:25 -0500

    test: canvas event handler

--- client/src/app/services/canvas-event-handler/canvas-event-handler.service.spec.ts
+++ client/src/app/services/canvas-event-handler/canvas-event-handler.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
+import { DrawService } from '../draw-service/draw-service.service';
 
 import { CanvasEventHandlerService } from './canvas-event-handler.service';
 
 describe('CanvasEventHandlerService', () => {
     let service: CanvasEventHandlerService;
-
+    let drawServiceSpyObj: jasmine.SpyObj<DrawService>;
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        drawServiceSpyObj = jasmine.createSpyObj('DrawService', ['redo', 'undo']);
+        TestBed.configureTestingModule({
+            providers: [{ provide: DrawService, useValue: drawServiceSpyObj }],
+        });
         service = TestBed.inject(CanvasEventHandlerService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should handle ctrl shift z', () => {
+        drawServiceSpyObj.redo.and.callFake(() => {});
+        service.handleCtrlShiftZ();
+        expect(drawServiceSpyObj.redo).toHaveBeenCalled();
+    });
+
+    it('should handle ctrl shift z', () => {
+        drawServiceSpyObj.undo.and.callFake(() => {});
+        service.handleCtrlZ();
+        expect(drawServiceSpyObj.undo).toHaveBeenCalled();
     });
 });

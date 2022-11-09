7d0cfe38b3f725ea03206305950c73f5e9dd921c
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-08 23:50:06 -0500

    fix : lint

--- server/app/services/message-event-service/message-event.service.spec.ts
+++ server/app/services/message-event-service/message-event.service.spec.ts
@@ -12,43 +12,43 @@ describe('EventMessage Service', () => {
 
     it('Should return a multiplayer difference found message with the player name if exists and if its a multiplayer game', () => {
         const userName = 'user';
-        const expectedResult = `Difference trouvée par ${userName} a ${new Date().toLocaleString('en-US', {timeZone: 'America/New_York',})}`;
+        const expectedResult = `Difference trouvée par ${userName} a ${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}`;
         expect(eventMessageService.differenceFoundMessage(userName, true)).to.be.equal(expectedResult);
     });
 
     it('Should return a solo difference found message with the player name if exists and if its solo game', () => {
         const userName = 'user';
-        const expectedResult = `Difference trouvée a ${new Date().toLocaleString('en-US', {timeZone: 'America/New_York',})}`;
+        const expectedResult = `Difference trouvée a ${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}`;
         expect(eventMessageService.differenceFoundMessage(userName, false)).to.be.equal(expectedResult);
     });
 
     it('Should return a solo difference found message if player name is undefined even if its a multiplayer game', () => {
         const userName = undefined;
-        const expectedResult = `Difference trouvée a ${new Date().toLocaleString('en-US', {timeZone: 'America/New_York',})}`;
+        const expectedResult = `Difference trouvée a ${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}`;
         expect(eventMessageService.differenceFoundMessage(userName, true)).to.be.equal(expectedResult);
     });
 
     it('Should return a multiplayer difference not found message with the player name if exists and if its a multiplayer game', () => {
         const userName = 'user';
-        const expectedResult = `Erreur par ${userName} a ${new Date().toLocaleString('en-US', {timeZone: 'America/New_York',})}`;
+        const expectedResult = `Erreur par ${userName} a ${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}`;
         expect(eventMessageService.differenceNotFoundMessage(userName, true)).to.be.equal(expectedResult);
     });
 
     it('Should return a solo difference not found message with the player name if exists and if its a solo game', () => {
         const userName = 'user';
-        const expectedResult = `Erreur a ${new Date().toLocaleString('en-US', {timeZone: 'America/New_York',})}`;
+        const expectedResult = `Erreur a ${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}`;
         expect(eventMessageService.differenceNotFoundMessage(userName, false)).to.be.equal(expectedResult);
     });
 
     it('Should return a solo difference not found message if player name is undefined even if its a multiplayer game', () => {
         const userName = undefined;
-        const expectedResult = `Erreur a ${new Date().toLocaleString('en-US', {timeZone: 'America/New_York',})}`;
+        const expectedResult = `Erreur a ${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}`;
         expect(eventMessageService.differenceNotFoundMessage(userName, true)).to.be.equal(expectedResult);
     });
 
     it('Should return a leaving game message if username exists ', () => {
         const userName = 'user';
-        const expectedResult = `${userName} a abandonné la partie a ${new Date().toLocaleString('en-US', {timeZone: 'America/New_York',})}`;
+        const expectedResult = `${userName} a abandonné la partie a ${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}`;
         expect(eventMessageService.leavingGameMessage(userName)).to.be.equal(expectedResult);
     });
 
--- server/app/services/message-event-service/message-event.service.ts
+++ server/app/services/message-event-service/message-event.service.ts
@@ -4,12 +4,14 @@ import { Service } from 'typedi';
 export class EventMessageService {
     differenceFoundMessage(userName?: string | undefined, isMulti?: boolean | undefined) {
         return isMulti && userName
-            ? `Difference trouvée par ${userName} a ${new Date().toLocaleString('en-US', {timeZone: 'America/New_York',})}`
+            ? `Difference trouvée par ${userName} a ${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}`
             : `Difference trouvée a ${new Date().toLocaleTimeString()}`;
     }
 
     differenceNotFoundMessage(userName?: string | undefined, isMulti?: boolean | undefined) {
-        return isMulti && userName ? `Erreur par ${userName} a ${new Date().toLocaleTimeString()}` : `Erreur a ${new Date().toLocaleString('en-US', {timeZone: 'America/New_York',})}`;
+        return isMulti && userName
+            ? `Erreur par ${userName} a ${new Date().toLocaleTimeString()}`
+            : `Erreur a ${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}`;
     }
 
     leavingGameMessage(userName: string | undefined) {

c111225d7bb67bb77f8827e654ba0e1f076c7e81
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-12 16:17:37 -0500

    fix : added missing imports

--- client/src/app/components/refresh-snackbar/refresh-snackbar.component.spec.ts
+++ client/src/app/components/refresh-snackbar/refresh-snackbar.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarRef, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { AppMaterialModule } from '@app/modules/material.module';
 
 import { RefreshSnackbarComponent } from './refresh-snackbar.component';
 
@@ -9,8 +12,12 @@ describe('RefreshSnackbarComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [RefreshSnackbarComponent],
+            imports: [MatDialogModule, AppMaterialModule],
+            providers: [
+                { provide: MatSnackBarRef, useValue: {} },
+                { provide: MAT_SNACK_BAR_DATA, useValue: {} },
+            ],
         }).compileComponents();
-
         fixture = TestBed.createComponent(RefreshSnackbarComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();

aebe6c81a87d3dad79499dafa419c9f8ca2268c8
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-14 21:37:42 -0500

    fix : lint

--- client/src/app/components/dialog-limited-time/dialog-limited-time.component.spec.ts
+++ client/src/app/components/dialog-limited-time/dialog-limited-time.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DialogLimitedTimeComponent } from './dialog-limited-time.component';
 
 describe('DialogLimitedTimeComponent', () => {
-  let component: DialogLimitedTimeComponent;
-  let fixture: ComponentFixture<DialogLimitedTimeComponent>;
+    let component: DialogLimitedTimeComponent;
+    let fixture: ComponentFixture<DialogLimitedTimeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DialogLimitedTimeComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DialogLimitedTimeComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DialogLimitedTimeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DialogLimitedTimeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

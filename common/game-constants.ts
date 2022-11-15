4ee01631aaf88ed851cc41f2a90ce0e7c79ac415
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-11-14 20:18:17 -0500

    feat: new gameconstants

---
+++ common/game-constants.ts
@@ -0,0 +1,11 @@
+export const GameConstants:Map<string, number> = new Map<string, number>([
+    ['gameTime', 35],
+    ['penaltyTime', 5],
+    ['successTime', 5],
+]);
+
+export const GameConstantsDefault:Map<string, number> = new Map<string, number>([
+    ['gameTime', 30],
+    ['penaltyTime', 5],
+    ['successTime', 5],
+]);

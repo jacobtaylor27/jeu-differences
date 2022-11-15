f73ab8e6762f271936c2620e556a829fbc1c9f7f
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-15 12:39:30 -0500

    feat : added missing imports dialog tests

--- client/src/app/components/dialog-limited-time/dialog-limited-time.component.spec.ts
+++ client/src/app/components/dialog-limited-time/dialog-limited-time.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppMaterialModule } from '@app/modules/material.module';
+import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
+import { GameMode } from '@common/game-mode';
 
 import { DialogLimitedTimeComponent } from './dialog-limited-time.component';
 
 describe('DialogLimitedTimeComponent', () => {
     let component: DialogLimitedTimeComponent;
     let fixture: ComponentFixture<DialogLimitedTimeComponent>;
+    let gameInformationHandlerService: jasmine.SpyObj<GameInformationHandlerService>;
 
     beforeEach(async () => {
+        gameInformationHandlerService = jasmine.createSpyObj('GameInformationHandlerService', ['setPlayerName', 'getPlayer', 'handleSocketEvent']);
+        gameInformationHandlerService.players = [{ name: '', nbDifferences: 2 }];
+        gameInformationHandlerService.gameMode = GameMode.LimitedTime;
         await TestBed.configureTestingModule({
             declarations: [DialogLimitedTimeComponent],
+            imports: [AppMaterialModule, RouterTestingModule, BrowserAnimationsModule, ReactiveFormsModule],
+            providers: [{ provide: GameInformationHandlerService, useValue: gameInformationHandlerService }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(DialogLimitedTimeComponent);

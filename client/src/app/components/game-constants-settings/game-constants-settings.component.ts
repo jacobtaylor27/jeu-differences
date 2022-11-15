36987e09a70057d46163f3ba9638f2248faf60e5
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-11-14 20:18:35 -0500

    feat: method to get values

--- client/src/app/components/game-constants-settings/game-constants-settings.component.ts
+++ client/src/app/components/game-constants-settings/game-constants-settings.component.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion -- values are stored in a map and are always present */
 import { Component } from '@angular/core';
+import { Theme } from '@app/enums/theme';
+import { GameConstants, GameConstantsDefault } from '@common/game-constants';
 
 @Component({
     selector: 'app-game-constants-settings',
@@ -6,5 +9,49 @@ import { Component } from '@angular/core';
     styleUrls: ['./game-constants-settings.component.scss'],
 })
 export class GameConstantsSettingsComponent {
-    favoriteTheme: string = 'deeppurple-amber-theme';
+    favoriteTheme: string = Theme.ClassName;
+
+    onClickRestoreDefaultValues(): void {
+        this.setGameTime(this.getDefaultGameTime());
+
+        this.setPenaltyTime(this.getDefaultPenaltyTime());
+
+        this.setSuccessTime(this.getDefaultSuccessTime());
+    }
+
+    getGameTime(): number {
+        return GameConstants.get('gameTime')!;
+    }
+
+    setGameTime(value: number): void {
+        GameConstants.set('gameTime', value);
+    }
+
+    getPenaltyTime(): number {
+        return GameConstants.get('penaltyTime')!;
+    }
+
+    setPenaltyTime(value: number): void {
+        GameConstants.set('penaltyTime', value);
+    }
+
+    getSuccessTime(): number {
+        return GameConstants.get('successTime')!;
+    }
+
+    setSuccessTime(value: number): void {
+        GameConstants.set('successTime', value);
+    }
+
+    getDefaultGameTime(): number {
+        return GameConstantsDefault.get('gameTime')!;
+    }
+
+    getDefaultPenaltyTime(): number {
+        return GameConstantsDefault.get('penaltyTime')!;
+    }
+
+    getDefaultSuccessTime(): number {
+        return GameConstantsDefault.get('successTime')!;
+    }
 }

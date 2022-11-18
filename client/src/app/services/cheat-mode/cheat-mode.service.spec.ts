e01aa32a3eb6f9664f23b4b019052f5d94d19bad
Xavier Jouslin <jouslin@nowray.net>
2022-11-18 16:03:34 -0500

    fix: import module for test

--- client/src/app/services/cheat-mode/cheat-mode.service.spec.ts
+++ client/src/app/services/cheat-mode/cheat-mode.service.spec.ts
@@ -1,12 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppMaterialModule } from '@app/modules/material.module';
+import { DifferencesDetectionHandlerService } from '@app/services/differences-detection-handler/differences-detection-handler.service';
 
 import { CheatModeService } from './cheat-mode.service';
 
 describe('CheatModeService', () => {
     let service: CheatModeService;
-
+    let differenceDetectionHandlerSpyObj: DifferencesDetectionHandlerService;
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        differenceDetectionHandlerSpyObj = jasmine.createSpyObj('DifferenceDetectionHandler', ['displayDifferenceTemp']);
+        TestBed.configureTestingModule({
+            imports: [AppMaterialModule, HttpClientModule, RouterTestingModule],
+            providers: [{ provide: DifferencesDetectionHandlerService, useValue: differenceDetectionHandlerSpyObj }],
+        });
         service = TestBed.inject(CheatModeService);
     });
 

4513a2c3ec9ebfe470005754aeff4431122dc719
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-11-17 20:45:24 -0500

    fix: bfs working

--- server/app/services/bmp-difference-interpreter-service/bmp-difference-interpreter.service.ts
+++ server/app/services/bmp-difference-interpreter-service/bmp-difference-interpreter.service.ts
@@ -13,7 +13,7 @@ export class BmpDifferenceInterpreter {
         for (let row = 0; row < pixels.length; row++) {
             for (let column = 0; column < pixels[row].length; column++) {
                 if (!pixels[row][column].isVisited && pixels[row][column].isBlack()) {
-                    const difference = this.depthFirstSearch(pixels, row, column);
+                    const difference = this.breadthFirstSearch(pixels, row, column);
                     differences.push(difference);
                 }
             }
@@ -21,37 +21,20 @@ export class BmpDifferenceInterpreter {
         return differences;
     }
 
-    private depthFirstSearch(pixels: Pixel[][], row: number, column: number): Coordinate[] {
-        pixels[row][column].isVisited = true;
-        let differenceArea: Coordinate[] = [{ x: column, y: row }];
-        const pixelNeighborsCoordinates = this.pixelNeighborsCoord({ x: row, y: column });
+    // private depthFirstSearch(pixels: Pixel[][], row: number, column: number): Coordinate[] {
+    //     pixels[row][column].isVisited = true;
+    //     let differenceArea: Coordinate[] = [{ x: column, y: row }];
+    //     const pixelNeighborsCoordinates = this.pixelNeighborsCoord({ x: row, y: column });
 
-        for (let i = 0; i < pixelNeighborsCoordinates.length; i++) {
-            const coordinate: Coordinate = { x: pixelNeighborsCoordinates[i].x, y: pixelNeighborsCoordinates[i].y };
-            if (!pixels[coordinate.x][coordinate.y].isVisited && pixels[coordinate.x][coordinate.y].isBlack()) {
-                const newDfs = this.depthFirstSearch(pixels, coordinate.x, coordinate.y);
-                differenceArea = differenceArea.concat(newDfs);
-            }
-        }
-        return differenceArea;
-    }
-
-    private pixelNeighborsCoord(pixel: Coordinate): Coordinate[] {
-        const coordinateResult: Coordinate[] = [];
-        for (let i = -1; i <= 1; i++) {
-            for (let j = -1; j <= 1; j++) {
-                const offsetCoord: Coordinate = { x: pixel.x + i, y: pixel.y + j };
-                if (this.isCoordinateValid(offsetCoord) && (offsetCoord.x !== pixel.x || offsetCoord.y !== pixel.y)) {
-                    coordinateResult.push(offsetCoord);
-                }
-            }
-        }
-        return coordinateResult;
-    }
-
-    private isCoordinateValid(coord: Coordinate) {
-        return coord.x >= 0 && coord.x < DEFAULT_IMAGE_HEIGHT && coord.y >= 0 && coord.y < DEFAULT_IMAGE_WIDTH;
-    }
+    //     for (let i = 0; i < pixelNeighborsCoordinates.length; i++) {
+    //         const coordinate: Coordinate = { x: pixelNeighborsCoordinates[i].x, y: pixelNeighborsCoordinates[i].y };
+    //         if (!pixels[coordinate.x][coordinate.y].isVisited && pixels[coordinate.x][coordinate.y].isBlack()) {
+    //             const newDfs = this.depthFirstSearch(pixels, coordinate.x, coordinate.y);
+    //             differenceArea = differenceArea.concat(newDfs);
+    //         }
+    //     }
+    //     return differenceArea;
+    // }
 
     private breadthFirstSearch(pixels: Pixel[][], row: number, column: number): Coordinate[] {
         const queue = new Queue();
@@ -78,5 +61,22 @@ export class BmpDifferenceInterpreter {
             }
         }
         return differenceArea;
+    }
+
+    private pixelNeighborsCoord(pixel: Coordinate): Coordinate[] {
+        const coordinateResult: Coordinate[] = [];
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                const offsetCoord: Coordinate = { x: pixel.x + i, y: pixel.y + j };
+                if (this.isCoordinateValid(offsetCoord) && (offsetCoord.x !== pixel.x || offsetCoord.y !== pixel.y)) {
+                    coordinateResult.push(offsetCoord);
+                }
+            }
+        }
+        return coordinateResult;
+    }
+
+    private isCoordinateValid(coord: Coordinate) {
+        return coord.x >= 0 && coord.x < DEFAULT_IMAGE_HEIGHT && coord.y >= 0 && coord.y < DEFAULT_IMAGE_WIDTH;
     }
 }

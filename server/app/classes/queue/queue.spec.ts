f98f2c562cd33b2377a7003883e5316f1a60609a
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-11-17 22:55:54 -0500

    feat: remove method in Queue class

--- server/app/classes/queue/queue.spec.ts
+++ server/app/classes/queue/queue.spec.ts
@@ -10,4 +10,13 @@ describe('Queue', () => {
         queue.add(coordinateToadd);
         expect(queue['array'][0]).to.equal(coordinateToadd);
     });
+
+    it.only('Should remove the first coord from the queue when using the remove method ', () => {
+        const queue = new Queue();
+        queue.add({ x: 2, y: 5 });
+        queue.add({ x: 1, y: 1 });
+        const expectedCoordinate: Coordinate = { x: 1, y: 1 };
+        queue.remove();
+        expect(queue['array'][0]).to.deep.eq(expectedCoordinate);
+    });
 });

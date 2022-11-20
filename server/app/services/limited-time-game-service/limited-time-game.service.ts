0a4415bd3a56a71894496ae8f67444263da2edea
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-20 13:45:54 -0500

    feat : new service to shuffle all the game cards available

---
+++ server/app/services/limited-time-game-service/limited-time-game.service.ts
@@ -0,0 +1,27 @@
+import { PrivateGameInformation } from '@app/interface/game-info';
+import { Service } from 'typedi';
+import { GameInfoService } from '@app/services/game-info-service/game-info.service';
+
+@Service()
+export class LimitedTimeGame {
+    constructor(private readonly gameInfoService: GameInfoService) {}
+
+    async generateGames() {
+        const allGames = await this.gameInfoService.getAllGameInfos();
+        return this.shuffle(allGames);
+    }
+
+    private shuffle(array: PrivateGameInformation[]) {
+        let currentIndex = array.length;
+        let randomIndex;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+}

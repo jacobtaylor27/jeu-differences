e9c1002e3ddb555fc3d52687aa025e5e083bd972
Xavier Jouslin <jouslin@nowray.net>
2022-11-20 13:07:39 -0500

    fix: add FlashTimer enum

--- client/src/app/constants/game-constants.ts
+++ client/src/app/constants/game-constants.ts
@@ -1,3 +1,7 @@
 export const HINT_PENALTY_TIME = 5;
 export const SUCCESSFUL_ANSWER_TIME_BONUS = 5;
 export const TIMER_TIME = 30;
+export enum FlashTimer {
+    CheatMode = 125,
+    Classic = 500,
+}

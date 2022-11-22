5e4728d2cdea371133e6e1039708f62226e194aa
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-11-22 15:58:01 -0500

    test: calling leaveCanvas and enterCanvas methode

--- client/src/app/components/draw-canvas/draw-canvas.component.spec.ts
+++ client/src/app/components/draw-canvas/draw-canvas.component.spec.ts
@@ -28,6 +28,8 @@ describe('DrawCanvasComponent', () => {
                 'stopDrawing',
                 'clearAllLayers',
                 'clearAllBackground',
+                'leaveCanvas',
+                'enterCanvas',
             ],
             {
                 $drawingImage: new Map<CanvasType, Subject<ImageData>>(),
@@ -81,34 +83,24 @@ describe('DrawCanvasComponent', () => {
         expect(drawServiceSpyObj.draw).toHaveBeenCalled();
     });
 
-    // not work for now
-    // it('should receive a new pencil', () => {
-    //     const expectedPencil = { cap: 'round', width: { pencil: 1, eraser: 2 }, state: Tool.Pencil, color: '#000000' } as Pencil;
-    //     toolBoxServiceSpyObj.$pencil.get(component.canvasType)?.subscribe((newPencil: Pencil) => {
-    //         expect(component.pencil).toEqual(newPencil);
-    //     });
-    //     component.ngAfterViewInit();
-    //     toolBoxServiceSpyObj.$pencil.get(component.canvasType)?.next(expectedPencil);
-    // });
-
-    // not work for now
-    // it('should subscribe to get the new image and draw it', async () => {
-    //     const ctx = component.background.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-    //     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    //     drawServiceSpyObj.updateImage.and.callFake(() => {});
-    //     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    //     spyOn(ctx, 'drawImage').and.callFake(() => {});
-    //     toolBoxServiceSpyObj.$uploadImage.get(component.canvasType)?.subscribe(() => {
-    //         expect(drawServiceSpyObj.updateImage).toHaveBeenCalled();
-    //     });
-    //     component.ngAfterViewInit();
-    //     toolBoxServiceSpyObj.$uploadImage.get(component.canvasType)?.next({} as ImageBitmap);
-    // });
-
     it('should have the current command to eraser', () => {
         // eslint-disable-next-line @typescript-eslint/no-empty-function
         drawServiceSpyObj.stopDrawing.and.callFake(() => {});
         component.stopDrawing();
         expect(drawServiceSpyObj.stopDrawing).toHaveBeenCalled();
+    });
+
+    it('should call enterCanvas when the client is entering the canvas', () => {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function, no-unused-vars
+        drawServiceSpyObj.enterCanvas.and.callFake((event: MouseEvent) => {});
+        component.enterCanvas({} as MouseEvent);
+        expect(drawServiceSpyObj.enterCanvas).toHaveBeenCalled();
+    });
+
+    it('should call leaveCanvas when the client is leaving the canvas', () => {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function, no-unused-vars
+        drawServiceSpyObj.leaveCanvas.and.callFake((event: MouseEvent) => {});
+        component.leaveCanvas({} as MouseEvent);
+        expect(drawServiceSpyObj.leaveCanvas).toHaveBeenCalled();
     });
 });

4070878d74c89edd2d4709128d59b690909b70b0
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-11-23 13:49:33 -0500

    feat: importing gameManager

--- server/app/services/clues-service/clues.service.ts
+++ server/app/services/clues-service/clues.service.ts
@@ -1,11 +1,11 @@
-import { Game } from '@app/classes/game/game';
 import { Coordinate } from '@common/coordinate';
+import { GameManagerService } from '../game-manager-service/game-manager.service';
 
 export class CluesService {
-    constructor(private readonly game: Game){}
+    constructor(private readonly gameManager: GameManagerService) {}
 
-    findRandomDifference(): Coordinate[] {
-        this.game.
+    findRandomDifference(gameId: string): Coordinate[] {
+        this.gameManager.getGameInfo(gameId);
     }
 
     findRandomPixel(difference: Coordinate[]): Coordinate {

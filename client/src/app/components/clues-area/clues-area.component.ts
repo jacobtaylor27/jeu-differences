907de8918f5dcf0389d37b667fb7ddf53f437842
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-11-23 13:11:50 -0500

    feat: sending socket

--- client/src/app/components/clues-area/clues-area.component.ts
+++ client/src/app/components/clues-area/clues-area.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CluesService } from '@app/services/clues-service/clues.service';
+import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
+import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
+import { SocketEvent } from '@common/socket-event';
 @Component({
     selector: 'app-clues-area',
     templateUrl: './clues-area.component.html',
@@ -10,13 +13,18 @@ export class CluesAreaComponent {
     isDisabled: boolean = false;
     private numberOfClues: number = 3;
 
-    constructor(private readonly clueService: CluesService) {}
+    constructor(
+        public clueService: CluesService,
+        public communicationSocket: CommunicationSocketService,
+        public gameInformation: GameInformationHandlerService,
+    ) {}
 
     getClue() {
         this.clueAskedCounter++;
         if (this.clueAskedCounter === this.numberOfClues) {
             this.isDisabled = true;
         }
+        this.communicationSocket.send(SocketEvent.Clue, { clueIndex: this.clueAskedCounter, gameId: this.gameInformation.getId()});
         console.log(this.clueService.foo);
     }
 

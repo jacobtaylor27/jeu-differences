c50fa06e37f0906d696ad784e17fa9c3bf6957c5
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-11-23 08:12:09 -0500

    fix: commentaire

--- client/src/app/components/clues-area/clues-area.component.ts
+++ client/src/app/components/clues-area/clues-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { CluesService } from '@app/services/clues-service/clues.service';
 @Component({
     selector: 'app-clues-area',
@@ -6,28 +6,26 @@ import { CluesService } from '@app/services/clues-service/clues.service';
     styleUrls: ['./clues-area.component.scss'],
 })
 export class CluesAreaComponent {
-    @Output() clueCounter = new EventEmitter<number>();
-
     clueAskedCounter: number = 0;
     isDisabled: boolean = false;
     private numberOfClues: number = 3;
 
     constructor(private readonly clueService: CluesService) {}
 
-    @HostListener('window: keydown', ['$event'])
-    buttonDetect(event: KeyboardEvent) {
-        if (event.key === 'i') {
-            if (!this.isDisabled) {
-                this.getClue();
-            }
-        }
-    }
-
     getClue() {
         this.clueAskedCounter++;
-        this.clueCounter.emit(this.clueAskedCounter);
         if (this.clueAskedCounter === this.numberOfClues) {
             this.isDisabled = true;
         }
+        console.log(this.clueService.foo);
     }
+
+    // @HostListener('window: keydown', ['$event'])
+    // buttonDetect(event: KeyboardEvent) {
+    //     if (event.key === 'i') {
+    //         if (!this.isDisabled) {
+    //             this.getClue();
+    //         }
+    //     }
+    // }
 }
--- client/src/app/services/clues-service/clues.service.ts
+++ client/src/app/services/clues-service/clues.service.ts
@@ -3,4 +3,20 @@ import { Injectable } from '@angular/core';
 @Injectable({
     providedIn: 'root',
 })
-export class CluesService {}
+export class CluesService {
+    foo: boolean = true;
+
+    // access for all differences
+    // choose a random game between them and knowing which one it is. (cote server);
+    // knowing in which quadrant a game is +  whats the difference
+    // coloring in grey for few seconds the rest of the difference images ;
+
+    // socket event clue(sending clue,gameId)
+    // socket event . send (clue number,gameId)
+    // socket event .on (je dessine).
+    // hint1 method.
+    // hint2 method.
+    // hint3 method.
+    // acces a l'array de diffence
+    // tout les differences sont dans PrivateGameInformation et les differences trouver sont dans le set.
+}

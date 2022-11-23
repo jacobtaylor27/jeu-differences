f70a64775ec7645ac42d51d55d998f61f5df0100
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-11-23 14:59:20 -0500

    test: switchForegroundImageData

--- client/src/app/services/draw-service/draw-service.service.ts
+++ client/src/app/services/draw-service/draw-service.service.ts
@@ -160,17 +160,8 @@ export class DrawService {
         const rightCanvas = this.canvasStateService.getCanvasState(CanvasType.Right);
 
         if (leftCanvas && rightCanvas) {
-            this.addCurrentCommand(new SwitchForegroundCommand(leftCanvas, rightCanvas, this));
+            this.addCurrentCommand(new SwitchForegroundCommand(leftCanvas, rightCanvas));
         }
-    }
-
-    switchForegroundImageData(primaryCanvasState: DrawingBoardState, secondCanvasState: DrawingBoardState) {
-        const primaryForeground = primaryCanvasState.foreground.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-        const secondForeground = secondCanvasState.foreground.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-        const leftImageData = primaryForeground.getImageData(0, 0, Canvas.WIDTH, Canvas.HEIGHT);
-        const rightImageData = secondForeground.getImageData(0, 0, Canvas.WIDTH, Canvas.HEIGHT);
-        primaryForeground.putImageData(rightImageData, 0, 0);
-        secondForeground.putImageData(leftImageData, 0, 0);
     }
 
     pasteExternalForegroundOn(canvasType: CanvasType) {

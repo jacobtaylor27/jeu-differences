caabdc28e5c072240ce94bb6829e3fd1c9d5c1f3
Xavier Jouslin <jouslin@nowray.net>
2022-11-26 14:08:20 -0500

    test: should set new pencil on draw canvas

--- client/src/app/components/draw-canvas/draw-canvas.component.spec.ts
+++ client/src/app/components/draw-canvas/draw-canvas.component.spec.ts
@@ -111,6 +111,15 @@ describe('DrawCanvasComponent', () => {
         expect(drawServiceSpyObj.leaveCanvas).toHaveBeenCalled();
     });
 
+    it('should change pencil', () => {
+        const newPencil = {} as Pencil;
+        toolBoxServiceSpyObj.$pencil.get(CanvasType.Left)?.subscribe(() => {
+            expect(component.pencil).toEqual(newPencil);
+        });
+        component.ngAfterViewInit();
+        toolBoxServiceSpyObj.$pencil.get(CanvasType.Left)?.next(newPencil);
+    });
+
     it('should upload image', () => {
         const newPencil = {} as Pencil;
         toolBoxServiceSpyObj.$pencil.get(CanvasType.Left)?.subscribe(() => {

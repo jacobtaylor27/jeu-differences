6f48c48e50d0b55847b8a06e2039961838a19acc
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-26 21:07:29 -0500

    feat : change htlm to context

--- client/src/app/components/player-left-snackbar/player-left-snackbar.component.ts
+++ client/src/app/components/player-left-snackbar/player-left-snackbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { MatSnackBarRef, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 import { Theme } from '@app/enums/theme';
+import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
+import { GameMode } from '@common/game-mode';
 
 @Component({
     selector: 'app-player-left-snackbar',
@@ -9,5 +11,17 @@ import { Theme } from '@app/enums/theme';
 })
 export class PlayerLeftSnackbarComponent {
     theme: typeof Theme = Theme;
-    constructor(public snackBarRef: MatSnackBarRef<PlayerLeftSnackbarComponent>, @Inject(MAT_SNACK_BAR_DATA) public data: unknown) {}
+    constructor(
+        public snackBarRef: MatSnackBarRef<PlayerLeftSnackbarComponent>,
+        @Inject(MAT_SNACK_BAR_DATA) public data: unknown,
+        public gameInfoHandlerService: GameInformationHandlerService,
+    ) {}
+
+    messageSnackBar() {
+        if (this.gameInfoHandlerService.gameMode === GameMode.LimitedTime) {
+            return 'Le joueur a quitté la partie. Vous jouez maintenant en solo.';
+        } else {
+            return 'Le joueur a quitté la partie.';
+        }
+    }
 }

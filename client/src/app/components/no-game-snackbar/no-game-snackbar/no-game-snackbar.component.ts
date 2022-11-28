67e60f038b95ec1719df3a13fefedc14074c5ddc
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-28 10:04:43 -0500

    feat : import theme

--- client/src/app/components/no-game-snackbar/no-game-snackbar/no-game-snackbar.component.ts
+++ client/src/app/components/no-game-snackbar/no-game-snackbar/no-game-snackbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Theme } from '@app/enums/theme';
 
 @Component({
   selector: 'app-no-game-snackbar',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./no-game-snackbar.component.scss']
 })
 export class NoGameSnackbarComponent {
-
+    theme: typeof Theme = Theme;
   constructor() { }
 
 

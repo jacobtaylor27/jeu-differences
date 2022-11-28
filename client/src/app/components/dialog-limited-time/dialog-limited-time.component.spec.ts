86a426ed5d7fe66a0f05129b9657b0e899d78ea4
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-28 11:08:40 -0500

    feat : test coop no game available

--- client/src/app/components/dialog-limited-time/dialog-limited-time.component.spec.ts
+++ client/src/app/components/dialog-limited-time/dialog-limited-time.component.spec.ts
@@ -58,6 +58,16 @@ describe('DialogLimitedTimeComponent', () => {
         expect(spySocket).toHaveBeenCalled();
         expect(gameInformationHandlerService.handleSocketEvent).toHaveBeenCalled();
     });
+
+    it('should not send Socket Event and handle socket on click coop if no game available', () => {
+        spyOn(component, 'noGameAvailable').and.callFake(() => true);
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- calls fake and return {}
+        const spySocket = spyOn(component['communicationSocketService'], 'send').and.callFake(() => {});
+        component.onClickCoop();
+        expect(spySocket).not.toHaveBeenCalled();
+        expect(gameInformationHandlerService.handleSocketEvent).not.toHaveBeenCalled();
+    });
+
     it('should return false if game are available ', () => {
         spyOn(component['gameCarouselService'], 'getNumberOfCards').and.callFake(() => 1);
         expect(component.noGameAvailable()).toEqual(false);

d8d8abd6c34659412ad36406e0fd2e5338502a19
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-28 09:46:49 -0500

    feat : added new component in app.modules.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -40,6 +40,7 @@ import { GameSelectionPageComponent } from './pages/game-selection-page/game-sel
 import { WaitingRoomComponent } from './pages/waiting-room/waiting-room.component';
 import { RefreshSnackbarComponent } from './components/refresh-snackbar/refresh-snackbar.component';
 import { DialogLimitedTimeComponent } from './components/dialog-limited-time/dialog-limited-time.component';
+import { NoGameSnackbarComponent } from './components/no-game-snackbar/no-game-snackbar/no-game-snackbar.component';
 /**
  * Main module that is used in main.ts.
  * All automatically generated components will appear in this module.
@@ -84,6 +85,7 @@ import { DialogLimitedTimeComponent } from './components/dialog-limited-time/dia
         RejectedDialogComponent,
         RefreshSnackbarComponent,
         DialogLimitedTimeComponent,
+        NoGameSnackbarComponent,
     ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
     providers: [],

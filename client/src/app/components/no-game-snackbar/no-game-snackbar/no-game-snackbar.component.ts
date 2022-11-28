deb33ab035e67c8d036bb69c737b817608086301
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-28 10:05:13 -0500

    fix : lint

--- client/src/app/components/no-game-snackbar/no-game-snackbar/no-game-snackbar.component.ts
+++ client/src/app/components/no-game-snackbar/no-game-snackbar/no-game-snackbar.component.ts
@@ -3,9 +3,9 @@ import { MatSnackBarRef } from '@angular/material/snack-bar';
 import { Theme } from '@app/enums/theme';
 
 @Component({
-  selector: 'app-no-game-snackbar',
-  templateUrl: './no-game-snackbar.component.html',
-  styleUrls: ['./no-game-snackbar.component.scss']
+    selector: 'app-no-game-snackbar',
+    templateUrl: './no-game-snackbar.component.html',
+    styleUrls: ['./no-game-snackbar.component.scss'],
 })
 export class NoGameSnackbarComponent {
     theme: typeof Theme = Theme;

ae3ae4bdf43af6fc6306c5248b9f3c336b9a21f5
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-28 11:06:34 -0500

    feat : snack bar test

--- client/src/app/components/no-game-snackbar/no-game-snackbar/no-game-snackbar.component.spec.ts
+++ client/src/app/components/no-game-snackbar/no-game-snackbar/no-game-snackbar.component.spec.ts
@@ -1,23 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarRef } from '@angular/material/snack-bar';
+import { AppMaterialModule } from '@app/modules/material.module';
 
 import { NoGameSnackbarComponent } from './no-game-snackbar.component';
 
 describe('NoGameSnackbarComponent', () => {
-  let component: NoGameSnackbarComponent;
-  let fixture: ComponentFixture<NoGameSnackbarComponent>;
+    let component: NoGameSnackbarComponent;
+    let fixture: ComponentFixture<NoGameSnackbarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NoGameSnackbarComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NoGameSnackbarComponent],
+            imports: [MatDialogModule, AppMaterialModule],
+            providers: [{ provide: MatSnackBarRef, useValue: {} }],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(NoGameSnackbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(NoGameSnackbarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

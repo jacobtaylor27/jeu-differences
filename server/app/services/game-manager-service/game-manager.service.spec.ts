1a6f6408a2ba8f24b5772815e2f80d948dd26f4c
karim El Hosni <karim.el-hosni@polymtl.ca>
2022-11-29 17:10:28 -0500

    test: increase number of clues

--- server/app/services/game-manager-service/game-manager.service.spec.ts
+++ server/app/services/game-manager-service/game-manager.service.spec.ts
@@ -296,6 +296,14 @@ describe('GameManagerService', () => {
         expect(spyLeaveGame.called).to.equal(true);
     });
 
+    it.only('should increase number of clues ', () => {
+        const game = new Game({ player: {} as User, isMulti: false }, { info: {} as PrivateGameInformation, mode: GameMode.Classic });
+        const spyFindGame = stub(Object.getPrototypeOf(gameManager), 'findGame').callsFake(() => game);
+        spyFindGame.callsFake(() => game);
+        gameManager.increaseNbClueAsked('');
+        expect(game.nbCluesAsked).to.equal(1);
+    });
+
     it('should delete a game if all player leave', () => {
         const game = new Game({ player: {} as User, isMulti: false }, { info: {} as PrivateGameInformation, mode: GameMode.Classic });
         stub(Object.getPrototypeOf(gameManager), 'findGame').callsFake(() => game);

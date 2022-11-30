6d935bcdc67c65760f0c779d1a6d86352c86726d
Xavier Jouslin <jouslin@nowray.net>
2022-11-29 20:26:34 -0500

    refactor: all difference found

--- server/app/services/difference-service/difference.service.ts
+++ server/app/services/difference-service/difference.service.ts
@@ -10,4 +10,18 @@ export class DifferenceService {
         this.gamesDifferencesFound = new Map();
         this.gamesDifferencesTotalFound = new Map();
     }
+
+    isAllDifferenceFound(playerId: string, game: Game): boolean {
+        const player = this.gamesDifferencesFound.get(game.identifier)?.get(playerId);
+
+        // if the game is already over all the differences are found and if the game is not initialize, 0 difference found
+        if (game.isGameInitialize() || game.isGameOver() || !player) {
+            return game.isGameOver();
+        }
+
+        return game.multi
+            ? player.size === this.getNbDifferencesThreshold(game.information.differences)
+            : player.size === game.information.differences.length;
+    }
+
 }

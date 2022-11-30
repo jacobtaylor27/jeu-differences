6b5101f125a8134783807671e796a017a558dc50
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-11-29 19:17:23 -0500

    fix : test

--- client/src/app/pages/main-page/main-page.component.spec.ts
+++ client/src/app/pages/main-page/main-page.component.spec.ts
@@ -1,14 +1,17 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpResponse } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { CarouselResponse } from '@app/interfaces/carousel-response';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { CommunicationService } from '@app/services/communication/communication.service';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
 import { MainPageService } from '@app/services/main-page/main-page.service';
 import { GameMode } from '@common/game-mode';
+import { of } from 'rxjs';
 
 describe('MainPageComponent', () => {
     let component: MainPageComponent;
@@ -16,11 +19,14 @@ describe('MainPageComponent', () => {
     let spyMainPageService: jasmine.SpyObj<MainPageService>;
     let spyGameInfosHandlerService: jasmine.SpyObj<GameInformationHandlerService>;
     let spyMatDialog: jasmine.SpyObj<MatDialog>;
+    let spyCommunicationService: jasmine.SpyObj<CommunicationService>;
 
     beforeEach(async () => {
         spyMainPageService = jasmine.createSpyObj('GamePageService', ['setGameMode']);
         spyGameInfosHandlerService = jasmine.createSpyObj('GameInformationHandlerService', ['setGameMode', 'getGameName', 'getGameMode']);
         spyMatDialog = jasmine.createSpyObj<MatDialog>('MatDialog', ['open']);
+        spyCommunicationService = jasmine.createSpyObj('CommunicationService', ['getAllGameInfos', 'getGamesInfoByPage']);
+
 
         await TestBed.configureTestingModule({
             declarations: [MainPageComponent],
@@ -35,8 +41,15 @@ describe('MainPageComponent', () => {
                     provide: GameInformationHandlerService,
                     useValue: spyGameInfosHandlerService,
                 },
+                {
+                    provide: CommunicationService,
+                    useValue: spyCommunicationService,
+                },
             ],
         }).compileComponents();
+        spyCommunicationService.getGamesInfoByPage.and.callFake(() => {
+            return of({ body: { carouselInfo: {}, games: [{}] } } as HttpResponse<CarouselResponse>);
+        });
         fixture = TestBed.createComponent(MainPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();

cd0549d5a1f02f76720f8e08defad7c0f2e57eab
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-01 13:01:33 -0500

    fix : added leave when play alone and leaves

--- server/app/services/socket-manager-service/socket-manager-service.service.ts
+++ server/app/services/socket-manager-service/socket-manager-service.service.ts
@@ -143,6 +143,9 @@ export class SocketManagerService {
                         .to(gameId)
                         .emit(this.gameManager.findGameMode(gameId) === GameMode.Classic ? SocketEvent.Win : SocketEvent.PlayerLeft);
                 }
+                if (!this.gameManager.isGameMultiplayer(gameId)) {
+                    socket.leave(gameId);
+                }
             });
 
             socket.on(SocketEvent.LeaveWaiting, (roomId: string, gameCard: string) => {

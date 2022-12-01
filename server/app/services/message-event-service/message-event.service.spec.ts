e87a6d7abce3868cd3d216c2c01e9aab80017228
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-12-01 18:23:33 -0500

    feat: tests for score messages

--- server/app/services/message-event-service/message-event.service.spec.ts
+++ server/app/services/message-event-service/message-event.service.spec.ts
@@ -63,4 +63,30 @@ describe('EventMessage Service', () => {
         const userName = undefined;
         expect(eventMessageService.leavingGameMessage(userName)).to.be.equal(null);
     });
+
+    it('should send a message when there is a new high score in multi', () => {
+        const messageRecord = {
+            record: { index: 1, time: 1000 },
+            playerName: 'user',
+            gameName: 'game',
+            isMulti: true,
+        };
+        const expectedResult = `${new Date(0).toLocaleTimeString('en-US')} - 
+        ${messageRecord.playerName} obtient la ${messageRecord.record.index} place dans les meilleurs temps du jeu 
+        ${messageRecord.gameName} en multijoueur`;
+        expect(eventMessageService.sendNewHighScoreMessage(messageRecord)).to.be.equal(expectedResult);
+    });
+
+    it('should send a message when there is a new high score in solo', () => {
+        const messageRecord = {
+            record: { index: 1, time: 1000 },
+            playerName: 'user',
+            gameName: 'game',
+            isMulti: false,
+        };
+        const expectedResult = `${new Date(0).toLocaleTimeString('en-US')} - 
+        ${messageRecord.playerName} obtient la ${messageRecord.record.index} place dans les meilleurs temps du jeu 
+        ${messageRecord.gameName} en solo`;
+        expect(eventMessageService.sendNewHighScoreMessage(messageRecord)).to.be.equal(expectedResult);
+    });
 });

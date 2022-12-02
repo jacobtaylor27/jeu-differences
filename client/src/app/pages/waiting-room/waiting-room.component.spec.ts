ab0e71b783278d50cdac19cf14862821fd3982cf
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-02 14:24:44 -0500

    fix : test remove unused import

--- client/src/app/pages/waiting-room/waiting-room.component.spec.ts
+++ client/src/app/pages/waiting-room/waiting-room.component.spec.ts
@@ -12,7 +12,6 @@ import { SocketEvent } from '@common/socket-event';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
-import { GameMode } from '@common/game-mode';
 import { PublicGameInformation } from '@common/game-information';
 
 /* eslint-disable @typescript-eslint/no-empty-function -- connect needs to be empty (Nikolay's example) */

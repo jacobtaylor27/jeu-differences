b4c787e1b726dca94555f78fc984f1a4f908a3ac
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-02 11:28:24 -0500

    feat : test should handle socket event difference found

--- client/src/app/components/play-area/play-area.component.spec.ts
+++ client/src/app/components/play-area/play-area.component.spec.ts
@@ -12,6 +12,7 @@ import { CommunicationService } from '@app/services/communication/communication.
 import { DifferencesDetectionHandlerService } from '@app/services/differences-detection-handler/differences-detection-handler.service';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
 import { MouseHandlerService } from '@app/services/mouse-handler/mouse-handler.service';
+import { DifferenceFound } from '@common/difference';
 import { PublicGameInformation } from '@common/game-information';
 import { GameMode } from '@common/game-mode';
 import { SocketEvent } from '@common/socket-event';
@@ -313,5 +314,32 @@ describe('PlayAreaComponent', () => {
         component.handleSocketDifferenceFound();
         socketHelper.peerSideEmit(SocketEvent.NewGameBoard, {} as PublicGameInformation);
         expect(spyDisplayImage).toHaveBeenCalled();
+    });
+
+    it('should handle socket event difference found', () => {
+        gameInformationHandlerServiceSpy.gameMode = GameMode.Classic;
+        const canvas = CanvasTestHelper.createCanvas(SIZE.x, SIZE.y);
+        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+        spyOn(component, 'getContextImgModified').and.callFake(() => {
+            return ctx;
+        });
+        spyOn(component, 'getContextModified').and.callFake(() => {
+            return ctx;
+        });
+        spyOn(component, 'getContextOriginal').and.callFake(() => {
+            return ctx;
+        });
+        spyOn(component, 'getContextDifferences').and.callFake(() => {
+            return ctx;
+        });
+        spyOn(component, 'getContextImgOriginal').and.callFake(() => {
+            return ctx;
+        });
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- calls fake and return {}
+        spyOn(component, 'displayImage').and.callFake(() => {});
+        component.handleSocketDifferenceFound();
+        socketHelper.peerSideEmit(SocketEvent.DifferenceFound, {} as DifferenceFound);
+        expect(differenceService.setNumberDifferencesFound).toHaveBeenCalled();
+        expect(differenceService.differenceDetected).toHaveBeenCalled();
     });
 });

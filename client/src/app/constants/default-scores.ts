c6b03eec895c59d717bb31050d8821b6d8fe27d8
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-12-02 12:25:46 -0500

    fix: updated default scores

--- client/src/app/constants/default-scores.ts
+++ client/src/app/constants/default-scores.ts
@@ -1,14 +1,8 @@
 import { Score } from '@common/score';
 import { ScoreType } from '@common/score-type';
 
-export const defaultSoloScores: Score[] = [
-    { playerName: 'imNull', time: 6039, type: ScoreType.Default },
-    { playerName: 'Branche', time: 6039, type: ScoreType.Default },
-    { playerName: 'Jus', time: 6039, type: ScoreType.Default },
-];
-
-export const defaultMultiScores: Score[] = [
-    { playerName: 'Coolio', time: 6039, type: ScoreType.Default },
-    { playerName: 'Jello', time: 6039, type: ScoreType.Default },
-    { playerName: 'Lapin', time: 6039, type: ScoreType.Default },
+export const defaultScores: Score[] = [
+    { playerName: 'joueur', time: 3599, type: ScoreType.Default },
+    { playerName: 'player', time: 3599, type: ScoreType.Default },
+    { playerName: 'laeib', time: 3599, type: ScoreType.Default },
 ];

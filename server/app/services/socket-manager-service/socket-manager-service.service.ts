ab8d2172ee0b94b487bb81c3bae60b5d1e06bcaf
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-12-02 12:22:29 -0500

    feat: imports

--- server/app/services/socket-manager-service/socket-manager-service.service.ts
+++ server/app/services/socket-manager-service/socket-manager-service.service.ts
@@ -8,6 +8,7 @@ import { ScoresHandlerService } from '@app/services/scores-handler-service/score
 import { Coordinate } from '@common/coordinate';
 import { PublicGameInformation } from '@common/game-information';
 import { GameMode } from '@common/game-mode';
+import { ScoreType } from '@common/score-type';
 import { SocketEvent } from '@common/socket-event';
 import * as http from 'http';
 import * as LZString from 'lz-string';

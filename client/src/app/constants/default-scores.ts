19bc271b0a7e890e8f6efa37beedc3521add8d66
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-12-02 10:55:42 -0500

    feat: new default scores value

---
+++ client/src/app/constants/default-scores.ts
@@ -0,0 +1,14 @@
+import { Score } from '@common/score';
+import { ScoreType } from '@common/score-type';
+
+export const defaultSoloScores: Score[] = [
+    { playerName: 'imNull', time: 6039, type: ScoreType.Default },
+    { playerName: 'Branche', time: 6039, type: ScoreType.Default },
+    { playerName: 'Jus', time: 6039, type: ScoreType.Default },
+];
+
+export const defaultMultiScores: Score[] = [
+    { playerName: 'Coolio', time: 6039, type: ScoreType.Default },
+    { playerName: 'Jello', time: 6039, type: ScoreType.Default },
+    { playerName: 'Lapin', time: 6039, type: ScoreType.Default },
+];

681a724b8b0cc903de73b1b8cf8ebf58b13a697b
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-02 12:35:08 -0500

    fix : remove useless imports

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -8,7 +8,6 @@ import { ClueHandlerService } from '@app/services/clue-handler-service/clue-hand
 import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
 import { ExitButtonHandlerService } from '@app/services/exit-button-handler/exit-button-handler.service';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
-import { GameMode } from '@common/game-mode';
 import { GameRecord } from '@common/game-record';
 import { SocketEvent } from '@common/socket-event';
 

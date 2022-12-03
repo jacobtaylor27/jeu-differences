ec8b255840d8e2f5e5443d72db1e1f41504aefaf
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-12-03 18:30:06 -0500

    feat: imports

--- client/src/app/components/game-card-buttons/game-card-buttons.component.ts
+++ client/src/app/components/game-card-buttons/game-card-buttons.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { GameCard } from '@app/interfaces/game-card';
-import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
 import { CommunicationService } from '@app/services/communication/communication.service';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
 import { RouterService } from '@app/services/router-service/router.service';
-import { SocketEvent } from '@common/socket-event';
 import { UserNameInputComponent } from '@app/components/user-name-input/user-name-input.component';
+import { ConfirmDeleteDialogComponent } from '@app/components/confirm-delete-dialog/confirm-delete-dialog.component';
 
 @Component({
     selector: 'app-game-card-buttons',

5bb6aa9fe2d21147f7ad2f6e5c475b074f99e133
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-03 15:23:38 -0500

    feat : new message

--- server/app/interface/reject-messages.ts
+++ server/app/interface/reject-messages.ts
@@ -1,6 +1,7 @@
 export interface RejectMessages {
     rejected: string;
     wrongName: string;
+    allGamesDeleted: string;
     deletedGame: string;
     playerQuit: string;
     gameStarted: string;

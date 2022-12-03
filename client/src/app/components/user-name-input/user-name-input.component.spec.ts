c6635ca178c4b26a4783a9df2f0917badb8550cc
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-12-03 17:57:11 -0500

    feat: imports

--- client/src/app/components/user-name-input/user-name-input.component.spec.ts
+++ client/src/app/components/user-name-input/user-name-input.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';

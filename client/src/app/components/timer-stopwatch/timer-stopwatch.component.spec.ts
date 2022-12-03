2a49f0b3bb10d64daf51f4bd73685b4b3b254611
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-03 17:24:30 -0500

    feat : missing spies

--- client/src/app/components/timer-stopwatch/timer-stopwatch.component.spec.ts
+++ client/src/app/components/timer-stopwatch/timer-stopwatch.component.spec.ts
@@ -4,6 +4,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { SocketTestHelper } from '@app/classes/socket-test-helper';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
+import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
+import { TimeFormatterService } from '@app/services/time-formatter/time-formatter.service';
 import { SocketEvent } from '@common/socket-event';
 import { Socket } from 'socket.io-client';
 import { TimerStopwatchComponent } from './timer-stopwatch.component';
@@ -14,16 +16,30 @@ describe('TimerStopwatchComponent', () => {
 
     let socketServiceMock: CommunicationSocketService;
     let socketHelper: SocketTestHelper;
+    let spyGameInfosService: jasmine.SpyObj<GameInformationHandlerService>;
+    let spyTimeFormatter: jasmine.SpyObj<TimeFormatterService>;
 
     beforeEach(async () => {
         socketHelper = new SocketTestHelper();
         socketServiceMock = new CommunicationSocketService();
         socketServiceMock['socket'] = socketHelper as unknown as Socket;
+        spyTimeFormatter = jasmine.createSpyObj('TimeFormatterService', ['formatTime']);
+        spyGameInfosService = jasmine.createSpyObj('GameInformationHandlerService', ['getConstants', 'isClassic', 'isLimitedTime']);
 
         await TestBed.configureTestingModule({
             declarations: [TimerStopwatchComponent],
             imports: [AppMaterialModule, RouterTestingModule, HttpClientModule],
-            providers: [{ provide: CommunicationSocketService, useValue: socketServiceMock }],
+            providers: [
+                { provide: CommunicationSocketService, useValue: socketServiceMock },
+                {
+                    provide: GameInformationHandlerService,
+                    useValue: spyGameInfosService,
+                },
+                {
+                    provide: TimeFormatterService,
+                    useValue: spyTimeFormatter,
+                },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(TimerStopwatchComponent);

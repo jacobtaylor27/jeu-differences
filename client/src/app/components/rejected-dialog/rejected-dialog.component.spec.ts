57e28ebb43037bec89cd50f9c309d18408383539
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-03 15:20:23 -0500

    fix : add missing imports

--- client/src/app/components/rejected-dialog/rejected-dialog.component.spec.ts
+++ client/src/app/components/rejected-dialog/rejected-dialog.component.spec.ts
@@ -1,5 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppMaterialModule } from '@app/modules/material.module';
 
 import { RejectedDialogComponent } from './rejected-dialog.component';
 
@@ -16,6 +19,7 @@ describe('RejectedDialogComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [RejectedDialogComponent],
             providers: [{ provide: MAT_DIALOG_DATA, useValue: model }],
+            imports: [RouterTestingModule, HttpClientModule, AppMaterialModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(RejectedDialogComponent);

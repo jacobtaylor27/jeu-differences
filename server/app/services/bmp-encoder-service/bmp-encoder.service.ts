4fe9eaa08e3581f8c1f7dd71c188b6d5d90a2c84
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-03 23:20:53 -0500

    fix : remove unused encodeBmpIntoB method

--- server/app/services/bmp-encoder-service/bmp-encoder.service.ts
+++ server/app/services/bmp-encoder-service/bmp-encoder.service.ts
@@ -4,11 +4,6 @@ import { Service } from 'typedi';
 
 @Service()
 export class BmpEncoderService {
-    async encodeBmpIntoB(filepath: string, bmpObj: Bmp): Promise<void> {
-        if (!(await this.isFileExtensionValid(filepath))) throw new Error('File extension must be a .bmp');
-        await fs.writeFile(filepath, (await bmpObj.toBmpImageData()).data);
-    }
-
     async base64Encode(file: string): Promise<string> {
         const body = await fs.readFile(file);
         return body.toString('base64');

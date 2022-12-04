e5a6f514d89cb26b8b55849f0cc86b9668e470d3
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-04 17:23:56 -0500

    feat : new subject

--- client/src/app/services/clue-handler-service/clue-handler.service.ts
+++ client/src/app/services/clue-handler-service/clue-handler.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { SocketEvent } from '@common/socket-event';
 import { CommunicationSocketService } from '@app/services/communication-socket/communication-socket.service';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
+import { Subject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ClueHandlerService {
     clueAskedCounter: number = 0;
+    $clueAsked: Subject<void> = new Subject();
 
     constructor(public communicationSocket: CommunicationSocketService, public gameInformation: GameInformationHandlerService) {}
 

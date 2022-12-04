02b4d19f3d938d0025e0bb31bdb42b41591721f0
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-04 17:23:26 -0500

    fix : remove i key event from clue component

--- client/src/app/components/clues-area/clues-area.component.ts
+++ client/src/app/components/clues-area/clues-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClueHandlerService } from '@app/services/clue-handler-service/clue-handler.service';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
 import { NUMBER_CLUES } from '@common/clues';
@@ -11,15 +11,8 @@ export class CluesAreaComponent implements OnInit {
     clueAskedCounter: number = 0;
     isDisabled: boolean = false;
 
-    constructor(public gameInformation: GameInformationHandlerService, private readonly clueHandlerService: ClueHandlerService) {}
-
-    @HostListener('window: keydown', ['$event'])
-    buttonDetect(event: KeyboardEvent) {
-        if (event.key === 'i') {
-            if (!this.isDisabled) {
-                this.getClue();
-            }
-        }
+    constructor(public gameInformation: GameInformationHandlerService, private readonly clueHandlerService: ClueHandlerService) {
+        this.handleClueAsked();
     }
 
     ngOnInit(): void {

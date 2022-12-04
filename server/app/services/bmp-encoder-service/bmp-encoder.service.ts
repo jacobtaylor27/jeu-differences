82d019bdb2823f54edc70935b6bf68a979a6426f
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-03 23:22:58 -0500

    fix : remove isFileExtensionValid unused

--- server/app/services/bmp-encoder-service/bmp-encoder.service.ts
+++ server/app/services/bmp-encoder-service/bmp-encoder.service.ts
@@ -1,4 +1,3 @@
-import { Bmp } from '@app/classes/bmp/bmp';
 import { promises as fs } from 'fs';
 import { Service } from 'typedi';
 
@@ -7,9 +6,5 @@ export class BmpEncoderService {
     async base64Encode(file: string): Promise<string> {
         const body = await fs.readFile(file);
         return body.toString('base64');
-    }
-
-    private async isFileExtensionValid(filename: string): Promise<boolean> {
-        return filename.match('^.*.(bmp)$') !== null;
     }
 }

f6d31ed83311250596a74b3de63422ac9fcd8e01
lauriebedard <laurie.bedard-cote@polymtl.ca>
2022-12-03 19:52:35 -0500

    feat : set infos

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
 import { GameInformationHandlerService } from '@app/services/game-information-handler/game-information-handler.service';
 import { GameMode } from '@common/game-mode';
 @Component({
@@ -6,12 +6,15 @@ import { GameMode } from '@common/game-mode';
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements AfterContentInit {
     gameMode: GameMode;
     gameName: string;
     isMulti: boolean;
+    penaltyTime : number;
+    differenceTime : number
 
     constructor(private readonly gameInformationHandlerService: GameInformationHandlerService) {
+        this.gameInformationHandlerService.getConstants();
         this.gameMode = this.gameInformationHandlerService.getGameMode();
         this.gameName = this.gameInformationHandlerService.getGameName();
         this.isMulti = this.gameInformationHandlerService.isMulti;
@@ -20,4 +23,19 @@ export class SidebarComponent {
             this.gameName = this.gameInformationHandlerService.getGameName();
         });
     }
+
+    ngAfterContentInit():  void {
+        this.penaltyTime = this.gameInformationHandlerService.gameTimeConstants.penaltyTime;
+        this.differenceTime = this.gameInformationHandlerService.gameTimeConstants.successTime;
+    }
+
+    isSoloMode(){
+        return !this.gameInformationHandlerService.isMulti;
+    }
+
+    isLimitedTimeMode(){
+        return this.gameInformationHandlerService.isLimitedTime();
+    }
+
+    
 }

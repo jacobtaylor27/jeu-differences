69967b8636b8b4b178676fdd08384dc38f434a1d
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-12-04 00:56:33 -0500

    feat: import

--- client/src/app/components/dialog-game-over/dialog-game-over.component.ts
+++ client/src/app/components/dialog-game-over/dialog-game-over.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Theme } from '@app/enums/theme';
+import { TimeFormatterService } from '@app/services/time-formatter/time-formatter.service';
 import { GameRecord } from '@common/game-record';
 
 @Component({

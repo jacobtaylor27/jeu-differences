71caf685c165a2c61eaf0b3352fe0dc93a4234e9
Xavier Jouslin <jouslin@nowray.net>
2022-12-04 20:30:31 -0500

    fix: remove anuse throw exception

--- server/app/classes/pixel/pixel.spec.ts
+++ server/app/classes/pixel/pixel.spec.ts
@@ -9,23 +9,6 @@ describe('Pixel', () => {
         expect(pixel.a && pixel.b && pixel.g).to.equal(0);
     });
 
-    it('Pixel constructor should not allow a pixel at its construction to be negative', () => {
-        expect(() => {
-            // eslint-disable-next-line -- no magic number
-            new Pixel(-1, 0, 0);
-        }).to.throw(Error);
-
-        expect(() => {
-            // eslint-disable-next-line -- no magic number
-            new Pixel(0, -1, 0);
-        }).to.throw(Error);
-
-        expect(() => {
-            // eslint-disable-next-line -- no magic number
-            new Pixel(0, 0, -1);
-        }).to.throw(Error);
-    });
-
     it('isWhite(...) should return true given an array of pixels with 255 for their value', () => {
         const whitePixel: Pixel = new Pixel(PIXEL_COLOR.white, PIXEL_COLOR.white, PIXEL_COLOR.white);
         expect(whitePixel['isWhite']()).to.equal(true);
@@ -44,17 +27,6 @@ describe('Pixel', () => {
     it("isBlack(...) should return false given an array of pixels that isn't perfectly black", () => {
         const whitePixel: Pixel = new Pixel(PIXEL_COLOR.black, PIXEL_COLOR.white, PIXEL_COLOR.black);
         expect(whitePixel['isBlack']()).to.equal(false);
-    });
-
-    it('arePixelValid(...) should return true when a pixel are superior to 0', () => {
-        const whitePixel: Pixel = new Pixel(PIXEL_COLOR.black, PIXEL_COLOR.white, PIXEL_COLOR.black);
-        expect(whitePixel['arePixelsValid'](0, 0, 0)).to.equal(true);
-    });
-
-    it('arePixelValid(...) should return false when a pixel is less then 0', () => {
-        const whitePixel: Pixel = new Pixel(PIXEL_COLOR.black, PIXEL_COLOR.white, PIXEL_COLOR.black);
-        // eslint-disable-next-line -- no magic number
-        expect(whitePixel['arePixelsValid'](0, -1, 0)).to.equal(false);
     });
 
     it('isEqual(...) should return false when the pixels are not deeply the same', () => {

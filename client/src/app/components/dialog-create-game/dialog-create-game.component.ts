e2c215e71f0b726aaae2f4c3c093438dccab61d8
Jacob Taylor <jacob.taylor@polymtl.ca>
2022-12-04 20:12:38 -0500

    feat: add validator to the form control

--- client/src/app/components/dialog-create-game/dialog-create-game.component.ts
+++ client/src/app/components/dialog-create-game/dialog-create-game.component.ts
@@ -16,7 +16,9 @@ import { CommunicationService } from '@app/services/communication/communication.
 export class DialogCreateGameComponent implements AfterViewInit {
     @ViewChild('imageDifference', { static: false }) differentImage!: ElementRef<HTMLCanvasElement>;
     theme: typeof Theme = Theme;
-    form: FormGroup = new FormGroup({ name: new FormControl('', [Validators.pattern('[a-zA-Z ]*'), Validators.required]) });
+    form: FormGroup = new FormGroup({
+        name: new FormControl('', [Validators.pattern('[a-zA-Z ]*'), Validators.required, this.noWhiteSpaceValidator]),
+    });
     // eslint-disable-next-line max-params -- absolutely need all the imported services
     constructor(
         @Inject(MAT_DIALOG_DATA)
